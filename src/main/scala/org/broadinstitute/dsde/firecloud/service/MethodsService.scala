c643ead076f973c5233d176c9ac2129c68f8a08b
Greg Rushton <grushton@broadinstitute.org>
2015-08-06 08:11:38 -0400

    Remove swagger from the methods service - it was already broken and not visible to the API

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/MethodsService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/MethodsService.scala
@@ -1,15 +1,10 @@
 package org.broadinstitute.dsde.firecloud.service
 
-import javax.ws.rs.Path
-
 import akka.actor.{Actor, Props}
-import com.wordnik.swagger.annotations.{ApiResponse, ApiResponses, Api, ApiOperation}
-import org.broadinstitute.dsde.firecloud.model.MethodRepository.{Configuration, Method}
+import org.broadinstitute.dsde.firecloud.{FireCloudConfig, HttpClient}
 import org.slf4j.LoggerFactory
 import spray.client.pipelining._
 import spray.routing.{HttpService, Route}
-
-import org.broadinstitute.dsde.firecloud.{FireCloudConfig, HttpClient}
 
 class MethodsServiceActor extends Actor with MethodsService {
   def actorRefFactory = context
@@ -18,46 +13,16 @@ class MethodsServiceActor extends Actor with MethodsService {
 
 trait MethodsService extends HttpService with FireCloudDirectives {
 
-  val routes = listMethodsRoute ~ listConfigurationsRoute
+  val routes = localRoutes
   lazy val log = LoggerFactory.getLogger(getClass)
 
-  @Path("/methods")
-  @Api(value = "/methods",
-    description = "Methods Repository Service",
-    produces = "application/json")
-  @ApiOperation(
-    value = "list methods",
-    nickname = "list methods",
-    httpMethod = "GET",
-    response = classOf[Method],
-    responseContainer = "List",
-    notes = "response is a list of methods from the methods repository")
-  @ApiResponses(Array(
-    new ApiResponse(code = 200, message = "Successful Request"),
-    new ApiResponse(code = 500, message = "Internal Error")))
-  def listMethodsRoute: Route =
+  def localRoutes: Route =
     path("methods") {
       get { requestContext =>
         actorRefFactory.actorOf(Props(new HttpClient(requestContext))) !
           HttpClient.PerformExternalRequest(Get(FireCloudConfig.Methods.methodsListUrl))
       }
-    }
-
-  @Path("/configurations")
-  @Api(value = "/configurations",
-    description = "Methods Repository Service",
-    produces = "application/json")
-  @ApiOperation(
-    value = "list configurations",
-    nickname = "list configurations",
-    httpMethod = "GET",
-    response = classOf[Configuration],
-    responseContainer = "List",
-    notes = "response is a list of configurations from the methods repository")
-  @ApiResponses(Array(
-    new ApiResponse(code = 200, message = "Successful Request"),
-    new ApiResponse(code = 500, message = "Internal Error")))
-  def listConfigurationsRoute: Route =
+    } ~
     path("configurations") {
       get { requestContext =>
         actorRefFactory.actorOf(Props(new HttpClient(requestContext))) !
