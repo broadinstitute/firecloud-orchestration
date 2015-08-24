26461290de27622c1697c78ee5f8bca50715828e
David Mohs <dgit@mohsinc.com>
2015-08-24 13:50:10 -0400

    First pass at Swagger via YAML.

--- src/main/scala/org/broadinstitute/dsde/firecloud/FireCloudConfig.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/FireCloudConfig.scala
@@ -14,20 +14,6 @@ object FireCloudConfig {
     lazy val timeoutSeconds = httpConfig.getLong("timeoutSeconds")
   }
 
-  object SwaggerConfig {
-    private val swagger = merged.getConfig("swagger")
-    lazy val apiVersion = swagger.getString("apiVersion")
-    lazy val swaggerVersion = swagger.getString("swaggerVersion")
-    lazy val info = swagger.getString("info")
-    lazy val description = swagger.getString("description")
-    lazy val termsOfServiceUrl = swagger.getString("termsOfServiceUrl")
-    lazy val contact = swagger.getString("contact")
-    lazy val license = swagger.getString("license")
-    lazy val licenseUrl = swagger.getString("licenseUrl")
-    lazy val baseUrl = swagger.getString("baseUrl")
-    lazy val apiDocs = swagger.getString("apiDocs")
-  }
-
   object Agora {
     private val methods = merged.getConfig("methods")
     lazy val baseUrl = methods.getString("baseUrl")
