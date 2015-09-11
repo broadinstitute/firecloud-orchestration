c106819900ccc4b8a1419236aa4944137b33e9c9
Matt Putnam <putnam@broadinstitute.org>
2015-09-11 17:32:35 -0400

    Added delete endpoint

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/WorkspaceService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/WorkspaceService.scala
@@ -5,7 +5,7 @@ import java.util.Date
 
 import akka.actor.{Actor, Props}
 import org.slf4j.LoggerFactory
-import spray.client.pipelining.{Get, Post, Patch}
+import spray.client.pipelining.{Get, Post, Patch, Delete}
 import spray.http.StatusCodes._
 import spray.json.DefaultJsonProtocol._
 import spray.json._
@@ -35,67 +35,73 @@ trait WorkspaceService extends HttpService with PerRequestCreator with FireCloud
           val extReq =  Get(rawlsWorkspacesRoot)
           externalHttpPerRequest(requestContext, extReq)
         } ~
-          post {
-            entity(as[String]) { ingest =>
-              // TODO: replace with a directive that pulls the username from the Google info!
-              // TODO: rawls should populate createdBy/createdDate/attributes on its own!
-              // commonNameFromOptionalCookie() { username => requestContext =>
-              requestContext =>
+        post {
+          entity(as[String]) { ingest =>
+            // TODO: replace with a directive that pulls the username from the Google info!
+            // TODO: rawls should populate createdBy/createdDate/attributes on its own!
+            // commonNameFromOptionalCookie() { username => requestContext =>
+            requestContext =>
               val username = Option("FIXME!")
 
-                username match {
-                  case Some(x) =>
-                    val params = ingest.parseJson.convertTo[Map[String, JsValue]]
-                      .updated("createdBy", username.get.toJson)
-                      .updated("createdDate", dateFormat.format(new Date()).toJson)
-                      .updated("attributes", JsObject())
-                    val extReq = Post(
-                      rawlsWorkspacesRoot,
-                      HttpClient.createJsonHttpEntity(params.toJson.compactPrint)
-                    )
-                    externalHttpPerRequest(requestContext, extReq)
-                  case None =>
-                    log.error("No authenticated username provided.")
-                    requestContext.complete(Unauthorized)
-                }
-              // }
-            }
+              username match {
+                case Some(x) =>
+                  val params = ingest.parseJson.convertTo[Map[String, JsValue]]
+                    .updated("createdBy", username.get.toJson)
+                    .updated("createdDate", dateFormat.format(new Date()).toJson)
+                    .updated("attributes", JsObject())
+                  val extReq = Post(
+                    rawlsWorkspacesRoot,
+                    HttpClient.createJsonHttpEntity(params.toJson.compactPrint)
+                  )
+                  externalHttpPerRequest(requestContext, extReq)
+                case None =>
+                  log.error("No authenticated username provided.")
+                  requestContext.complete(Unauthorized)
+              }
+            // }
           }
+        }
       } ~
-        pathPrefix(Segment / Segment) { (workspaceNamespace, workspaceName) =>
-          pathEnd { requestContext =>
+      pathPrefix(Segment / Segment) { (workspaceNamespace, workspaceName) =>
+        pathEnd {
+          get { requestContext =>
             val extReq = Get(rawlsWorkspacesRoot + "/%s/%s".format(workspaceNamespace, workspaceName))
             externalHttpPerRequest(requestContext, extReq)
           } ~
-            path("methodconfigs") {
-              get { requestContext =>
-                val extReq = Get(rawlsWorkspacesRoot + "/%s/%s/methodconfigs".format(workspaceNamespace, workspaceName))
-                externalHttpPerRequest(requestContext, extReq)
-              }
-            } ~
-            path("importEntities") {
-              post {
-                formFields( 'entities ) { (entitiesTSV) =>
-                  respondWithJSON { requestContext =>
-                    perRequest(requestContext, Props(new EntityClient(requestContext)),
-                      EntityClient.ImportEntitiesFromTSV(workspaceNamespace, workspaceName, entitiesTSV))
-                  }
-                }
-              }
-            } ~
-            path("acl") {
-              get { requestContext =>
-                val extReq = Get(rawlsWorkspacesRoot + "/%s/%s/acl".format(workspaceNamespace, workspaceName))
-                externalHttpPerRequest(requestContext, extReq)
-              } ~
-              patch {
-                respondWithJSON { requestContext =>
-                  val extReq = Patch(rawlsWorkspacesRoot + "/%s/%s/acl".format(workspaceNamespace, workspaceName),
-                    requestContext.request.entity)
-                  externalHttpPerRequest(requestContext, extReq)
-                }
+          delete { requestContext =>
+            val extReq = Delete(rawlsWorkspacesRoot + "/%s/%s".format(workspaceNamespace, workspaceName))
+            externalHttpPerRequest(requestContext, extReq)
+          }
+        } ~
+        path("methodconfigs") {
+          get { requestContext =>
+            val extReq = Get(rawlsWorkspacesRoot + "/%s/%s/methodconfigs".format(workspaceNamespace, workspaceName))
+            externalHttpPerRequest(requestContext, extReq)
+          }
+        } ~
+        path("importEntities") {
+          post {
+            formFields( 'entities ) { (entitiesTSV) =>
+              respondWithJSON { requestContext =>
+                perRequest(requestContext, Props(new EntityClient(requestContext)),
+                  EntityClient.ImportEntitiesFromTSV(workspaceNamespace, workspaceName, entitiesTSV))
               }
             }
+          }
+        } ~
+        path("acl") {
+          get { requestContext =>
+            val extReq = Get(rawlsWorkspacesRoot + "/%s/%s/acl".format(workspaceNamespace, workspaceName))
+            externalHttpPerRequest(requestContext, extReq)
+          } ~
+          patch {
+            respondWithJSON { requestContext =>
+              val extReq = Patch(rawlsWorkspacesRoot + "/%s/%s/acl".format(workspaceNamespace, workspaceName),
+                requestContext.request.entity)
+              externalHttpPerRequest(requestContext, extReq)
+            }
+          }
         }
+      }
     }
 }
