d71ba66b42117e9fc4d19baf22d93a5734ee91fa
Matt Putnam <putnam@broadinstitute.org>
2015-09-14 14:49:03 -0400

    Add directive to pass through endpoints

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/SubmissionService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/SubmissionService.scala
@@ -1,12 +1,11 @@
 package org.broadinstitute.dsde.firecloud.service
 
-import akka.actor.{Props, Actor}
+import akka.actor.Actor
 import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol._
 import org.broadinstitute.dsde.firecloud.model.SubmissionIngest
-import org.broadinstitute.dsde.firecloud.{FireCloudConfig, HttpClient}
+import org.broadinstitute.dsde.firecloud.FireCloudConfig
 import org.slf4j.LoggerFactory
 import spray.client.pipelining._
-import spray.http.HttpRequest
 import spray.httpx.SprayJsonSupport._
 import spray.routing.{HttpService, Route}
 
@@ -22,45 +21,20 @@ trait SubmissionService extends HttpService with PerRequestCreator with FireClou
   lazy val log = LoggerFactory.getLogger(getClass)
 
   def postAndGetRoutes: Route =
-    path("workspaces" / Segment / Segment / "submissions") { (workspaceNamespace, workspaceName) =>
-      get { requestContext =>
-          val extReq = Get(FireCloudConfig.Rawls.
-            submissionsUrl(workspaceNamespace, workspaceName))
-          externalHttpPerRequest(requestContext, extReq)
-      }
-    } ~
-    path("workspaces" / Segment / Segment / "submissions") { (workspaceNamespace, workspaceName) =>
-      post {
-        entity(as[SubmissionIngest]) { submission => requestContext =>
-          val extReq = Post(FireCloudConfig.Rawls.
-            submissionsUrl(workspaceNamespace, workspaceName), submission)
-          externalHttpPerRequest(requestContext, extReq)
+    pathPrefix("workspaces" / Segment / Segment / "submissions") { (workspaceNamespace, workspaceName) =>
+      val listSubmissionsPath = FireCloudConfig.Rawls.submissionsUrl(workspaceNamespace, workspaceName)
+      pathEnd {
+        passthrough(listSubmissionsPath, "get", "post")
+      } ~
+      pathPrefix(Segment) { submissionId =>
+        pathEnd {
+          passthrough(FireCloudConfig.Rawls.
+            submissionByIdUrl(workspaceNamespace, workspaceName, submissionId), "get", "delete")
+        } ~
+        path("workflows" / Segment / "outputs") { workflowId =>
+          passthrough(FireCloudConfig.Rawls.
+            workflowOutputsByIdUrl(workspaceNamespace, workspaceName, submissionId, workflowId), "get")
         }
       }
-    } ~
-    path("workspaces" / Segment / Segment / "submissions" / Segment) {
-      (workspaceNamespace, workspaceName, submissionId) =>
-      get { requestContext =>
-        val extReq = Get(FireCloudConfig.Rawls.
-          submissionByIdUrl(workspaceNamespace, workspaceName, submissionId))
-        externalHttpPerRequest(requestContext, extReq)
-      }
-    } ~
-    path("workspaces" / Segment / Segment / "submissions" / Segment) {
-      (workspaceNamespace, workspaceName, submissionId) =>
-        delete { requestContext =>
-          val extReq: HttpRequest = Delete(FireCloudConfig.Rawls.
-            submissionByIdUrl(workspaceNamespace, workspaceName, submissionId))
-          externalHttpPerRequest(requestContext, extReq)
-        }
-    } ~
-    path("workspaces" / Segment / Segment / "submissions" / Segment / "workflows" / Segment / "outputs") {
-      (workspaceNamespace, workspaceName, submissionId, workflowId) =>
-        get { requestContext =>
-          val extReq = Get(FireCloudConfig.Rawls.
-            workflowOutputsByIdUrl(workspaceNamespace, workspaceName, submissionId, workflowId))
-          externalHttpPerRequest(requestContext, extReq)
-        }
     }
-
 }
