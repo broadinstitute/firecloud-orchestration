f940dc29a5cdcea44e55df17786089a8f124def7
Edward A. Salinas <esalinas@broadinstitute.org>
2015-10-15 14:17:28 -0400

    merge the configs and methods routes into a single route using a regex and using matching

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/MethodsService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/MethodsService.scala
@@ -18,24 +18,23 @@ class MethodsServiceActor extends Actor with MethodsService {
 
 
 
+
+
 trait MethodsService extends HttpService with PerRequestCreator with FireCloudDirectives {
 
   lazy val log = LoggerFactory.getLogger(getClass)
 
-  // Agora routes that require special handling
+  // Agora permissions that require special handling
 
-  val methodsACLOverrideRoute: Route =
-    path("methods" / Segment / Segment / IntNumber / "permissions") { (namespace, name, snapshotId) =>
-
-      // generate the Agora url we'll call
-      val url = FireCloudConfig.Agora.methodsPermissionsFromMethod(namespace, name, snapshotId)
-
-        get { requestContext =>
-          // pass to AgoraPermissionHandler
-          perRequest(requestContext,
-            Props(new AgoraPermissionActor(requestContext)),
-            AgoraPermissionHandler.Get(url))
-        } ~
+  val methodsAndConfigsACLOverrideRoute: Route =
+    path( "configurations|methods".r / Segment / Segment / IntNumber / "permissions") { ( agora_ent, namespace, name, snapshotId) =>
+      val url = getUrlFromBasePath(agora_ent, namespace, name, snapshotId)
+      get { requestContext =>
+        // pass to AgoraPermissionHandler
+        perRequest(requestContext,
+          Props(new AgoraPermissionActor(requestContext)),
+          AgoraPermissionHandler.Get(url))
+      } ~
         post {
           // take the body of the HTTP POST and construct a FireCloudPermission from it
           entity(as[List[FireCloudPermission]]) {
@@ -52,32 +51,6 @@ trait MethodsService extends HttpService with PerRequestCreator with FireCloudDi
 
 
 
-  val configurationsACLOverrideRoute: Route =
-    path("configurations" / Segment / Segment / IntNumber / "permissions") { (namespace, name, snapshotId) =>
-
-      // generate the Agora url we'll call
-      val url = FireCloudConfig.Agora.configurationsPermissionsFromConfig(namespace, name, snapshotId)
-
-      get { requestContext =>
-        // pass to AgoraPermissionHandler
-        perRequest(requestContext,
-          Props(new AgoraPermissionActor(requestContext)),
-          AgoraPermissionHandler.Get(url))
-        } ~
-      post {
-        // take the body of the HTTP POST and construct a FireCloudPermission from it
-        entity(as[List[FireCloudPermission]]) {
-          fireCloudPermissions => requestContext =>
-            // translate the FireCloudPermissions into  AgoraPermissions
-            val agoraPermissions = fireCloudPermissions.map(x => x.toAgoraPermission)
-            // pass to AgoraPermissionHandler
-            perRequest(
-              requestContext,
-              Props(new AgoraPermissionActor(requestContext)),
-              AgoraPermissionHandler.Post(url, agoraPermissions))
-          }
-        }}
-
 
       // Agora routes that can be passthroughs. Because these routes conflict with the override routes, make sure
       // they are processed second!
@@ -86,7 +59,17 @@ trait MethodsService extends HttpService with PerRequestCreator with FireCloudDi
           passthroughAllPaths("configurations", FireCloudConfig.Agora.configurationsBaseUrl)
 
       // combine all of the above route definitions, keeping overrides first.
-      val routes =  methodsACLOverrideRoute ~ configurationsACLOverrideRoute ~ passthroughRoutes
+      val routes =  methodsAndConfigsACLOverrideRoute ~ passthroughRoutes
 
 
+
+  private def getUrlFromBasePath(agoraEntity: String, namespace: String, name: String, snapshotId: Int): String = {
+    agoraEntity match {
+      case "methods" =>
+        FireCloudConfig.Agora.methodsPermissionsFromMethod(namespace, name, snapshotId)
+      case "configurations" =>
+        FireCloudConfig.Agora.configurationsPermissionsFromConfig(namespace, name, snapshotId)
+    }
+
+  }
 }
