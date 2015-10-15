854da947f4da5d5b35130b48ee9722f132a06e5a
Greg Rushton <grushton@broadinstitute.org>
2015-10-15 06:08:19 -0400

    Update workspace service so that create call is both documented in the swagger and makes a passthrough call to rawls.

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/WorkspaceService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/WorkspaceService.scala
@@ -1,17 +1,12 @@
 package org.broadinstitute.dsde.firecloud.service
 
 import java.text.SimpleDateFormat
-import java.util.Date
 
 import akka.actor.{Actor, Props}
+import org.broadinstitute.dsde.firecloud.{EntityClient, FireCloudConfig}
 import org.slf4j.LoggerFactory
 import spray.http.HttpMethods
-import spray.http.StatusCodes._
-import spray.json.DefaultJsonProtocol._
-import spray.json._
 import spray.routing._
-
-import org.broadinstitute.dsde.firecloud.{EntityClient, FireCloudConfig, HttpClient}
 
 class WorkspaceServiceActor extends Actor with WorkspaceService {
   def actorRefFactory = context
@@ -30,33 +25,7 @@ trait WorkspaceService extends HttpService with PerRequestCreator with FireCloud
   val routes: Route =
     pathPrefix("workspaces") {
       pathEnd {
-        passthrough(rawlsWorkspacesRoot, HttpMethods.GET) ~
-        post {
-          entity(as[String]) { ingest =>
-            // TODO: replace with a directive that pulls the username from the Google info!
-            // TODO: rawls should populate createdBy/createdDate/attributes on its own!
-            // commonNameFromOptionalCookie() { username => requestContext =>
-            requestContext =>
-              val username = Option("FIXME!")
-
-              username match {
-                case Some(x) =>
-                  val params = ingest.parseJson.convertTo[Map[String, JsValue]]
-                    .updated("createdBy", username.get.toJson)
-                    .updated("createdDate", dateFormat.format(new Date()).toJson)
-                    .updated("attributes", JsObject())
-                  val extReq = Post(
-                    rawlsWorkspacesRoot,
-                    HttpClient.createJsonHttpEntity(params.toJson.compactPrint)
-                  )
-                  externalHttpPerRequest(requestContext, extReq)
-                case None =>
-                  log.error("No authenticated username provided.")
-                  requestContext.complete(Unauthorized)
-              }
-            // }
-          }
-        }
+        passthrough(rawlsWorkspacesRoot, HttpMethods.GET, HttpMethods.POST)
       } ~
       pathPrefix(Segment / Segment) { (workspaceNamespace, workspaceName) =>
         val workspacePath = rawlsWorkspacesRoot + "/%s/%s".format(workspaceNamespace, workspaceName)
