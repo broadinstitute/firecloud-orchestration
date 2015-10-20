e0a707fc9f8046dc71deb206e7e89b6285a69908
Edward A. Salinas <esalinas@broadinstitute.org>
2015-10-20 09:15:58 -0400

    added comments/spaces

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/MethodsService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/MethodsService.scala
@@ -38,12 +38,16 @@ trait MethodsService extends HttpService with PerRequestCreator with FireCloudDi
           entity(as[List[FireCloudPermission]]) {
             fireCloudPermissions => requestContext =>
               //scan user and roles of each input to make sure they're valid
+              //this validityFlag represents a list of booleans where each item
+              //is true if an individual input is valid and okay to send to Agora
               val validityFlags=fireCloudPermissions.map(x => x.verifyValidUserAndRole)
               //see http://stackoverflow.com/questions/22518773/applying-logical-and-to-list-of-boolean-values
               val andAllByUsersAndRoles=validityFlags.foldLeft(true)(_ && _)
               if(!(andAllByUsersAndRoles))
                 {
-                requestContext.complete(HttpResponseWithErrorReport(BadRequest, "Invalid user or access setting detected! Each permission posted must have a non-empty user and a permission of READER, OWNER, or NO ACCESS!"))
+                requestContext.complete(HttpResponseWithErrorReport(BadRequest,
+                  "Invalid user or access setting detected! Each permission posted"+
+                    " must have a non-empty user and a permission of READER, OWNER, or NO ACCESS!"))
                 }
 
               // translate the FireCloudPermissions into  AgoraPermissions
