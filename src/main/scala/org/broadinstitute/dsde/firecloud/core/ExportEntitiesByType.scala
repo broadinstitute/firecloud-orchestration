82896eeef14a9436156acb5ebc62101cfebb0127
Gregory Rushton <grushton@broadinstitute.org>
2015-11-23 17:57:20 -0500

    Fix close-over-sender anti-pattern with pipeTo pattern

--- src/main/scala/org/broadinstitute/dsde/firecloud/core/ExportEntitiesByType.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/core/ExportEntitiesByType.scala
@@ -2,12 +2,16 @@ package org.broadinstitute.dsde.firecloud.core
 
 import akka.actor.{Actor, Props}
 import akka.event.Logging
+import akka.pattern.pipe
+
 import org.broadinstitute.dsde.firecloud.core.ExportEntitiesByType.ProcessEntities
 import org.broadinstitute.dsde.firecloud.core.GetEntitiesWithType.EntityWithType
 import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol._
+import org.broadinstitute.dsde.firecloud.model.RequestCompleteWithErrorReport
 import org.broadinstitute.dsde.firecloud.service.FireCloudRequestBuilding
 import org.broadinstitute.dsde.firecloud.service.PerRequest.{RequestComplete, RequestCompleteWithHeaders}
 import org.broadinstitute.dsde.firecloud.utils.TSVFormatter
+
 import spray.client.pipelining._
 import spray.http.MediaTypes._
 import spray.http.StatusCodes._
@@ -16,7 +20,7 @@ import spray.httpx.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
 import spray.routing.RequestContext
 
-import scala.util.{Failure, Success}
+import scala.concurrent.Future
 
 object ExportEntitiesByType {
   case class ProcessEntities(url: String, filename: String, entityType: String)
@@ -31,30 +35,26 @@ class ExportEntitiesByTypeActor(requestContext: RequestContext) extends Actor wi
 
   override def receive: Receive = {
     case ProcessEntities(encodedUrl: String, filename: String, entityType: String) =>
-      log.debug("Processing entities for url: " + encodedUrl)
       val pipeline = authHeaders(requestContext) ~> sendReceive
-      pipeline { Get(encodedUrl) } onComplete {
-        case Success(response) if response.status == OK =>
-          val entities = unmarshal[List[EntityWithType]].apply(response)
-          log.debug("Processed entities: " + entities.toString)
-          val data = TSVFormatter.makeTsvString(entities, entityType)
-          context.parent ! RequestCompleteWithHeaders(
-            (OK, data),
-            HttpHeaders.`Content-Disposition`.apply("attachment", Map("filename" -> filename)),
-            HttpHeaders.`Content-Type`(`text/plain`))
-          context stop self
-        case Success(response) if response.status != OK =>
-          context.parent ! RequestComplete(response)
-          context stop self
-        case Failure(error) =>
-          context.parent ! RequestComplete(StatusCodes.InternalServerError, error.getMessage)
-          context stop self
+      pipeline { Get(encodedUrl) }.map { response => {
+        response match {
+          case x if x.status == OK =>
+            val entities = unmarshal[List[EntityWithType]].apply(response)
+            log.debug("Processed entities: " + entities.toString)
+            val data = TSVFormatter.makeTsvString(entities, entityType)
+            RequestCompleteWithHeaders(
+              (OK, data),
+              HttpHeaders.`Content-Disposition`.apply("attachment", Map("filename" -> filename)),
+              HttpHeaders.`Content-Type`(`text/plain`))
+          case x if x.status != OK =>
+            RequestComplete(response)
+        }
       }
+      }.recoverWith {
+        case e: Throwable => Future(RequestCompleteWithErrorReport(InternalServerError, e.getMessage))
+      } pipeTo context.parent
     case _ =>
-      context.parent ! RequestComplete(StatusCodes.BadRequest)
-      context stop self
+      Future(RequestComplete(StatusCodes.BadRequest)) pipeTo context.parent
   }
 
 }
-
-
--- src/main/scala/org/broadinstitute/dsde/firecloud/core/ProfileActor.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/core/ProfileActor.scala
@@ -2,18 +2,20 @@ package org.broadinstitute.dsde.firecloud.core
 
 import akka.actor.{Actor, Props}
 import akka.event.Logging
+import akka.pattern.pipe
+
 import org.broadinstitute.dsde.firecloud.core.ProfileActor.UpdateProfile
 import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol._
 import org.broadinstitute.dsde.firecloud.model._
 import org.broadinstitute.dsde.firecloud.service.{UserService, FireCloudRequestBuilding}
-import org.broadinstitute.dsde.firecloud.service.PerRequest.RequestComplete
+import org.broadinstitute.dsde.firecloud.service.PerRequest.{PerRequestMessage, RequestComplete}
+
 import spray.client.pipelining._
-import spray.http.HttpResponse
+import spray.http.{StatusCode, HttpResponse}
 import spray.http.StatusCodes._
 import spray.httpx.SprayJsonSupport._
 import spray.routing.RequestContext
 
-import scala.collection.immutable.Iterable
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
@@ -31,56 +33,50 @@ class ProfileActor(requestContext: RequestContext) extends Actor with FireCloudR
   override def receive: Receive = {
 
     case UpdateProfile(userInfo: UserInfo, profile: Profile) =>
+      val parent = context.parent
       val pipeline = authHeaders(requestContext) ~> sendReceive
       val profilePropertyMap = profile.propertyValueMap
-      val posts: Iterable[Future[HttpResponse]] = profilePropertyMap map {
+      val propertyPosts = profilePropertyMap map {
         case (key, value) =>
           val kv = FireCloudKeyValue(Some(key), Some(value))
           pipeline {
             Post(UserService.remoteSetKeyURL, ThurloeKeyValue(Some(userInfo.getUniqueId), Some(kv)))
           }
       }
-      val propertyUpdates: Future[List[HttpResponse]] = Future sequence posts.toList
-
-      val kv2 = FireCloudKeyValue(Some("isRegistrationComplete"), Some("true"))
-      val completionUpdate = pipeline {
-        Post(UserService.remoteSetKeyURL, ThurloeKeyValue(Some(userInfo.getUniqueId), Some(kv2)))
-      }
-
-      propertyUpdates onComplete {
-        case Success(responses) if responses.forall { _.status == OK } =>
-          log.debug("Profile is saved, update isRegistrationComplete status.")
-          completionUpdate onComplete {
-            case Success(response) if response.status == OK =>
-              context.parent ! RequestComplete(OK, response.entity)
-              context stop self
-            case Success(response) =>
-              context.parent ! RequestCompleteWithErrorReport(response.status, response.toString)
-              context stop self
-            case _ =>
-              context.parent ! RequestCompleteWithErrorReport(InternalServerError,
-                "Failed to save registration completion status: [%s]".format(profilePropertyMap))
-              context stop self
-          }
-
-        case Success(responses) =>
-          val errors = responses.filterNot(_.status == OK) map { e => (e, ErrorReport.tryUnmarshal(e) ) }
-          val errorReports = errors collect { case (_, Success(report)) => report }
-          val missingReports = errors collect { case (originalError, Failure(_)) => originalError }
-          val errorMessage = {
-            val baseMessage = "%d failures out of %d attempts saving profile.  Errors: %s".format(profilePropertyMap.size, errors.size, errors mkString ",")
-            if (missingReports.isEmpty) baseMessage
-            else {
-              val supplementalErrorMessage = "Additionally, %d of these failures did not provide error reports: %s".format(missingReports.size, missingReports mkString ",")
-              baseMessage + "\n" + supplementalErrorMessage
+      val propertyUpdates: Future[List[HttpResponse]] = Future sequence propertyPosts.toList
+      val updatedPropertyResponse: Future[PerRequestMessage] = propertyUpdates flatMap { responses =>
+        val allSucceeded = responses.forall { _.status == OK }
+        allSucceeded match {
+          case true =>
+            val kv2 = FireCloudKeyValue(Some("isRegistrationComplete"), Some("true"))
+            val completionUpdate = pipeline {
+              Post(UserService.remoteSetKeyURL, ThurloeKeyValue(Some(userInfo.getUniqueId), Some(kv2)))
             }
-          }
-          context.parent ! RequestCompleteWithErrorReport(InternalServerError, errorMessage, errorReports)
-          context stop self
-        case _ =>
-          context.parent ! RequestCompleteWithErrorReport(InternalServerError, "Failure saving profile: [%s]".format(profilePropertyMap))
-          context stop self
-      }
+            completionUpdate.map { response =>
+              response.status match {
+                case OK => RequestComplete(OK, response.entity)
+                case _ => RequestCompleteWithErrorReport(response.status, response.toString)
+              }
+            } recoverWith { case e: Throwable => Future(RequestCompleteWithErrorReport(InternalServerError, e.getMessage)) }
+          case false =>
+            val errors = responses.filterNot(_.status == OK) map { e => (e, ErrorReport.tryUnmarshal(e) ) }
+            val errorReports = errors collect { case (_, Success(report)) => report }
+            val missingReports = errors collect { case (originalError, Failure(_)) => originalError }
+            val errorMessage = {
+              val baseMessage = "%d failures out of %d attempts saving profile.  Errors: %s"
+                .format(profilePropertyMap.size, errors.size, errors mkString ",")
+              if (missingReports.isEmpty) baseMessage
+              else {
+                val supplementalErrorMessage = "Additionally, %d of these failures did not provide error reports: %s"
+                  .format(missingReports.size, missingReports mkString ",")
+                baseMessage + "\n" + supplementalErrorMessage
+              }
+            }
+            Future(RequestCompleteWithErrorReport(InternalServerError, errorMessage, errorReports))
+        }
+      } recover { case e: Throwable => RequestCompleteWithErrorReport(InternalServerError, e.getMessage) }
+
+      updatedPropertyResponse pipeTo context.parent
 
   }
 
