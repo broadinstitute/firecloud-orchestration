1d7de8c30b1daf6e2754fa69f6aaab47d2887980
Greg Rushton <grushton@broadinstitute.org>
2016-02-29 11:58:14 -0500

    Update the set handling semantics. Fix a bug where empty values were not in the download.

--- src/main/scala/org/broadinstitute/dsde/firecloud/core/ExportEntitiesByType.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/core/ExportEntitiesByType.scala
@@ -10,7 +10,7 @@ import akka.pattern.pipe
 import org.broadinstitute.dsde.firecloud.core.ExportEntitiesByType.ProcessEntities
 import org.broadinstitute.dsde.firecloud.core.GetEntitiesWithType.EntityWithType
 import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol._
-import org.broadinstitute.dsde.firecloud.model.{RequestCompleteWithErrorReport, ErrorReport}
+import org.broadinstitute.dsde.firecloud.model.{ModelSchema, RequestCompleteWithErrorReport, ErrorReport}
 import org.broadinstitute.dsde.firecloud.service.FireCloudRequestBuilding
 import org.broadinstitute.dsde.firecloud.service.PerRequest.{RequestComplete, RequestCompleteWithHeaders}
 import org.broadinstitute.dsde.firecloud.utils.TSVFormatter
@@ -24,7 +24,7 @@ import spray.json.DefaultJsonProtocol._
 import spray.routing.RequestContext
 
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
+import scala.util.{Try, Failure, Success}
 
 object ExportEntitiesByType {
   case class ProcessEntities(baseEntityUrl: String, filename: String, entityType: String)
@@ -40,28 +40,18 @@ class ExportEntitiesByTypeActor(requestContext: RequestContext) extends Actor wi
   override def receive: Receive = {
     case ProcessEntities(baseEntityUrl: String, filename: String, entityType: String) =>
       val pipeline = authHeaders(requestContext) ~> sendReceive
-
-      // If we have a set type of entity, we need to make multiple calls for each data set.
-      val entityUrls = entityType match {
-        case x if x.endsWith("_set") => List(baseEntityUrl + "/" + entityType, baseEntityUrl + "/" + x.replace("_set", ""))
-        case _ => List(baseEntityUrl + "/" + entityType)
-      }
-
-      val urlsFuture: Future[List[HttpResponse]] = Future sequence { entityUrls map { url => pipeline { Get(url) } } }
-
-      urlsFuture map {
-        responses =>
-          val allSucceeded = responses.forall(_.status == OK)
-          allSucceeded match {
-            case true =>
-              val entities = responses.flatMap(unmarshal[List[EntityWithType]].apply)
-              entityType match {
-                case x if x.endsWith("_set") =>
-                  val rootType = x.replace("_set", "")
-                  val zippedFileName = x + ".zip"
-                  val rootData = TSVFormatter.makeEntityTsvString(entities, rootType)
-                  val membershipData = TSVFormatter.makeMembershipTsvString(entities, entityType)
-                  val zipBytes: Array[Byte] = getZipBytes(filename, membershipData, rootType, rootData)
+      pipeline { Get(baseEntityUrl + "/" + entityType) } map {
+        response =>
+          response.status match {
+            case OK =>
+              val entities = unmarshal[List[EntityWithType]].apply(response)
+              ModelSchema.getCollectionMemberType(entityType) match {
+                case Success(collectionType) if collectionType.isDefined =>
+                  val collectionMemberType = ModelSchema.getPlural(collectionType.get)
+                  val entityData = TSVFormatter.makeEntityTsvString(entities, entityType)
+                  val membershipData = TSVFormatter.makeMembershipTsvString(entities, entityType, collectionMemberType.get)
+                  val zipBytes: Array[Byte] = getZipBytes(entityType, membershipData, entityData)
+                  val zippedFileName = entityType + ".zip"
                   RequestCompleteWithHeaders(
                     HttpEntity(ContentTypes.`application/octet-stream`, zipBytes),
                     HttpHeaders.`Content-Disposition`.apply("attachment", Map("filename" -> zippedFileName)))
@@ -73,34 +63,21 @@ class ExportEntitiesByTypeActor(requestContext: RequestContext) extends Actor wi
                     HttpHeaders.`Content-Type`(`text/plain`))
               }
             case _ =>
-              val errors = responses.filterNot(_.status == OK) map { e => (e, ErrorReport.tryUnmarshal(e)) }
-              val errorReports = errors collect { case (_, Success(report)) => report }
-              val missingReports = errors collect { case (originalError, Failure(_)) => originalError }
-              val errorMessage = {
-                val baseMessage = "%d failures out of %d attempts retrieving entity urls.  Errors: %s"
-                  .format(errors.size, responses.size, errors mkString ",")
-                if (missingReports.isEmpty) baseMessage
-                else {
-                  val supplementalErrorMessage = "Additionally, %d of these failures did not provide error reports: %s"
-                    .format(missingReports.size, missingReports mkString ",")
-                  baseMessage + "\n" + supplementalErrorMessage
-                }
-              }
-              RequestCompleteWithErrorReport(InternalServerError, errorMessage, errorReports)
+              RequestCompleteWithErrorReport(response.status, response.entity.asString)
           }
       } recover { case e:Throwable =>  RequestCompleteWithErrorReport(InternalServerError, e.getMessage) } pipeTo context.parent
     case _ =>
       Future(RequestComplete(StatusCodes.BadRequest)) pipeTo context.parent
   }
 
-  private def getZipBytes(filename: String, membershipData: String, rootType: String, rootData: String): Array[Byte] = {
+  private def getZipBytes(entityType: String, membershipData: String, entityData: String): Array[Byte] = {
     val bos = new ByteArrayOutputStream()
     val zos = new ZipOutputStream(bos)
-    zos.putNextEntry(new ZipEntry(filename))
+    zos.putNextEntry(new ZipEntry(entityType + "_membership.txt"))
     zos.write(membershipData.getBytes)
     zos.closeEntry()
-    zos.putNextEntry(new ZipEntry(rootType + ".txt"))
-    zos.write(rootData.getBytes)
+    zos.putNextEntry(new ZipEntry(entityType + "_entity.txt"))
+    zos.write(entityData.getBytes)
     zos.closeEntry()
     zos.finish()
     bos.toByteArray
--- src/main/scala/org/broadinstitute/dsde/firecloud/utils/TSVFormatter.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/utils/TSVFormatter.scala
@@ -4,27 +4,36 @@ import org.broadinstitute.dsde.firecloud.core.GetEntitiesWithType.EntityWithType
 import org.broadinstitute.dsde.firecloud.model.ModelSchema
 
 import scala.collection.immutable
-import scala.util.Try
+import scala.util.{Success, Try}
 
 import spray.json.DefaultJsonProtocol._
 import spray.json.JsValue
 
 object TSVFormatter {
 
-  def makeMembershipTsvString(entities: List[EntityWithType], entityType: String): String = {
+  def makeMembershipTsvString(entities: List[EntityWithType], entityType: String, collectionMemberType: String): String = {
     val headers: immutable.IndexedSeq[String] = immutable.IndexedSeq("membership:" + entityType + "_id", entityType.replace("_set", "_id"))
-    val rows: List[IndexedSeq[String]] = entities filter { _.entityType == entityType } flatMap { entity =>
-      entity.attributes.getOrElse(Map.empty) flatMap { m =>
-        m._2.convertTo[List[JsValue]] map { jsValue =>
-          val cellValue: String = jsValue match {
-            case x if Try(x.asJsObject.fields.contains("entityName")).isSuccess =>
-              cleanValue(x.asJsObject.fields.getOrElse("entityName", x))
-            case _ =>
-              ""
-          }
-          IndexedSeq[String](entity.name, cellValue)
+    val rows: List[IndexedSeq[String]] = entities.filter { _.entityType == entityType }.flatMap {
+      entity =>
+        entity.attributes.getOrElse(Map.empty).filter {
+          // To make the membership file, we need the array of elements that correspond to the set type.
+          // All other top-level properties are not necessary and are only used for the data load file.
+          attribute => attribute._1.equals(collectionMemberType) }.flatMap {
+          m =>
+            Try(m._2.convertTo[List[JsValue]]) match {
+              case Success(values) =>
+                values map { jsValue =>
+                  val cellValue: String = jsValue match {
+                    case x if Try(x.asJsObject.fields.contains("entityName")).isSuccess =>
+                      cleanValue(x.asJsObject.fields.getOrElse("entityName", x))
+                    case _ =>
+                      throw new RuntimeException("TSV formatting error, member entity is incorrectly formatted: " + jsValue.toString())
+                  }
+                  IndexedSeq[String](entity.name, cellValue)
+                }
+              case _ => IndexedSeq.empty
+            }
         }
-      }
     }
     exportToString(headers, rows.toIndexedSeq)
   }
@@ -32,14 +41,26 @@ object TSVFormatter {
   def makeEntityTsvString(entities: List[EntityWithType], entityType: String): String = {
     val headerRenamingMap: Map[String, String] = ModelSchema.getAttributeExportRenamingMap(entityType).getOrElse(Map.empty[String, String])
     val entityHeader = "entity:" + entityType + "_id"
-    val headers: immutable.IndexedSeq[String] = entityHeader +: entities.
+    // if we have a set entity, we need to filter out the attribute array of the members so that we only
+    // have top-level attributes to construct columns from.
+    val memberType = ModelSchema.getCollectionMemberType(entityType)
+    val filteredEntities = memberType match {
+      case Success(collectionType) if collectionType.isDefined =>
+        ModelSchema.getPlural(collectionType.get) match {
+          case Success(attributeName) => filterAttributeFromEntities(entities, attributeName)
+          case _ => entities
+        }
+      case _ => entities
+    }
+
+    val headers: immutable.IndexedSeq[String] = entityHeader +: filteredEntities.
       filter { _.entityType == entityType }.
       map { _.attributes.getOrElse(Map.empty) }.
       flatMap(_.keySet).
       distinct.
       map { key => headerRenamingMap.getOrElse(key, key) }.
       toIndexedSeq
-    val rows: immutable.IndexedSeq[IndexedSeq[String]] = entities.filter { _.entityType == entityType }
+    val rows: immutable.IndexedSeq[IndexedSeq[String]] = filteredEntities.filter { _.entityType == entityType }
       .map { entity =>
         makeRow(entity, headers, headerRenamingMap)
       }.toIndexedSeq
@@ -53,12 +74,28 @@ object TSVFormatter {
   }
 
   /**
-   * Generate a row of values in the same order as the headers.
-   *
-   * @param entity The EntityWithType object to extract data from
-   * @param headerValues List of ordered header values to determine order of values
-   * @return IndexedSeq of ordered data fields
-   */
+    * New list of EntityWithType objects with specified attribute filtered out.
+    *
+    * @param entities Initial list of EntityWithType
+    * @return new list of EntityWithType
+    */
+  private def filterAttributeFromEntities(entities: List[EntityWithType], attributeName: String): List[EntityWithType] = {
+    entities map {
+      entity =>
+        val attributes = entity.attributes.getOrElse(Map.empty) filterNot {
+          attribute => attribute._1.equals(attributeName)
+        }
+        EntityWithType(name = entity.name, entityType = entity.entityType, attributes = Some(attributes))
+    }
+  }
+
+  /**
+    * Generate a row of values in the same order as the headers.
+    *
+    * @param entity The EntityWithType object to extract data from
+    * @param headerValues List of ordered header values to determine order of values
+    * @return IndexedSeq of ordered data fields
+    */
   private def makeRow(entity: EntityWithType, headerValues: IndexedSeq[String],
     headerRenamingMap:Map[String, String]): IndexedSeq[String] = {
     val rowMap: Map[Int, String] =  entity.attributes.getOrElse(Map.empty) map {
@@ -72,18 +109,27 @@ object TSVFormatter {
         }
         columnPosition -> cellValue
     }
-    // The rowMap manipulation sorts the position-value map by the key, converts it to a seq of
-    // tuples, pulls out the second element of the tuple (column value) resulting in a sorted seq
-    // of the original map values.
-    entity.name +: rowMap.toSeq.sortBy(_._1).map(_._2).toIndexedSeq
+    // If there are entities that don't have a value for which there is a known header, that will
+    // be missing in the row. Fill up those positions with empty strings in that case.
+    val completedRowMap: IndexedSeq[(Int, String)] =
+      IndexedSeq.range(1, headerValues.size).map {
+        i => (i, rowMap.getOrElse(i, ""))
+      }
+
+    // This rowMap manipulation:
+    //  1. sorts the position-value map by the key
+    //  2. converts it to a seq of tuples
+    //  3. pulls out the second element of the tuple (column value)
+    //  4. resulting in a seq of the column values sorted by the column position
+    entity.name +: completedRowMap.toSeq.sortBy(_._1).map(_._2).toIndexedSeq
   }
 
   /**
-   * JsValues are double-quoted. Need to remove them before putting them into a cell position
-   *
-   * @param value The JsValue to remove leading and trailing quotes from.
-   * @return Trimmed string value
-   */
+    * JsValues are double-quoted. Need to remove them before putting them into a cell position
+    *
+    * @param value The JsValue to remove leading and trailing quotes from.
+    * @return Trimmed string value
+    */
   private def cleanValue(value: JsValue): String = {
     val regex = "^\"|\"$".r
     regex.replaceAllIn(value.toString(), "")
