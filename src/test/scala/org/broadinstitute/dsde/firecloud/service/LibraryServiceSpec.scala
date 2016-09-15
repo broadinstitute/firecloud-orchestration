b9fd72507f5298eb6a713a74ea06ae09c166cc6f
David An <davidan@broadinstitute.org>
2016-09-15 17:25:46 -0400

    add missing test: Library schema is valid json

--- src/test/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSpec.scala
@@ -5,8 +5,12 @@ import org.broadinstitute.dsde.firecloud.mock.MockUtils._
 import org.mockserver.integration.ClientAndServer
 import org.mockserver.integration.ClientAndServer._
 import org.mockserver.model.HttpRequest._
+import org.parboiled.common.FileUtils
+import scala.util.Try
 import spray.http.HttpMethods
 import spray.http.StatusCodes._
+import spray.json._
+import spray.util._
 
 class LibraryServiceSpec extends ServiceSpec with LibraryService {
 
@@ -35,7 +39,7 @@ class LibraryServiceSpec extends ServiceSpec with LibraryService {
     /* Handle passthrough handlers here */
 
     "when calling the isCurator endpoint" - {
-      s"PUT, POST, DELETE on /api/library/user/role/curator" - {
+      "PUT, POST, DELETE on /api/library/user/role/curator" - {
         "should receive a MethodNotAllowed" in {
           List(HttpMethods.PUT, HttpMethods.POST, HttpMethods.DELETE) map {
             method =>
@@ -47,6 +51,22 @@ class LibraryServiceSpec extends ServiceSpec with LibraryService {
         }
       }
     }
+
+    "in its schema definition" - {
+      "has valid JSON" in {
+        val classLoader = actorSystem(actorRefFactory).dynamicAccess.classLoader
+        val inputStream = classLoader.getResource("library/attribute-definitions.json").openStream()
+        try {
+          val fileContents = FileUtils.readAllText(inputStream)
+          val jsonVal:Try[JsValue] = Try(fileContents.parseJson)
+          assert(jsonVal.isSuccess)
+        } finally {
+          inputStream.close()
+        }
+      }
+    }
+
+
 
   }
 
