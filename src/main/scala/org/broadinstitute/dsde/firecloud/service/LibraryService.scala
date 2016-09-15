8be6511c59c55c762dd6e3905d637258a1611527
David An <davidan@broadinstitute.org>
2016-09-15 16:57:10 -0400

    passthrough for isCurator rawls API

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/LibraryService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/LibraryService.scala
@@ -1,6 +1,8 @@
 package org.broadinstitute.dsde.firecloud.service
 
+import org.broadinstitute.dsde.firecloud.FireCloudConfig
 import org.slf4j.LoggerFactory
+import spray.http.HttpMethods
 import spray.http.StatusCodes._
 import spray.json._
 import spray.routing._
@@ -10,6 +12,8 @@ trait LibraryService extends HttpService with FireCloudDirectives {
   private implicit val executionContext = actorRefFactory.dispatcher
   lazy val log = LoggerFactory.getLogger(getClass)
 
+  lazy val rawlsCuratorUrl = FireCloudConfig.Rawls.authUrl + "/user/role/curator"
+
   val routes: Route =
     pathPrefix("schemas") {
       path("library-attributedefinitions-v1") {
@@ -17,6 +21,13 @@ trait LibraryService extends HttpService with FireCloudDirectives {
           withResourceFileContents("library/attribute-definitions.json") { jsonContents =>
             complete(OK, jsonContents)
           }
+        }
+      }
+    } ~
+    pathPrefix("api") {
+      pathPrefix("library") {
+        path("user" / "role" / "curator") {
+          passthrough(rawlsCuratorUrl, HttpMethods.GET)
         }
       }
     }
---
+++ src/test/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSpec.scala
@@ -0,0 +1,56 @@
+package org.broadinstitute.dsde.firecloud.service
+
+import org.broadinstitute.dsde.firecloud.mock.MockUtils
+import org.broadinstitute.dsde.firecloud.mock.MockUtils._
+import org.mockserver.integration.ClientAndServer
+import org.mockserver.integration.ClientAndServer._
+import org.mockserver.model.HttpRequest._
+import spray.http.HttpMethods
+import spray.http.StatusCodes._
+
+class LibraryServiceSpec extends ServiceSpec with LibraryService {
+
+  def actorRefFactory = system
+  var workspaceServer: ClientAndServer = _
+
+  lazy val isCuratorPath = "/api/library/user/role/curator"
+
+  override def beforeAll(): Unit = {
+
+    workspaceServer = startClientAndServer(workspaceServerPort)
+    workspaceServer
+      .when(request.withMethod("GET").withPath(isCuratorPath))
+      .respond(
+        org.mockserver.model.HttpResponse.response()
+          .withHeaders(MockUtils.header).withStatusCode(OK.intValue)
+      )
+  }
+
+  override def afterAll(): Unit = {
+    workspaceServer.stop()
+  }
+
+  "LibraryService" - {
+
+    /* Handle passthrough handlers here */
+
+    "when calling the isCurator endpoint" - {
+      s"PUT, POST, DELETE on /api/library/user/role/curator" - {
+        "should receive a MethodNotAllowed" in {
+          List(HttpMethods.PUT, HttpMethods.POST, HttpMethods.DELETE) map {
+            method =>
+              new RequestBuilder(method)(isCuratorPath) ~> dummyUserIdHeaders("1234") ~> sealRoute(routes) ~> check {
+                System.out.println(response)
+                status should equal(MethodNotAllowed)
+              }
+          }
+        }
+      }
+    }
+
+  }
+
+
+
+
+}
