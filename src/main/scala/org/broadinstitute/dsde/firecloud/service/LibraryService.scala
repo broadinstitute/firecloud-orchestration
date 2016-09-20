29af0934b02170b55c10bc16d9360b4abb0a1df8
David An <davidan@genomebridge.org>
2016-09-20 12:06:20 -0400

    GAWB-1063: refactor isCurator endpoint to return true/false (#254)
    refactor isCurator endpoint to return true/false

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/LibraryService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/LibraryService.scala
@@ -1,13 +1,15 @@
 package org.broadinstitute.dsde.firecloud.service
 
 import org.broadinstitute.dsde.firecloud.FireCloudConfig
+import org.broadinstitute.dsde.firecloud.model.Curator
+import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol.impCurator
 import org.slf4j.LoggerFactory
-import spray.http.HttpMethods
 import spray.http.StatusCodes._
-import spray.json._
+import spray.httpx.SprayJsonSupport._
+import spray.client.pipelining._
 import spray.routing._
 
-trait LibraryService extends HttpService with FireCloudDirectives {
+trait LibraryService extends HttpService with FireCloudDirectives with FireCloudRequestBuilding {
 
   private implicit val executionContext = actorRefFactory.dispatcher
   lazy val log = LoggerFactory.getLogger(getClass)
@@ -27,7 +29,16 @@ trait LibraryService extends HttpService with FireCloudDirectives {
     pathPrefix("api") {
       pathPrefix("library") {
         path("user" / "role" / "curator") {
-          passthrough(rawlsCuratorUrl, HttpMethods.GET)
+          get { requestContext =>
+            val pipeline = authHeaders(requestContext) ~> sendReceive
+            pipeline{Get(rawlsCuratorUrl)} map { response =>
+              response.status match {
+                case OK => requestContext.complete(OK, Curator(true))
+                case NotFound => requestContext.complete(OK, Curator(false))
+                case _ => requestContext.complete(response) // replay the root exception
+              }
+            }
+          }
         }
       }
     }
