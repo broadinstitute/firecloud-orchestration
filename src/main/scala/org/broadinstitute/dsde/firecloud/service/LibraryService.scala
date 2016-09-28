042fb8db407b188e504daf3b488b953a5037c825
David An <davidan@broadinstitute.org>
2016-09-28 15:43:08 -0400

    WIP 2

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/LibraryService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/LibraryService.scala
@@ -4,8 +4,9 @@ import akka.actor.{Actor, Props}
 import akka.pattern._
 import org.broadinstitute.dsde.firecloud.{Application, FireCloudConfig, FireCloudException, FireCloudExceptionWithErrorReport}
 import org.broadinstitute.dsde.firecloud.dataaccess.RawlsDAO
-import org.broadinstitute.dsde.firecloud.model.{AttributeString, ErrorReport, RawlsWorkspaceResponse, UserInfo}
+import org.broadinstitute.dsde.firecloud.model.{RequestCompleteWithErrorReport, _}
 import org.broadinstitute.dsde.firecloud.model.AttributeUpdateOperations._
+import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol.impRawlsWorkspaceResponse
 import org.broadinstitute.dsde.firecloud.service.LibraryService._
 import org.broadinstitute.dsde.firecloud.service.PerRequest.{PerRequestMessage, RequestComplete}
 import org.broadinstitute.dsde.firecloud.utils.RoleSupport
@@ -16,6 +17,7 @@ import spray.httpx.unmarshalling._
 import spray.httpx.PipelineException
 import spray.httpx.SprayJsonSupport._
 import spray.json._
+
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -33,7 +35,6 @@ object LibraryService {
 
   def constructor(app: Application)(userInfo: UserInfo)(implicit executionContext: ExecutionContext) =
     new LibraryService(userInfo, app.rawlsDAO)
-
 }
 
 class LibraryService (protected val userInfo: UserInfo, val rawlsDAO: RawlsDAO)(implicit protected val executionContext: ExecutionContext) extends Actor with RoleSupport {
@@ -45,48 +46,64 @@ class LibraryService (protected val userInfo: UserInfo, val rawlsDAO: RawlsDAO)(
   }
 
   def updateAttributes(ns: String, name: String, attrs: JsValue): Future[PerRequestMessage] = {
-
-    import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol.impRawlsWorkspaceResponse
-
+    // spray routing can only (easily) make a JsValue; we need to work with a JsObject
     val userAttrs = attrs.asJsObject
 
     // TODO: schema-validate user input
 
-    // TODO: factor out get workspace into own method
-    val workspacePipeline = addCredentials(userInfo.accessToken) ~> sendReceive ~> unmarshal[RawlsWorkspaceResponse]
-    val workspaceUrl = FireCloudConfig.Rawls.authUrl + FireCloudConfig.Rawls.workspacesPath + s"/%s/%s".format(ns, name)
-    workspacePipeline(Get(workspaceUrl)) map {workspaceResponse =>
-      // TODO: verify owner on workspace - factor out
+    getWorkspace(ns, name) flatMap { workspaceResponse =>
+      // verify owner on workspace
       if (!workspaceResponse.accessLevel.contains("OWNER")) {
-        throw new FireCloudExceptionWithErrorReport(errorReport=ErrorReport(spray.http.StatusCodes.Forbidden, "must be an owner"))
+        Future(RequestCompleteWithErrorReport(Forbidden, "must be an owner"))
       } else {
         val libraryAttrKeys = workspaceResponse.workspace.get.attributes.keySet.filter(_.startsWith("library:"))
 
         // remove any attributes that currently exist on the workspace, but are not in the user's packet
-        val keysToRemove = libraryAttrKeys.diff(userAttrs.fields.keySet)
-        // anything the user posted should be updated, so no extra calculation necessary
-
-        val removeOperations = (keysToRemove ++ userAttrs.fields.filter(_._2.isInstanceOf[JsArray]).keySet).map(RemoveAttribute(_))
+        // for any array attributes, we remove them and recreate them entirely. Add the array attrs.
+        val keysToRemove = libraryAttrKeys.diff(userAttrs.fields.keySet) ++ userAttrs.fields.filter(_._2.isInstanceOf[JsArray]).keySet
+        val removeOperations = keysToRemove.map(RemoveAttribute(_)).toSeq
 
         // TODO: handle numeric/boolean values
-        val updateOperations = userAttrs.fields.seq flatMap {
+        val updateOperations = userAttrs.fields.toSeq flatMap {
           // case (key, value:JsBoolean) => AddUpdateAttribute(key, AttributeString(value.toString()))
           // case (key, value:JsNumber) => AddUpdateAttribute(key, AttributeString(value.toString()))
           case (key, value:JsArray) => value.elements.map{x => AddUpdateAttribute(key, AttributeString(x.toString()))}
           case (key, value:JsValue) => Seq(AddUpdateAttribute(key, AttributeString(value.toString())))
         }
 
-        val allOperations = removeOperations ++ updateOperations
+        // handle removals before upserts
+        val allOperations: Seq[AttributeUpdateOperation] = removeOperations ++ updateOperations
 
-        // TODO: update attributes in rawls
+        // spray marshalling is so very sensitive to location of imports!
+        import spray.json.DefaultJsonProtocol._
+        import org.broadinstitute.dsde.firecloud.model.AttributeUpdateOperations.AttributeUpdateOperationFormat
 
-
-        // TODO: respond with updated workspace|updated attributes
-        RequestComplete(NotImplemented, "Still a work in progress")
+        // update attributes in rawls
+        val attrUpdatePipeline = addCredentials(userInfo.accessToken) ~> sendReceive
+        attrUpdatePipeline(Patch(getWorkspaceUrl(ns, name), allOperations)) flatMap { attrUpdateResponse =>
+          attrUpdateResponse.status match {
+            // respond with updated workspace|updated attributes
+            case OK =>
+              getWorkspace(ns, name) map {ws => RequestComplete(OK, ws)}
+              // RequestComplete(OK, getWorkspace(ns, name))
+              // RequestComplete(OK)
+            case x => Future(RequestCompleteWithErrorReport(x, "Could not update workspace", Seq(ErrorReport(attrUpdateResponse))))
+          }
+        }
       }
-    } recover {
-      case pe:PipelineException => throw new FireCloudException("Error with workspace", pe)
     }
+      /* recover {
+      case pe:PipelineException => RequestCompleteWithErrorReport(InternalServerError, "Error with workspace", pe)
+      case _ => RequestCompleteWithErrorReport(InternalServerError, "Unknown error")
+    } */
   }
+
+  // TODO: should be in a rawls dao, not library dao
+  private def getWorkspace(ns: String, name: String): Future[RawlsWorkspaceResponse] = {
+    val workspacePipeline = addCredentials(userInfo.accessToken) ~> sendReceive ~> unmarshal[RawlsWorkspaceResponse]
+    workspacePipeline(Get(getWorkspaceUrl(ns, name)))
+  }
+
+  private def getWorkspaceUrl(ns: String, name: String) = FireCloudConfig.Rawls.authUrl + FireCloudConfig.Rawls.workspacesPath + s"/%s/%s".format(ns, name)
 
 }
