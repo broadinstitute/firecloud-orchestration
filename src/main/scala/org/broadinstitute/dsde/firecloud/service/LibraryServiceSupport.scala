dfb6bd57fca7c6d8194fd391a9c951278c2bcb9f
David An <davidan@broadinstitute.org>
2016-10-17 14:15:17 -0400

    basic json-schema validation in library metadata endpoint

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSupport.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSupport.scala
@@ -62,6 +62,7 @@ trait LibraryServiceSupport {
     val rawSchema:JSONObject = new JSONObject(new JSONTokener(schemaStr))
     val schema:Schema = SchemaLoader.load(rawSchema)
     schema.validate(new JSONObject(data))
+
   }
 
 }
