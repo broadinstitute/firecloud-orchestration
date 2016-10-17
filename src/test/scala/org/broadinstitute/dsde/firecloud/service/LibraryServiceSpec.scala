a7201b2c479a987ec10b5aab7d7a61f5eeb425eb
David An <davidan@broadinstitute.org>
2016-10-17 16:26:43 -0400

    update unit tests to reflect namespaced schema

--- src/test/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSpec.scala
@@ -41,7 +41,6 @@ class LibraryServiceSpec extends FreeSpec with LibraryServiceSupport {
   val testLibraryMetadata =
     """
       |{
-<<<<<<< dfb6bd57fca7c6d8194fd391a9c951278c2bcb9f
       |  "description" : "some description",
       |  "userAttributeOne" : "one",
       |  "userAttributeTwo" : "two",
@@ -59,26 +58,6 @@ class LibraryServiceSpec extends FreeSpec with LibraryServiceSupport {
       |  "library:studyDesign" : "study",
       |  "library:cellType" : "cell",
       |  "library:technology" : ["is an optional","array attribute"]
-=======
-      |  "workspaceId" : "11111111-2222-3333-4444-555555555555",
-      |  "workspaceName" : "x",
-      |  "workspaceNamespace" : "x",
-      |  "library:foo" : "bar",
-      |  "library:baz" : "qux",
-      |  "datasetName" : "x",
-      |  "datasetDescription" : "x",
-      |  "datasetCustodian" : "x",
-      |  "datasetDepositor" : "x",
-      |  "datasetOwner" : "x",
-      |  "institute" : ["x"],
-      |  "indication" : "x",
-      |  "numSubjects" : 123,
-      |  "projectName" : "x",
-      |  "datatype" : ["x"],
-      |  "dataUseRestriction" : "x",
-      |  "studyDesign" : "x",
-      |  "cellType" : "x"
->>>>>>> unit tests for schema validation
       |}
     """.stripMargin
 
