eb3a59caa5a5cf0a25135b5e648feb14d314d6e1
Sarah Walker <skwalker@broadinstitute.org>
2016-10-21 15:58:34 -0400

    got mappings to go through to ES

--- src/main/scala/org/broadinstitute/dsde/firecloud/model/ESMapping.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/model/ESMapping.scala
@@ -1,8 +1,12 @@
 package org.broadinstitute.dsde.firecloud.model
 
 case class AttributeDefinition(properties: Map[String, AttributeDetail])
-case class AttributeDetail(`type`: String)
+case class AttributeDetail(`type`: String, items: Option[AttributeDetail]=None)
 
-case class ESMapping(mappings: Map[String, ESProperty])
-case class ESProperty(properties: ESDetail)
+case class ESMapping(mappings: ESDataset)
+case class ESDataset(dataset: ESDatasetProperty)
+case class ESDatasetProperty(properties: Map[String, Either[ESDetail, ESArray]])
+case class ESArray(properties: ESItem)
+case class ESItem(items: ESDetail)
 case class ESDetail(`type`: String)
+
