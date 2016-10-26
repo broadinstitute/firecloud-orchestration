0a9d287e24ebebad2e214bec362ecf1e29142f63
Sarah Walker <skwalker@broadinstitute.org>
2016-10-26 15:29:01 -0400

    responding to comments

--- src/main/scala/org/broadinstitute/dsde/firecloud/model/ESMapping.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/model/ESMapping.scala
@@ -3,8 +3,6 @@ package org.broadinstitute.dsde.firecloud.model
 case class AttributeDefinition(properties: Map[String, AttributeDetail])
 case class AttributeDetail(`type`: String, items: Option[AttributeDetail]=None)
 
-case class ESMapping(mappings: ESDataset)
-case class ESDataset(dataset: ESDatasetProperty)
 case class ESDatasetProperty(properties: Map[String, ESDetail])
 case class ESDetail(`type`: String)
 
--- src/test/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSpec.scala
@@ -392,7 +392,7 @@ class LibraryServiceSpec extends FreeSpec with LibraryServiceSupport with Elasti
       "works for string type" in {
         val label = "library:attr"
         val `type` = "string"
-        val expected = Map(label -> ESDetail(`type`))
+        val expected = label -> ESDetail(`type`)
         assertResult(expected) {
           detailFromAttribute(label, AttributeDetail(`type`))
         }
@@ -402,7 +402,7 @@ class LibraryServiceSpec extends FreeSpec with LibraryServiceSupport with Elasti
         val `type` = "array"
         val subtype = "string"
         val detail = AttributeDetail(`type`, Some(AttributeDetail(subtype)))
-        val expected = Map(label -> ESDetail(subtype))
+        val expected = label -> ESDetail(subtype)
         assertResult(expected) {
           detailFromAttribute(label, detail)
         }
