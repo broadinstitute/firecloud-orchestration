7f7f62bcf414838f09b6650c93a8c040dbe29038
Andrea Haessly <ahaessly@broadinstitute.org>
2016-11-16 16:49:47 -0500

    if search string is the empty string, do a match all

--- src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ElasticSearchDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ElasticSearchDAO.scala
@@ -101,7 +101,7 @@ class ElasticSearchDAO(servers:Seq[Authority], indexName: String) extends Search
   override def findDocuments(criteria: LibrarySearchParams) : LibrarySearchResponse = {
     val searchStr =
     criteria.searchTerm match {
-      case None => ESQuery(new ESMatchAll).toJson.compactPrint
+      case None | Some("") => ESQuery(new ESMatchAll).toJson.compactPrint
       case Some(searchTerm:String) => ESQuery(new ESWildcard(searchTerm)).toJson.compactPrint
     }
     val searchReq = client.prepareSearch(indexName).setQuery(searchStr)
