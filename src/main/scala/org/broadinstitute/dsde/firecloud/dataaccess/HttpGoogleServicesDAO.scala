f3d554b0cbc9c3786f83e28e32a431b201f317c4
Brian Reilly <breilly@broadinstitute.org>
2016-11-17 11:34:31 -0500

    Added Gzip decoding of price list response.

--- src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/HttpGoogleServicesDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/HttpGoogleServicesDAO.scala
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory
 import spray.client.pipelining._
 import spray.http.StatusCodes._
 import spray.http._
+import spray.httpx.encoding.Gzip
 import spray.json._
 import spray.routing.RequestContext
 
@@ -302,7 +303,7 @@ object HttpGoogleServicesDAO extends FireCloudRequestBuilding {
 
   /** Fetch the latest price list from Google. Returns only the subset of prices that we find we have use for. */
   def fetchPriceList(implicit actorRefFactory: ActorRefFactory, executionContext: ExecutionContext): Future[GooglePriceList] = {
-    val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
+    val pipeline: HttpRequest => Future[HttpResponse] = sendReceive ~> decode(Gzip)
     val response: Future[HttpResponse] = pipeline(Get(FireCloudConfig.GoogleCloud.priceListUrl))
     response map { r => r.entity.asString.parseJson.convertTo[GooglePriceList] }
   }
