905c02f2eb7b55e72aeccfd2f335c1ba9cab5165
Andrea Haessly <ahaessly@broadinstitute.org>
2016-11-30 17:28:04 -0500

    partial implementation of multiple requests for correct aggregate info

--- src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ElasticSearchDAOQuerySupport.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ElasticSearchDAOQuerySupport.scala
@@ -3,13 +3,22 @@ package org.broadinstitute.dsde.firecloud.dataaccess
 import org.broadinstitute.dsde.firecloud.model._
 import spray.json._
 import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol._
-import org.elasticsearch.action.search.SearchRequestBuilder
-import org.elasticsearch.search.aggregations.AggregationBuilders
+import org.elasticsearch.search.aggregations.{AggregationBuilders, Aggregations}
+import org.elasticsearch.search.aggregations.bucket.terms.Terms
+import org.elasticsearch.client.transport.TransportClient
+import org.elasticsearch.action.search.{SearchRequest, SearchRequestBuilder, SearchResponse}
+import org.elasticsearch.search.aggregations.bucket.terms.Terms
+
+import scala.collection.JavaConverters._
+import spray.json.DefaultJsonProtocol._
+
+import scala.collection.mutable
+
 
 /**
   * Created by ahaessly on 11/28/16.
   */
-trait ElasticSearchDAOQuerySupport {
+trait ElasticSearchDAOQuerySupport extends ElasticSearchDAOSupport {
 
   def createESMatch(data: Map[String, Seq[String]]): Seq[QueryMap] = {
     (data map {
@@ -30,25 +39,90 @@ trait ElasticSearchDAOQuerySupport {
     ESShould(clauses)
   }
 
-  def createQueryString(criteria: LibrarySearchParams): String = {
-    val qmseq: Seq[QueryMap] = (criteria.searchString, criteria.searchFields.size) match {
+  def createQuery(criteria: LibrarySearchParams): Seq[QueryMap] = {
+    (criteria.searchString, criteria.searchFields.size) match {
       case (None | Some(""), 0) => Seq(new ESMatchAll)
       case (None | Some(""), _) => createListOfMusts(criteria.searchFields)
       case (Some(searchTerm: String), 0) => Seq(new ESMatch(searchTerm.toLowerCase))
       case (Some(searchTerm: String), _) => createListOfMusts(criteria.searchFields) :+ new ESMatch(searchTerm.toLowerCase)
     }
-
-    ESQuery(ESConstantScore(ESFilter(ESBool(ESMust(qmseq))))).toJson.compactPrint
   }
 
-  def addAggregations(searchReq: SearchRequestBuilder, criteria: LibrarySearchParams) = {
-    criteria.fieldAggregations map { field: String =>
+  def addAggregations(searchReq: SearchRequestBuilder, aggFields: Seq[String], maxAggs: Option[Int]): SearchRequestBuilder = {
+    aggFields foreach { field: String =>
       val terms = AggregationBuilders.terms(field)
-      if (None != criteria.maxAggregations) {
-        terms.size(criteria.maxAggregations.get)
+      if (None != maxAggs) {
+        terms.size(maxAggs.get)
       }
       searchReq.addAggregation(terms.field(field + ".raw"))
     }
+    searchReq
   }
 
+  def queryToJsonQueryString(qmseq: Seq[QueryMap]): String = {
+    ESQuery(ESConstantScore(ESFilter(ESBool(ESMust(qmseq))))).toJson.compactPrint
+  }
+
+  def createESSearchRequest(client: TransportClient, indexname: String, qmseq: Seq[QueryMap], from: Int, size: Int): SearchRequestBuilder = {
+    client.prepareSearch(indexname)
+      .setQuery(queryToJsonQueryString(qmseq))
+      .setFrom(from)
+      .setSize(size)
+  }
+
+  def buildMainQuery(client: TransportClient, indexname: String, baseQuery: Seq[QueryMap], criteria: LibrarySearchParams): SearchRequestBuilder = {
+    val mainQuery = createESSearchRequest(client, indexname, baseQuery, criteria.from, criteria.size)
+    if (criteria.fieldAggregations.size != 0 && criteria.fieldAggregations.size != criteria.searchFields.size) {
+      // if we are not collecting aggregation data (in the case of pagination)
+      // and if there are some aggregations that are not part of the search criteria
+      // then the aggregation data for those fields will be accurate from the standard search
+      addAggregations(mainQuery, criteria.fieldAggregations.diff(criteria.searchFields.keySet.toIndexedSeq), criteria.maxAggregations)
+    }
+    mainQuery
+  }
+
+
+  def buildAggregateQueries(client: TransportClient, indexname: String, baseQuery: Seq[QueryMap], criteria: LibrarySearchParams): Map[String, SearchRequestBuilder] = {
+    // for aggregtions fields that are part of the current search criteria, we need to do a separate
+    // aggregate request *without* that term in the search criteria
+    (criteria.searchFields.keys map { field =>
+      val query = baseQuery.filterNot((m: QueryMap) => m.isInstanceOf[ESShould] && m.asInstanceOf[ESShould].should.last.isInstanceOf[ESTerm]
+        && m.asInstanceOf[ESShould].should.last.asInstanceOf[ESTerm].term.last._1.equals(field))
+      // setting size to 0, we will ignore the actual search results
+      (field -> addAggregations(createESSearchRequest(client, indexname, query, 0, 0), Seq(field), criteria.maxAggregations))
+    }).toMap
+  }
+
+  def getAggregationsFromResults(aggResults: Aggregations): Seq[LibraryAggregationResponse] = {
+    aggResults.getAsMap.keySet().asScala.toSeq map { field: String =>
+      val terms: Terms = aggResults.get(field)
+      LibraryAggregationResponse(terms.getName(),
+        AggregationFieldResults(terms.getSumOfOtherDocCounts.toInt,
+          terms.getBuckets.asScala.toSeq map { bucket: Terms.Bucket =>
+            AggregationTermResult(bucket.getKey.toString, bucket.getDocCount.toInt)
+          }))
+    }
+  }
+
+
+  def findDocumentsWithAggregateInfo(client: TransportClient, indexname: String, criteria: LibrarySearchParams): LibrarySearchResponse = {
+    val baseQuery = createQuery(criteria)
+    val mainQuery = buildMainQuery(client, indexname, baseQuery, criteria)
+    val aggregateQueries = buildAggregateQueries(client, indexname, baseQuery, criteria)
+    //    executeQueries()
+    //    buildResponse()
+
+
+    val searchResults = executeESRequest[SearchRequest, SearchResponse, SearchRequestBuilder](mainQuery)
+
+    val sourceDocuments = searchResults.getHits.getHits.toList map { hit =>
+      hit.getSourceAsString.parseJson
+    }
+
+    val aggResults: scala.collection.mutable.ArraySeq[LibraryAggregationResponse] = mutable.ArraySeq(getAggregationsFromResults(searchResults.getAggregations()):_*)
+//    aggResults +: getAggregationsFromResults(searchResults.getAggregations())
+
+    new LibrarySearchResponse(criteria, searchResults.getHits.totalHits().toInt, sourceDocuments, aggResults)
+  }
 }
+
