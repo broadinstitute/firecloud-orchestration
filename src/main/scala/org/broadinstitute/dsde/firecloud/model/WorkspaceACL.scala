4a87b1cf12f1a14b133642d18c72ce0478a24b5c
Matt Bemis <mbemis@broadinstitute.org>
2017-01-23 15:47:07 -0500

    update acl response model

--- src/main/scala/org/broadinstitute/dsde/firecloud/model/WorkspaceACL.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/model/WorkspaceACL.scala
@@ -16,7 +16,7 @@ case class WorkspaceACLUpdate(email: String, accessLevel: WorkspaceAccessLevel, 
 
 case class WorkspaceACLUpdateResponse(subjectId: String, accessLevel: WorkspaceAccessLevel)
 
-case class WorkspaceACLUpdateResponseList(usersUpdated: Seq[WorkspaceACLUpdateResponse], invitesUpdated: Seq[WorkspaceACLUpdate], usersNotFound: Seq[WorkspaceACLUpdate])
+case class WorkspaceACLUpdateResponseList(usersUpdated: Seq[WorkspaceACLUpdateResponse], invitesSent: Seq[WorkspaceACLUpdate], invitesUpdated: Seq[WorkspaceACLUpdate], usersNotFound: Seq[WorkspaceACLUpdate])
 
 object WorkspaceAccessLevels {
   sealed trait WorkspaceAccessLevel extends Ordered[WorkspaceAccessLevel] {
@@ -86,5 +86,5 @@ object WorkspaceACLJsonSupport {
 
   implicit val WorkspaceACLUpdateResponseFormat = jsonFormat2(WorkspaceACLUpdateResponse)
 
-  implicit val WorkspaceACLUpdateResponseListFormat = jsonFormat3(WorkspaceACLUpdateResponseList)
+  implicit val WorkspaceACLUpdateResponseListFormat = jsonFormat4(WorkspaceACLUpdateResponseList)
 }
