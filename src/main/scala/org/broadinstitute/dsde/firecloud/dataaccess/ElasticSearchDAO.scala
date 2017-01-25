3c3cf6be31b3b932fa3f8ae5e5f323e12f0051e3
Andrea Haessly <ahaessly@broadinstitute.org>
2017-01-25 17:41:23 -0500

    moved back and added lazy in the declaration

--- src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ElasticSearchDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ElasticSearchDAO.scala
@@ -22,34 +22,6 @@ class ElasticSearchDAO(servers: Seq[Authority], indexName: String) extends Searc
   private val client: TransportClient = buildClient(servers)
   private final val datatype = "dataset"
 
-  // see https://www.elastic.co/guide/en/elasticsearch/guide/current/_index_time_search_as_you_type.html
-  //  and https://qbox.io/blog/multi-field-partial-word-autocomplete-in-elasticsearch-using-ngrams
-  private final val analysisSettings=
-  """
-    |{
-    |	"analysis": {
-    |		"filter": {
-    |			"autocomplete_filter": {
-    |				"type":     "edge_ngram",
-    |				"min_gram": 1,
-    |				"max_gram": 20
-    |			}
-    |		},
-    |		"analyzer": {
-    |			"autocomplete": {
-    |				"type":      "custom",
-    |				"tokenizer": "standard",
-    |				"filter": [
-    |					"lowercase",
-    |					"autocomplete_filter"
-    |				]
-    |			}
-    |		}
-    |	}
-    |}
-  """.stripMargin
-
-
   initIndex
 
   // if the index does not exist, create it.
@@ -137,5 +109,33 @@ class ElasticSearchDAO(servers: Seq[Authority], indexName: String) extends Searc
   override def suggest(criteria: LibrarySearchParams): Future[LibrarySearchResponse] = {
     autocompleteSuggestions(client, indexName, criteria)
   }
-  
+
+  // see https://www.elastic.co/guide/en/elasticsearch/guide/current/_index_time_search_as_you_type.html
+  //  and https://qbox.io/blog/multi-field-partial-word-autocomplete-in-elasticsearch-using-ngrams
+  // lazy is necessary here because we use it above
+  private final lazy val analysisSettings=
+  """
+    |{
+    |	"analysis": {
+    |		"filter": {
+    |			"autocomplete_filter": {
+    |				"type":     "edge_ngram",
+    |				"min_gram": 1,
+    |				"max_gram": 20
+    |			}
+    |		},
+    |		"analyzer": {
+    |			"autocomplete": {
+    |				"type":      "custom",
+    |				"tokenizer": "standard",
+    |				"filter": [
+    |					"lowercase",
+    |					"autocomplete_filter"
+    |				]
+    |			}
+    |		}
+    |	}
+    |}
+  """.stripMargin
+
 }
