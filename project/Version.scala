ab566884f32f013ab70ce416e16fbe2a719200a8
David An <davidan@broadinstitute.org>
2017-02-09 09:36:03 -0500

    refactor build.sbt and project/ to support integration tests

---
+++ project/Version.scala
@@ -0,0 +1,41 @@
+import sbt.Keys._
+import sbt._
+
+object Version {
+  val versionRoot = "0.1"
+
+  def getVersionString(baseDir: File) = {
+    // Get the revision, or -1 (later will be bumped to zero)
+    val versionRevision = {
+      try {
+        ("git rev-list --count HEAD" #|| "echo -1").!!.trim.toInt
+      } catch {
+        case e: Exception =>
+          0
+      }
+    }
+
+    // Set the suffix to None...
+    val versionSuffix = {
+      try {
+        // ...except when there are no modifications...
+        if ("git diff --quiet HEAD".! == 0) {
+          // ...then set the suffix to the revision "dash" git hash
+          Option(versionRevision + "-" + "git rev-parse --short HEAD".!!.trim)
+        } else {
+          None
+        }
+      } catch {
+        case e: Exception =>
+          None
+      }
+    }
+
+    versionRoot + "-" + versionSuffix.getOrElse((versionRevision + 1) + "-SNAPSHOT")
+
+  }
+
+  val rootVersionSettings: Seq[Setting[_]] =
+    Seq(version := getVersionString(baseDirectory.value))
+
+}
