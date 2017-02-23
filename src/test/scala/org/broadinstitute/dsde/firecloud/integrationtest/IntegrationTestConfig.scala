745e0b664e5bb73dec35a90d9678d1399183122d
David An <davidan@broadinstitute.org>
2017-02-23 13:09:56 -0500

    integration test config hierarchy

--- src/test/scala/org/broadinstitute/dsde/firecloud/integrationtest/IntegrationTestConfig.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/integrationtest/IntegrationTestConfig.scala
@@ -8,11 +8,22 @@ import spray.http.Uri.Authority
 
 trait IntegrationTestConfig {
   val unitTestConf = ConfigFactory.load()
-  val itTestConf = ConfigFactory.parseFile(new File("config/firecloud-orchestration.conf"))
+  val runtimeConf = ConfigFactory.parseFile(new File("config/firecloud-orchestration.conf"))
+  val itTestConf = ConfigFactory
+      .parseFile(new File("src/test/resources/ittest.conf"))
+      .withFallback(runtimeConf)
+      .withFallback(unitTestConf)
 
   object ITElasticSearch {
-    private val elasticsearch = itTestConf.getConfig("elasticsearch")
-    val servers: Seq[Authority] = FireCloudConfig.parseESServers(elasticsearch.getString("urls"))
+    // the list of ES servers to use for integration tests looks in this order:
+    //  - passed on command-line, e.g. "sbt -Desurls=foo it:test", which is what Jenkins does
+    //  - in src/test/resources/ittest.conf, which doesn't normally exist unless you put it there
+    //  - in config/firecloud-orchestration.conf, which usually exists for developers but won't exist in Jenkins
+    //  - in standard test conf, typically src/test/resources/reference.conf, which is unlikely to have real server urls
+    private val systemESUrls = System.getProperty("esurls",
+        itTestConf.getConfig("elasticsearch").getString("urls"))
+
+    val servers: Seq[Authority] = FireCloudConfig.parseESServers(systemESUrls)
   }
 
 }
