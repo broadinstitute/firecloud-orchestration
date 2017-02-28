f7eb309a4bc22b7801a849a95bbb39e57f95d8e5
Greg Rushton <grushton@broadinstitute.org>
2017-02-28 06:59:43 -0500

    test for overwriting published attribute on clone

--- src/test/scala/org/broadinstitute/dsde/firecloud/model/WorkspaceModelSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/model/WorkspaceModelSpec.scala
@@ -13,11 +13,19 @@ class WorkspaceModelSpec extends FreeSpec with Assertions {
       assert(!wc.isProtected.getOrElse(false))
     }
 
-    "Workspace Clone" in {
+    "Workspace Clone is unpublished" in {
       val clone = WorkspaceCreate.toWorkspaceClone(wc)
       assert(!clone.isProtected.getOrElse(false))
       assert(clone.attributes.nonEmpty)
       assert(clone.attributes.contains(AttributeName("library","published")))
+      val published: Option[Attribute] = clone.attributes.get(key = AttributeName("library", "published"))
+      assert(published.getOrElse(None).isInstanceOf[AttributeBoolean])
+      assert(!published.getOrElse(None).asInstanceOf[AttributeBoolean].value)
+    }
+
+    "Workspace with published attribute is unpublished when cloned" in {
+      val publishedWC = WorkspaceCreate("namespace", "name", Map(AttributeName("library","published") -> AttributeBoolean(true)))
+      val clone = WorkspaceCreate.toWorkspaceClone(publishedWC)
       val published: Option[Attribute] = clone.attributes.get(key = AttributeName("library", "published"))
       assert(published.getOrElse(None).isInstanceOf[AttributeBoolean])
       assert(!published.getOrElse(None).asInstanceOf[AttributeBoolean].value)
