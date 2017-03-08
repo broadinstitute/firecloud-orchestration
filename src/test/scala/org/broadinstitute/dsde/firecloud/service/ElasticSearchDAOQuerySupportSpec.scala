8b46e25107c117779323d4bab69373c0e9262a6f
David An <davidan@genomebridge.org>
2017-03-08 16:22:58 -0500

    expanded ES query unit tests (#394)
    expanded ES query unit tests

--- src/test/scala/org/broadinstitute/dsde/firecloud/service/ElasticSearchDAOQuerySupportSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/service/ElasticSearchDAOQuerySupportSpec.scala
@@ -26,83 +26,160 @@ class ElasticSearchDAOQuerySupportSpec extends FreeSpec with ElasticSearchDAOQue
   val client: TransportClient = buildClient(FireCloudConfig.ElasticSearch.servers)
 
   "ElasticSearchDAOQuerySupport" - {
-    "when createQuery is given a group for the current user" - {
-      "should include group in search query" in {
-        val baseRequest = buildSearchQuery(client, indexname, criteria, Seq("whitelistedgroup"))
-        val jsonRequest = getSearchRequestAsJson(baseRequest)
-        validateGroupTerm(jsonRequest, Some("whitelistedgroup"))
-      }
-    }
-    "when createQuery is given no groups for the current user" - {
-      "should not have groups in search query" in {
-        val baseRequest = buildSearchQuery(client, indexname, criteria, Seq.empty[String])
-        val jsonRequest = getSearchRequestAsJson(baseRequest)
-        validateGroupTerm(jsonRequest, None)
-      }
-    }
-    "when sorting by an explicit field and direction" - {
-      "sort field and direction should be present in query" in {
-        val sortField = Some("library:datasetName")
-        val sortDirection = Some("asc")
 
-        val sortCriteria = criteria.copy(sortField=sortField,sortDirection=sortDirection)
-        val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
-        val jsonRequest = getSearchRequestAsJson(baseRequest)
-        validateSortField(jsonRequest, sortField)
-        validateSortDirection(jsonRequest, sortDirection)
+    "discoverability" - {
+      "when createQuery is given a group for the current user" - {
+        "should include group in search query" in {
+          val baseRequest = buildSearchQuery(client, indexname, criteria, Seq("whitelistedgroup"))
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          validateGroupTerm(jsonRequest, Some("whitelistedgroup"))
+        }
+      }
+      "when createQuery is given no groups for the current user" - {
+        "should not have groups in search query" in {
+          val baseRequest = buildSearchQuery(client, indexname, criteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          validateGroupTerm(jsonRequest, None)
+        }
       }
     }
-    "when sorting by an explicit field without direction" - {
-      "sort field is present in query and sort direction is defaulted to asc" in {
-        val sortField = Some("library:datasetName")
 
-        val sortCriteria = criteria.copy(sortField=sortField,sortDirection=None)
-        val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
-        val jsonRequest = getSearchRequestAsJson(baseRequest)
-        validateSortField(jsonRequest, sortField)
-        validateSortDirection(jsonRequest, Some("asc"))
-      }
-    }
-    "when sorting by an unknown direction" - {
-      "sort field is present in query and sort direction is defaulted to asc" in {
-        val sortField = Some("library:datasetName")
+    "sorting" - {
+      "when sorting by an explicit field and direction" - {
+        "sort field and direction should be present in query" in {
+          val sortField = Some("library:datasetName")
+          val sortDirection = Some("asc")
 
-        val sortCriteria = criteria.copy(sortField=sortField,sortDirection=Some("unknown"))
-        val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
-        val jsonRequest = getSearchRequestAsJson(baseRequest)
-        validateSortField(jsonRequest, sortField)
-        validateSortDirection(jsonRequest, Some("asc"))
+          val sortCriteria = criteria.copy(sortField=sortField,sortDirection=sortDirection)
+          val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          validateSortField(jsonRequest, sortField)
+          validateSortDirection(jsonRequest, sortDirection)
+        }
+      }
+      "when sorting by an explicit field without direction" - {
+        "sort field is present in query and sort direction is defaulted to asc" in {
+          val sortField = Some("library:datasetName")
+
+          val sortCriteria = criteria.copy(sortField=sortField,sortDirection=None)
+          val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          validateSortField(jsonRequest, sortField)
+          validateSortDirection(jsonRequest, Some("asc"))
+        }
+      }
+      "when sorting by an unknown direction" - {
+        "sort field is present in query and sort direction is defaulted to asc" in {
+          val sortField = Some("library:datasetName")
+
+          val sortCriteria = criteria.copy(sortField=sortField,sortDirection=Some("unknown"))
+          val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          validateSortField(jsonRequest, sortField)
+          validateSortDirection(jsonRequest, Some("asc"))
+        }
+      }
+      "when specifying a sort order but no sort key" - {
+        "neither sort order nor sort key is present in query" in {
+          val sortCriteria = criteria.copy(sortField=None,sortDirection=Some("asc"))
+          val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          validateSortField(jsonRequest, None)
+          validateSortDirection(jsonRequest, None)
+        }
+      }
+      "when specifying neither sort order nor sort key" - {
+        "neither sort order nor sort key is present in query" in {
+          val sortCriteria = criteria.copy(sortField=None,sortDirection=None)
+          val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          validateSortField(jsonRequest, None)
+          validateSortDirection(jsonRequest, None)
+        }
       }
     }
-    "when specifying a sort order but no sort key" - {
-      "neither sort order nor sort key is present in query" in {
-        val sortCriteria = criteria.copy(sortField=None,sortDirection=Some("asc"))
-        val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
-        val jsonRequest = getSearchRequestAsJson(baseRequest)
-        validateSortField(jsonRequest, None)
-        validateSortDirection(jsonRequest, None)
+
+    "pagination" - {
+      "when specifying a page offset" - {
+        "page offset is present in query" in {
+          val offset = 23
+          val searchCriteria = criteria.copy(from=offset)
+          val baseRequest = buildSearchQuery(client, indexname, searchCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          assertResult(Some(offset)) {getFromValue(jsonRequest)}
+          assertResult(Some(10)) {getSizeValue(jsonRequest)}
+        }
+      }
+      "when omitting a page offset" - {
+        "page offset defaults to 0" in {
+          val baseRequest = buildSearchQuery(client, indexname, criteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          assertResult(Some(0)) {getFromValue(jsonRequest)}
+          assertResult(Some(10)) {getSizeValue(jsonRequest)}
+        }
+      }
+      "when specifying a page size" - {
+        "page size is present in query" in {
+          val pageSize = 46
+          val searchCriteria = criteria.copy(size=pageSize)
+          val baseRequest = buildSearchQuery(client, indexname, searchCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          assertResult(Some(0)) {getFromValue(jsonRequest)}
+          assertResult(Some(pageSize)) {getSizeValue(jsonRequest)}
+        }
+      }
+      "when omitting a page size" - {
+        "page size defaults to 10" in {
+          val baseRequest = buildSearchQuery(client, indexname, criteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          assertResult(Some(0)) {getFromValue(jsonRequest)}
+          assertResult(Some(10)) {getSizeValue(jsonRequest)}
+        }
+      }
+      "when specifying both page offset and page size" - {
+        "both page offset and page size are present in query" in {
+          val offset = 23
+          val pageSize = 46
+          val searchCriteria = criteria.copy(from=offset,size=pageSize)
+          val baseRequest = buildSearchQuery(client, indexname, searchCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          assertResult(Some(offset)) {getFromValue(jsonRequest)}
+          assertResult(Some(pageSize)) {getSizeValue(jsonRequest)}
+        }
       }
     }
-    "when specifying neither sort order nor sort key" - {
-      "neither sort order nor sort key is present in query" in {
-        val sortCriteria = criteria.copy(sortField=None,sortDirection=None)
-        val baseRequest = buildSearchQuery(client, indexname, sortCriteria, Seq.empty[String])
-        val jsonRequest = getSearchRequestAsJson(baseRequest)
-        validateSortField(jsonRequest, None)
-        validateSortDirection(jsonRequest, None)
+
+    "text search" - {
+      "when specifying text search" - {
+        "user criteria is present, searching against _all" in {
+          val searchTerm = "normcore kitsch mustache bespoke semiotics"
+          val searchCriteria = criteria.copy(searchString=Some(searchTerm))
+          val baseRequest = buildSearchQuery(client, indexname, searchCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          validateSearchTermAll(jsonRequest, searchTerm)
+        }
+      }
+      "when omitting text search" - {
+        "no search is present in query" in {
+          val searchCriteria = criteria.copy(searchString=None)
+          val baseRequest = buildSearchQuery(client, indexname, searchCriteria, Seq.empty[String])
+          val jsonRequest = getSearchRequestAsJson(baseRequest)
+          // when omitting search term, we have an empty "match_all" and the "bool" for discover mode
+          val arr = getMustArray(jsonRequest)
+          val matchAllClause = arr.elements.head.asJsObject
+          assertResult(Set("match_all"), "first element of must clause should be a match") {matchAllClause.fields.keySet}
+          assertResult(JsObject(Map.empty[String, JsValue])) {matchAllClause.fields("match_all").asJsObject}
+          // calling getMustBoolObject will validate it down to that level
+          getMustBoolObject(jsonRequest)
+        }
       }
     }
+
   }
 
-  // TODO: does text search criteria properly search against _all?
-  // TODO: does empty text search do the right thing?
   // TODO: do facet selections properly become term filters?
   // TODO: do facet requests properly become aggregations?
   // TODO: does an expanded facet properly expand?
-  // TODO: does pagination work?
-  // TODO: does page size work?
-
-
 
   def getSearchRequestAsJson(baseQuery:SearchRequestBuilder): JsObject = {
     baseQuery.internalBuilder().buildAsBytes(XContentType.JSON).toUtf8.parseJson.asJsObject
@@ -164,6 +241,32 @@ class ElasticSearchDAOQuerySupportSpec extends FreeSpec with ElasticSearchDAOQue
   }
 
   def validateGroupTerm(json:JsObject, expectedGroup:Option[String]) = {
+    val groupBoolClause = getMustBoolObject(json)
+    val groupbool = groupBoolClause.fields("bool")
+    expectedGroup match {
+      case Some(group) =>
+        assertResult(expectedDiscoverableGroup(group), "group criteria should include expected group name") {groupbool}
+      case None =>
+        assertResult(expectedNoDiscoverableGroups, "group criteria should be just the must-not-exists") {groupbool}
+    }
+  }
+
+  def validateSearchTermAll(json:JsObject, expectedTerm:String) = {
+    validateSearchTerm(json, expectedTerm, "_all")
+  }
+
+  def validateSearchTerm(json:JsObject, expectedTerm:String, expectedField:String) = {
+    val matchClause = getMustMatchObject(json)
+    assertResult(Set(expectedField), s"search clause should execute against only $expectedField") {matchClause.fields.keySet}
+    val searchCriteria = matchClause.fields(expectedField).asJsObject
+    assertResult(Set("query","type","minimum_should_match"), s"search criteria should have 'query','type','minimum_should_match'") {searchCriteria.fields.keySet}
+    assertResult(expectedTerm) {searchCriteria.fields("query").asInstanceOf[JsString].value}
+    assertResult("boolean") {searchCriteria.fields("type").asInstanceOf[JsString].value}
+    assertResult("2<67%") {searchCriteria.fields("minimum_should_match").asInstanceOf[JsString].value}
+
+  }
+
+  private def getMustArray(json:JsObject):JsArray = {
     getQuery(json) match {
       case Some(a:JsObject) =>
         assertResult(Set("bool"), "query should be an outer bool clause") {a.fields.keySet}
@@ -173,22 +276,25 @@ class ElasticSearchDAOQuerySupportSpec extends FreeSpec with ElasticSearchDAOQue
         must match {
           case arr:JsArray =>
             assertResult(2, "must clause should have two elements") {arr.elements.size}
-            val matchClause = arr.elements.head.asJsObject
-            assertResult(Set("match"), "first element of must clause should be a match") {matchClause.fields.keySet}
-            val groupBoolClause = arr.elements.tail.head.asJsObject
-            assertResult(Set("bool"), "second (and last) element of must clause should be a bool clause") {groupBoolClause.fields.keySet}
-            val groupbool = groupBoolClause.fields("bool")
-            expectedGroup match {
-              case Some(group) =>
-                assertResult(expectedDiscoverableGroup(group), "group criteria should include expected group name") {groupbool}
-              case None =>
-                assertResult(expectedNoDiscoverableGroups, "group criteria should be just the must-not-exists") {groupbool}
-            }
+            arr
           case _ => fail("must clause should be a JsArray")
         }
-
       case _ => fail("query was not a JsObject")
     }
+  }
+
+  private def getMustMatchObject(json:JsObject):JsObject = {
+    val arr = getMustArray(json)
+    val matchClause = arr.elements.head.asJsObject
+    assertResult(Set("match"), "first element of must clause should be a match") {matchClause.fields.keySet}
+    matchClause.fields("match").asJsObject
+  }
+
+  private def getMustBoolObject(json:JsObject):JsObject = {
+    val arr = getMustArray(json)
+    val groupBoolClause = arr.elements.tail.head.asJsObject
+    assertResult(Set("bool"), "second (and last) element of must clause should be a bool clause") {groupBoolClause.fields.keySet}
+    groupBoolClause
   }
 
   private val expectedNoDiscoverableGroups = """{
