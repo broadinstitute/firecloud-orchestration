3a3191e5f6a45e26cd9e5ac6ecb0fd3df4ea9530
David An <davidan@broadinstitute.org>
2017-03-09 17:20:31 -0500

    ontology models are tolerant of missing definitions

--- src/test/scala/org/broadinstitute/dsde/firecloud/dataaccess/MockOntologyDAO.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/dataaccess/MockOntologyDAO.scala
@@ -12,33 +12,33 @@ class MockOntologyDAO extends OntologyDAO {
       ontology="Disease",
       usable=true,
       label="central sleep apnea",
-      definition="A sleep apnea that is characterized by a malfunction of the basic neurological controls for breathing rate and the failure to give the signal to inhale, causing the individual to miss one or more cycles of breathing.",
+      definition=Some("A sleep apnea that is characterized by a malfunction of the basic neurological controls for breathing rate and the failure to give the signal to inhale, causing the individual to miss one or more cycles of breathing."),
       synonyms=Some(List("primary central sleep apnea")),
       parents=Some(List(
         TermParent(
           id="http://purl.obolibrary.org/obo/DOID_0050847",
           order=1,
           label="sleep apnea",
-          definition="A sleep disorder characterized by repeated cessation and commencing of breathing that repeatedly disrupts sleep."
+          definition=Some("A sleep disorder characterized by repeated cessation and commencing of breathing that repeatedly disrupts sleep.")
         ),
         TermParent(
           id="http://purl.obolibrary.org/obo/DOID_535",
           order=2,
           label="sleep disorder",
-          definition="A disease of mental health that involves disruption of sleep patterns.",
+          definition=Some("A disease of mental health that involves disruption of sleep patterns."),
           synonyms=Some(List("Non-organic sleep disorder"))
         ),
         TermParent(
           id="http://purl.obolibrary.org/obo/DOID_150",
           order=3,
           label="disease of mental health",
-          definition="A disease that involves a psychological or behavioral pattern generally associated with subjective distress or disability that occurs in an individual, and which are not a part of normal development or culture."
+          definition=Some("A disease that involves a psychological or behavioral pattern generally associated with subjective distress or disability that occurs in an individual, and which are not a part of normal development or culture.")
         ),
         TermParent(
           id="http://purl.obolibrary.org/obo/DOID_4",
           order=4,
           label="disease",
-          definition="A disease is a disposition (i) to undergo pathological processes that (ii) exists in an organism because of one or more disorders in that organism."
+          definition=Some("A disease is a disposition (i) to undergo pathological processes that (ii) exists in an organism because of one or more disorders in that organism.")
         )
       )))),
     "DOID_4" -> List(TermResource(
@@ -46,7 +46,7 @@ class MockOntologyDAO extends OntologyDAO {
       ontology="Disease",
       usable=true,
       label="disease",
-      definition="A disease is a disposition (i) to undergo pathological processes that (ii) exists in an organism because of one or more disorders in that organism."
+      definition=Some("A disease is a disposition (i) to undergo pathological processes that (ii) exists in an organism because of one or more disorders in that organism.")
     ))
   )
 
