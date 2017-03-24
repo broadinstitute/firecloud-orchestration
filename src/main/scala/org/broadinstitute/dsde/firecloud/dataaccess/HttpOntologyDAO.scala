7dd78a9353eae7af1dd8f42897b4f53c379193b4
David An <davidan@broadinstitute.org>
2017-03-24 15:52:41 -0400

    first pass at using host connector

--- src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/HttpOntologyDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/HttpOntologyDAO.scala
@@ -1,22 +1,30 @@
 package org.broadinstitute.dsde.firecloud.dataaccess
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
+import akka.io.IO
+import akka.pattern.ask
+import akka.util.Timeout
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.broadinstitute.dsde.firecloud.FireCloudConfig
 import org.broadinstitute.dsde.firecloud.model.Ontology.TermResource
 import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol._
 import org.broadinstitute.dsde.firecloud.utils.RestJsonClient
+import spray.can.Http
 import spray.client.pipelining._
 import spray.http.Uri
 import spray.httpx.SprayJsonSupport._
 import spray.httpx.unmarshalling._
 import spray.json.DefaultJsonProtocol._
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 
 class HttpOntologyDAO(implicit val system: ActorSystem, implicit val executionContext: ExecutionContext)
   extends OntologyDAO with RestJsonClient with LazyLogging {
+
+  private val ontologyHostSetup = Http.HostConnectorSetup(FireCloudConfig.Duos.baseOntologyUrl,
+    port = 443, sslEncryption = true)
 
   override def search(term: String): Future[Option[List[TermResource]]] = {
     searchBlocking(term)
@@ -37,17 +45,23 @@ class HttpOntologyDAO(implicit val system: ActorSystem, implicit val executionCo
   }
 
   private def searchAsync(term: String): Future[Option[List[TermResource]]] = {
-    val targetUri = Uri(ontologySearchUrl).withQuery(("id", term))
-    logger.info(s"HttpOntologyDAO querying ${term} ...")
-    unAuthedRequest(Get(targetUri)) map { response =>
-      logger.info(s"HttpOntologyDAO querying ${term}: ${response.status.defaultMessage}")
-      response.entity.as[List[TermResource]] match {
-        case Right(obj) => Some(obj)
-        case Left(err) =>
-          logger.warn(s"Error while retrieving ontology parents for id '$term': $err")
-          None
+    getHostConnector flatMap { hostConnector =>
+      val targetUri = Uri(ontologySearchUrl).withQuery(("id", term))
+      logger.info(s"HttpOntologyDAO querying ${term} ...")
+      unAuthedRequest(Get(targetUri), connector = Some(hostConnector)) map { response =>
+        logger.info(s"HttpOntologyDAO querying ${term}: ${response.status.defaultMessage}")
+        response.entity.as[List[TermResource]] match {
+          case Right(obj) => Some(obj)
+          case Left(err) =>
+            logger.warn(s"Error while retrieving ontology parents for id '$term': $err")
+            None
+        }
       }
     }
   }
 
+  private def getHostConnector: Future[ActorRef] = {
+    implicit val timeout:Timeout = 60.seconds // timeout to get the host connector reference
+    for (Http.HostConnectorInfo(connector, _) <- IO(Http) ? ontologyHostSetup) yield connector
+  }
 }
--- src/main/scala/org/broadinstitute/dsde/firecloud/utils/RestJsonClient.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/utils/RestJsonClient.scala
@@ -1,6 +1,6 @@
 package org.broadinstitute.dsde.firecloud.utils
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
 import org.broadinstitute.dsde.firecloud.FireCloudExceptionWithErrorReport
 import org.broadinstitute.dsde.firecloud.model.ErrorReportExtensions.FCErrorReport
 import org.broadinstitute.dsde.firecloud.model.WithAccessToken
@@ -13,6 +13,7 @@ import spray.http.{HttpRequest, HttpResponse}
 import spray.httpx.encoding.Gzip
 import spray.httpx.unmarshalling._
 
+import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 
 /**
@@ -22,18 +23,26 @@ trait RestJsonClient extends FireCloudRequestBuilding {
   implicit val system: ActorSystem
   implicit val executionContext: ExecutionContext
 
-  def unAuthedRequest(req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false): Future[HttpResponse] = {
+  def unAuthedRequest(req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false, connector: Option[ActorRef] = None): Future[HttpResponse] = {
     implicit val userInfo:WithAccessToken = null
-    doRequest(None)(req, compressed, useFireCloudHeader)
+    doRequest(None)(req, compressed, useFireCloudHeader, connector)
   }
-  def userAuthedRequest(req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false)(implicit userInfo: WithAccessToken): Future[HttpResponse] = doRequest(Option(addCredentials(userInfo.accessToken)))(req, compressed, useFireCloudHeader)
-  def adminAuthedRequest(req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false): Future[HttpResponse] = doRequest(Option(addAdminCredentials))(req, compressed, useFireCloudHeader)
+  def userAuthedRequest(req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false, connector: Option[ActorRef] = None)(implicit userInfo: WithAccessToken): Future[HttpResponse] =
+    doRequest(Option(addCredentials(userInfo.accessToken)))(req, compressed, useFireCloudHeader, connector)
+  def adminAuthedRequest(req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false, connector: Option[ActorRef] = None): Future[HttpResponse] =
+    doRequest(Option(addAdminCredentials))(req, compressed, useFireCloudHeader, connector)
 
-  private def doRequest(addCreds: Option[RequestTransformer])(req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false): Future[HttpResponse] = {
+  private def doRequest(addCreds: Option[RequestTransformer])(req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false, connector: Option[ActorRef] = None): Future[HttpResponse] = {
+    val sr = if (connector.isDefined) {
+      sendReceive(connector.get)(executionContext, futureTimeout = 60.seconds)
+    } else {
+      sendReceive
+    }
+
     val pipeline = (compressed, useFireCloudHeader) match {
-      case (true, true) => addFireCloudCredentials ~> addHeader (`Accept-Encoding`(gzip)) ~> sendReceive ~> decode(Gzip)
-      case (true, false) => addHeader (`Accept-Encoding`(gzip)) ~> sendReceive ~> decode(Gzip)
-      case (false, true) => addFireCloudCredentials ~> sendReceive
+      case (true, true) => addFireCloudCredentials ~> addHeader (`Accept-Encoding`(gzip)) ~> sr ~> decode(Gzip)
+      case (true, false) => addHeader (`Accept-Encoding`(gzip)) ~> sr ~> decode(Gzip)
+      case (false, true) => addFireCloudCredentials ~> sr
       case _ => sendReceive
     }
 
@@ -41,20 +50,20 @@ trait RestJsonClient extends FireCloudRequestBuilding {
     finalPipeline(req)
   }
 
-  def authedRequestToObject[T](req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false)(implicit userInfo: WithAccessToken, unmarshaller: Unmarshaller[T], ers: ErrorReportSource): Future[T] = {
-    requestToObject(true, req, compressed, useFireCloudHeader)
+  def authedRequestToObject[T](req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false, connector: Option[ActorRef] = None)(implicit userInfo: WithAccessToken, unmarshaller: Unmarshaller[T], ers: ErrorReportSource): Future[T] = {
+    requestToObject(true, req, compressed, useFireCloudHeader, connector)
   }
 
-  def unAuthedRequestToObject[T](req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false)(implicit unmarshaller: Unmarshaller[T], ers: ErrorReportSource): Future[T] = {
+  def unAuthedRequestToObject[T](req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false, connector: Option[ActorRef] = None)(implicit unmarshaller: Unmarshaller[T], ers: ErrorReportSource): Future[T] = {
     implicit val userInfo:WithAccessToken = null
-    requestToObject(false, req, compressed, useFireCloudHeader)
+    requestToObject(false, req, compressed, useFireCloudHeader, connector)
   }
 
-  def requestToObject[T](auth: Boolean, req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false)(implicit userInfo: WithAccessToken, unmarshaller: Unmarshaller[T], ers: ErrorReportSource): Future[T] = {
+  def requestToObject[T](auth: Boolean, req: HttpRequest, compressed: Boolean = false, useFireCloudHeader: Boolean = false, connector: Option[ActorRef] = None)(implicit userInfo: WithAccessToken, unmarshaller: Unmarshaller[T], ers: ErrorReportSource): Future[T] = {
     val resp = if(auth) {
-      userAuthedRequest(req, compressed, useFireCloudHeader)
+      userAuthedRequest(req, compressed, useFireCloudHeader, connector)
     } else {
-      unAuthedRequest(req, compressed, useFireCloudHeader)
+      unAuthedRequest(req, compressed, useFireCloudHeader, connector)
     }
 
     resp map { response =>
