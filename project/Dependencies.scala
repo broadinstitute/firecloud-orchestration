dd3fa91097a472c551ee959ed434c1e31dad96a2
Matt Bemis <mbemis@broadinstitute.org>
2017-05-04 13:08:15 -0400

    update rawls model, update swagger

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
     "com.getsentry.raven" % "raven-logback" % "7.8.6",
     "com.typesafe.scala-logging" %% "scala-logging-slf4j" % "2.1.2",
     "org.broadinstitute.dsde.vault" %%  "vault-common"  % "0.1-19-ca8b927",
-    "org.broadinstitute.dsde" %%  "rawls-model"  % "0.1-955be21-SNAP"
+    "org.broadinstitute.dsde" %%  "rawls-model"  % "0.1-cb7f5b1"
       exclude("com.typesafe.scala-logging", "scala-logging_2.11"),
     "io.spray"            %%  "spray-can"     % sprayV,
     "io.spray"            %%  "spray-routing" % sprayV,
