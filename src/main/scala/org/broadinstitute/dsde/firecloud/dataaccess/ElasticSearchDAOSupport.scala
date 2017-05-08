884988c85ad950dd5a85a1681d12bf4dbf47e2a4
David An <davidan@broadinstitute.org>
2017-05-08 17:14:08 -0400

    syntax for es client initialization

--- src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ElasticSearchDAOSupport.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ElasticSearchDAOSupport.scala
@@ -11,6 +11,7 @@ import org.elasticsearch.action.{ActionRequest, ActionRequestBuilder, ActionResp
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.transport.InetSocketTransportAddress
+import org.elasticsearch.transport.client.PreBuiltTransportClient
 import spray.http.Uri.Authority
 import spray.json._
 
@@ -20,13 +21,13 @@ trait ElasticSearchDAOSupport extends LazyLogging {
 
   def buildClient(servers:Seq[Authority]): TransportClient = {
     // cluster name is constant across environments; no need to add it to config
-    val settings = Settings.settingsBuilder
+    val settings = Settings.builder
       .put("cluster.name", "firecloud-elasticsearch")
       .build
     val addresses = servers map { server =>
       new InetSocketTransportAddress(InetAddress.getByName(server.host.address), server.port)
     }
-    TransportClient.builder().settings(settings).build.addTransportAddresses(addresses: _*)
+    new PreBuiltTransportClient(settings).addTransportAddresses(addresses: _*)
   }
 
   def executeESRequest[T <: ActionRequest[T], U <: ActionResponse, V <: ActionRequestBuilder[T, U, V]](req: V): U = {
