92a43c1dda3394ea172498687a575f6e193d8082
David An <davidan@genomebridge.org>
2017-06-12 15:31:49 -0400

    GAWB-2123: workspace tag apis (#463)
    APIs for manipulating workspace tags

--- src/main/scala/org/broadinstitute/dsde/firecloud/service/WorkspaceService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/service/WorkspaceService.scala
@@ -15,6 +15,7 @@ import org.broadinstitute.dsde.firecloud.service.PerRequest.{PerRequestMessage, 
 import org.broadinstitute.dsde.firecloud.utils.{PermissionsSupport, TSVFormatter, TSVLoadFile}
 import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol._
 import org.broadinstitute.dsde.firecloud.model.RequestCompleteWithErrorReport
+import org.broadinstitute.dsde.rawls.model.AttributeUpdateOperations.{AddListMember, AddUpdateAttribute, RemoveListMember}
 import spray.http.MediaTypes._
 import spray.http.{HttpHeaders, StatusCodes}
 import spray.httpx.SprayJsonSupport._
@@ -38,6 +39,10 @@ object WorkspaceService {
   case class UpdateWorkspaceACL(workspaceNamespace: String, workspaceName: String, aclUpdates: Seq[WorkspaceACLUpdate], originEmail: String, inviteUsersNotFound: Boolean) extends WorkspaceServiceMessage
   case class ExportWorkspaceAttributesTSV(workspaceNamespace: String, workspaceName: String, filename: String) extends WorkspaceServiceMessage
   case class ImportAttributesFromTSV(workspaceNamespace: String, workspaceName: String, tsvString: String) extends WorkspaceServiceMessage
+  case class GetTags(workspaceNamespace: String, workspaceName: String) extends WorkspaceServiceMessage
+  case class PutTags(workspaceNamespace: String, workspaceName: String, tags: List[String]) extends WorkspaceServiceMessage
+  case class PatchTags(workspaceNamespace: String, workspaceName: String, tags: List[String]) extends WorkspaceServiceMessage
+  case class DeleteTags(workspaceNamespace: String, workspaceName: String, tags: List[String]) extends WorkspaceServiceMessage
 
   def props(workspaceServiceConstructor: WithAccessToken => WorkspaceService, userToken: WithAccessToken): Props = {
     Props(workspaceServiceConstructor(userToken))
@@ -76,6 +81,14 @@ class WorkspaceService(protected val argUserToken: WithAccessToken, val rawlsDAO
       exportWorkspaceAttributesTSV(workspaceNamespace, workspaceName, filename) pipeTo sender
     case ImportAttributesFromTSV(workspaceNamespace: String, workspaceName: String, tsvString: String) =>
       importAttributesFromTSV(workspaceNamespace, workspaceName, tsvString) pipeTo sender
+    case GetTags(workspaceNamespace: String, workspaceName: String) =>
+      getTags(workspaceNamespace, workspaceName) pipeTo sender
+    case PutTags(workspaceNamespace: String, workspaceName: String,tags:List[String]) =>
+      putTags(workspaceNamespace, workspaceName, tags) pipeTo sender
+    case PatchTags(workspaceNamespace: String, workspaceName: String,tags:List[String]) =>
+      patchTags(workspaceNamespace, workspaceName, tags) pipeTo sender
+    case DeleteTags(workspaceNamespace: String, workspaceName: String,tags:List[String]) =>
+      deleteTags(workspaceNamespace, workspaceName, tags) pipeTo sender
 
   }
 
@@ -153,5 +166,53 @@ class WorkspaceService(protected val argUserToken: WithAccessToken, val rawlsDAO
       }
     }
   }
+
+  def getTags(workspaceNamespace: String, workspaceName: String): Future[PerRequestMessage] = {
+    rawlsDAO.getWorkspace(workspaceNamespace, workspaceName) flatMap { workspaceResponse =>
+      val tags = getTagsFromWorkspace(workspaceResponse.workspace)
+      Future(RequestComplete(StatusCodes.OK, formatTags(tags)))
+    }
+  }
+
+  def putTags(workspaceNamespace: String, workspaceName: String, tags: List[String]): Future[PerRequestMessage] = {
+    val attrList = AttributeValueList(tags map (tag => AttributeString(tag.trim)))
+    val op = AddUpdateAttribute(AttributeName.withTagsNS, attrList)
+    rawlsDAO.patchWorkspaceAttributes(workspaceNamespace, workspaceName, Seq(op)) flatMap { ws =>
+      val tags = getTagsFromWorkspace(ws)
+      Future(RequestComplete(StatusCodes.OK, formatTags(tags)))
+    }
+  }
+
+  def patchTags(workspaceNamespace: String, workspaceName: String, tags: List[String]): Future[PerRequestMessage] = {
+    rawlsDAO.getWorkspace(workspaceNamespace, workspaceName) flatMap { origWs =>
+      val origTags = getTagsFromWorkspace(origWs.workspace)
+      val attrOps = (tags diff origTags) map (tag => AddListMember(AttributeName.withTagsNS, AttributeString(tag.trim)))
+      rawlsDAO.patchWorkspaceAttributes(workspaceNamespace, workspaceName, attrOps) flatMap { patchedWs =>
+        val tags = getTagsFromWorkspace(patchedWs)
+        Future(RequestComplete(StatusCodes.OK, formatTags(tags)))
+      }
+    }
+  }
+
+  def deleteTags(workspaceNamespace: String, workspaceName: String, tags: List[String]): Future[PerRequestMessage] = {
+    val attrOps = tags map (tag => RemoveListMember(AttributeName.withTagsNS, AttributeString(tag.trim)))
+    rawlsDAO.patchWorkspaceAttributes(workspaceNamespace, workspaceName, attrOps) flatMap { ws =>
+      val tags = getTagsFromWorkspace(ws)
+      Future(RequestComplete(StatusCodes.OK, formatTags(tags)))
+    }
+
+  }
+
+  private def getTagsFromWorkspace(ws:Workspace): Seq[String] = {
+    ws.attributes.get(AttributeName.withTagsNS) match {
+      case Some(vals:AttributeValueList) => vals.list collect {
+        case s:AttributeString => s.value
+      }
+      case _ => Seq.empty[String]
+    }
+  }
+
+  private def formatTags(tags: Seq[String]) = tags.toList.sortBy(_.toLowerCase)
+
 
 }
