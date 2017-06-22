d50738a2a7700b8a94f34d01b0c3d15bbfae3fe4
Greg Rushton <grushton@broadinstitute.org>
2017-06-22 14:39:30 -0400

    some tests

--- src/test/scala/org/broadinstitute/dsde/firecloud/service/ExportEntitiesByTypeServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/service/ExportEntitiesByTypeServiceSpec.scala
@@ -3,6 +3,7 @@ package org.broadinstitute.dsde.firecloud.service
 import akka.actor.ActorSystem
 import org.broadinstitute.dsde.firecloud.model._
 import org.broadinstitute.dsde.firecloud.webservice.ExportEntitiesApiService
+import spray.http.HttpHeaders
 import spray.http.StatusCodes._
 
 class ExportEntitiesByTypeServiceSpec extends BaseServiceSpec with ExportEntitiesApiService {
@@ -11,10 +12,24 @@ class ExportEntitiesByTypeServiceSpec extends BaseServiceSpec with ExportEntitie
 
   val exportEntitiesByTypeConstructor: UserInfo => ExportEntitiesByTypeActor = ExportEntitiesByTypeActor.constructor(app)
 
+  val validFireCloudEntitiesSampleSetTSVPath = "/api/workspaces/broad-dsde-dev/valid/entities/sample_set/tsv"
   val validFireCloudEntitiesSampleTSVPath = "/api/workspaces/broad-dsde-dev/valid/entities/sample/tsv"
   val invalidFireCloudEntitiesSampleTSVPath = "/api/workspaces/broad-dsde-dev/invalid/entities/sample/tsv"
+  val invalidFireCloudEntitiesParticipantSetTSVPath = "/api/workspaces/broad-dsde-dev/invalid/entities/participant_set/tsv"
 
   "EntityService-ExportEntitiesByType" - {
+
+    "when calling GET on exporting a valid collection type" - {
+      "OK response is returned" in {
+        Get(validFireCloudEntitiesSampleSetTSVPath) ~> dummyUserIdHeaders("1234") ~> sealRoute(exportEntitiesRoutes) ~> check {
+          handled should be(true)
+          status should be(OK)
+          response.entity shouldNot be(empty)
+          response.headers.contains(HttpHeaders.Connection("Keep-Alive")) should be(true)
+          response.headers.contains(HttpHeaders.`Content-Disposition`.apply("attachment", Map("filename" -> "sample_set.zip"))) should be(true)
+        }
+      }
+    }
 
     "when calling GET on exporting a valid entity type" - {
       "OK response is returned" in {
@@ -22,6 +37,17 @@ class ExportEntitiesByTypeServiceSpec extends BaseServiceSpec with ExportEntitie
           handled should be(true)
           status should be(OK)
           response.entity shouldNot be(empty)
+          response.headers.contains(HttpHeaders.Connection("Keep-Alive")) should be(true)
+          response.headers.contains(HttpHeaders.`Content-Disposition`.apply("attachment", Map("filename" -> "sample.txt"))) should be(true)
+        }
+      }
+    }
+
+    "when calling GET on exporting an invalid collection type" - {
+      "NotFound response is returned" in {
+        Get(invalidFireCloudEntitiesParticipantSetTSVPath) ~> dummyUserIdHeaders("1234") ~> sealRoute(exportEntitiesRoutes) ~> check {
+          handled should be(true)
+          status should be(NotFound)
         }
       }
     }
