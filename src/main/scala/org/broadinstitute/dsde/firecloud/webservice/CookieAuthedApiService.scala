3b8918746011a943a9b1eea95d5c6d44f66d1423
Greg Rushton <grushton@broadinstitute.org>
2017-06-22 10:35:59 -0400

    refactor response handling and fix test

--- src/main/scala/org/broadinstitute/dsde/firecloud/webservice/CookieAuthedApiService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/webservice/CookieAuthedApiService.scala
@@ -1,9 +1,6 @@
 package org.broadinstitute.dsde.firecloud.webservice
 
-import akka.Done
 import akka.actor.Props
-import akka.pattern.ask
-import akka.util.Timeout
 import org.broadinstitute.dsde.firecloud.dataaccess.HttpGoogleServicesDAO
 import org.broadinstitute.dsde.firecloud.model.UserInfo
 import org.broadinstitute.dsde.firecloud.service._
@@ -11,9 +8,7 @@ import org.slf4j.LoggerFactory
 import spray.http._
 import spray.routing._
 
-import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.util.Success
 
 /**
  * Created by dvoet on 11/16/16.
@@ -37,13 +32,7 @@ trait CookieAuthedApiService extends HttpService with PerRequestCreator with Fir
             val exportProps: Props = ExportEntitiesByTypeActor.props(exportEntitiesByTypeConstructor, userInfo)
             val exportMessage = ExportEntitiesByTypeActor.ExportEntities(requestContext, workspaceNamespace, workspaceName, entityType, attributeNames)
             val exportActor = actorRefFactory.actorOf(exportProps)
-            // Necessary for the actor ask pattern
-            implicit val timeout: Timeout = 1.minutes
-            lazy val exportFuture = (exportActor ? exportMessage).mapTo[Done]
-            onComplete(exportFuture) {
-              case Success(result) => complete(StatusCodes.OK)
-              case _ => complete(StatusCodes.InternalServerError, "Error generating entity download")
-            }.apply(requestContext)
+            exportActor ! exportMessage
           }
         }
     } ~
