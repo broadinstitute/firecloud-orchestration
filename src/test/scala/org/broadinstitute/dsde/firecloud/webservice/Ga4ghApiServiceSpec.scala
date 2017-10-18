cad8c61e7c40f0e7d68bb284684d88ffd9e99f43
Greg Rushton <grushton@broadinstitute.org>
2017-10-18 17:16:22 -0400

    test new path handling

--- src/test/scala/org/broadinstitute/dsde/firecloud/webservice/Ga4ghApiServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/webservice/Ga4ghApiServiceSpec.scala
@@ -1,5 +1,6 @@
 package org.broadinstitute.dsde.firecloud.webservice
 
+import akka.actor.ActorSystem
 import org.broadinstitute.dsde.firecloud.mock.MockUtils
 import org.broadinstitute.dsde.firecloud.service.BaseServiceSpec
 import org.mockserver.integration.ClientAndServer
@@ -11,19 +12,33 @@ import spray.http.StatusCodes._
 
 class Ga4ghApiServiceSpec extends BaseServiceSpec with Ga4ghApiService with BeforeAndAfterEach {
 
-  def actorRefFactory = system
+  def actorRefFactory: ActorSystem = system
 
   var toolRegistryServer: ClientAndServer = _
-  val toolRegistryDummyUrlPath = "/ga4gh/v1/tools/namespace:name/versions/1/WDL/descriptor"
+  val toolPaths = List(
+    "/ga4gh/v1/metadata",
+    "/ga4gh/v1/tool-classes",
+    "/ga4gh/v1/tools",
+    "/ga4gh/v1/tools/namespace:name",
+    "/ga4gh/v1/tools/namespace:name/versions",
+    "/ga4gh/v1/tools/namespace:name/versions/1",
+    "/ga4gh/v1/tools/namespace:name/versions/1/WDL/descriptor")
+
+  val unImplementedPaths = List(
+    "/ga4gh/v1/tools/namespace:name/versions/1/dockerfile",
+    "/ga4gh/v1/tools/namespace:name/versions/1/WDL/descriptor/1",
+    "/ga4gh/v1/tools/namespace:name/versions/1/WDL/tests"
+  )
 
   override def beforeAll(): Unit = {
     toolRegistryServer = startClientAndServer(MockUtils.methodsServerPort)
-
-    toolRegistryServer.when(request().withMethod(HttpMethods.GET.name).withPath(toolRegistryDummyUrlPath))
-      .respond(
-        org.mockserver.model.HttpResponse.response()
-          .withStatusCode(OK.intValue)
-      )
+    toolPaths.map { path =>
+      toolRegistryServer.when(request().withMethod(HttpMethods.GET.name).withPath(path))
+        .respond(
+          org.mockserver.model.HttpResponse.response()
+            .withStatusCode(OK.intValue)
+        )
+    }
   }
 
   override def afterAll(): Unit = {
@@ -32,19 +47,29 @@ class Ga4ghApiServiceSpec extends BaseServiceSpec with Ga4ghApiService with Befo
 
   "GA4GH API service" - {
     "Tool Registry" - {
-      "get-descriptor passthrough API" - {
-
+      "passthrough APIs" - {
+        "should handle un-implemented paths" in {
+          unImplementedPaths.map { path =>
+            Get(path) ~> ga4ghRoutes ~> check {
+              status should equal(NotImplemented)
+            }
+          }
+        }
         "should reject all but GET" in {
           List(HttpMethods.POST, HttpMethods.PUT, HttpMethods.PATCH, HttpMethods.DELETE, HttpMethods.HEAD) foreach {
             method =>
-              new RequestBuilder(method)(toolRegistryDummyUrlPath) ~> ga4ghRoutes ~> check {
-                assert(!handled)
+              (toolPaths ++ unImplementedPaths).map { path =>
+                new RequestBuilder(method)(path) ~> ga4ghRoutes ~> check {
+                  assert(!handled)
+                }
               }
           }
         }
         "should accept GET" in {
-          Get(toolRegistryDummyUrlPath) ~> ga4ghRoutes ~> check {
-           assert(handled)
+          toolPaths.map { path =>
+            Get(path) ~> ga4ghRoutes ~> check {
+              assert(handled)
+            }
           }
         }
       }
