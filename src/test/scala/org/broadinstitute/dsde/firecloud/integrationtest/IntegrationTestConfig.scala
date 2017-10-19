c85aa554030b2e2230f211d7c27f3f5875662a68
David An <davidan@genomebridge.org>
2017-10-19 11:31:48 -0400

    upgrade third-party dependencies for security scan (#514)
    upgrade third-party dependencies for security scan. Requires reconfiguration/forking of integration tests.

--- src/test/scala/org/broadinstitute/dsde/firecloud/integrationtest/IntegrationTestConfig.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/integrationtest/IntegrationTestConfig.scala
@@ -16,12 +16,15 @@ trait IntegrationTestConfig {
 
   object ITElasticSearch {
     // the list of ES servers to use for integration tests looks in this order:
-    //  - passed on command-line, e.g. "sbt -Desurls=foo it:test", which is what Jenkins does
+    //  - as a system property named esurls. Note that without changes to sbt config, this cannot be passed
+    //      via the -D flag on the command line, since we fork tests into their own JVM.
+    //  - in the ESURLS environment variable, which is what Jenkins does
     //  - in src/test/resources/ittest.conf, which doesn't normally exist unless you put it there
     //  - in config/firecloud-orchestration.conf, which usually exists for developers but won't exist in Jenkins
     //  - in standard test conf, typically src/test/resources/reference.conf, which is unlikely to have real server urls
-    private val systemESUrls = System.getProperty("esurls",
-        itTestConf.getConfig("elasticsearch").getString("urls"))
+    private val envESUrls = System.getenv.getOrDefault("ESURLS", itTestConf.getConfig("elasticsearch").getString("urls"))
+
+    private val systemESUrls = System.getProperty("esurls", envESUrls)
 
     val servers: Seq[Authority] = FireCloudConfig.parseESServers(systemESUrls)
     val clusterName: String = itTestConf.getConfig("elasticsearch").getString("clusterName")
