fe6c46b360d6a54ad51ab148b662eb6553bf921c
Greg Rushton <rushtong@users.noreply.github.com>
2017-10-20 14:09:49 -0400

    GAWB-849: Agora supported GA4GH APIs (#517)
    * GA4GH support

--- src/main/scala/org/broadinstitute/dsde/firecloud/webservice/Ga4ghApiService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/webservice/Ga4ghApiService.scala
@@ -11,17 +11,58 @@ trait Ga4ghApiService extends HttpService with FireCloudDirectives {
 
   private implicit val ec: ExecutionContext = actorRefFactory.dispatcher
 
-  private val agora = FireCloudConfig.Agora.baseUrl
+  private val agoraGA4GH = s"${FireCloudConfig.Agora.baseUrl}/ga4gh/v1"
 
   val ga4ghRoutes: Route =
     pathPrefix("ga4gh") {
       pathPrefix("v1") {
-        pathPrefix("tools") {
-          path(Segment / "versions" / Segment / Segment / "descriptor") { (id, versionId, descriptorType) =>
-            get {
-              val targetUri = Uri(s"$agora/ga4gh/v1/tools/$id/versions/$versionId/$descriptorType/descriptor")
-              passthrough(targetUri, HttpMethods.GET)
+        get {
+          path("metadata") {
+            val targetUri = Uri(s"$agoraGA4GH/metadata")
+            passthrough(targetUri, HttpMethods.GET)
+          } ~
+          path("tool-classes") {
+            val targetUri = Uri(s"$agoraGA4GH/tool-classes")
+            passthrough(targetUri, HttpMethods.GET)
+          } ~
+          path("tools") {
+            parameterSeq { params =>
+              val targetUri = Uri(s"$agoraGA4GH/tools")
+              val uri = if (params.isEmpty) {
+                targetUri
+              } else {
+                targetUri.withQuery(params.toMap)
+              }
+              passthrough(uri, HttpMethods.GET)
             }
+          } ~
+          path("tools" / Segment) { (id) =>
+            val targetUri = Uri(s"$agoraGA4GH/tools/$id")
+            passthrough(targetUri, HttpMethods.GET)
+          } ~
+          path("tools" / Segment / "versions") { (id) =>
+            val targetUri = Uri(s"$agoraGA4GH/tools/$id/versions")
+            passthrough(targetUri, HttpMethods.GET)
+          } ~
+          path("tools" / Segment / "versions" / Segment / "dockerfile") { (id, versionId) =>
+            val targetUri = Uri(s"$agoraGA4GH/tools/$id/versions/$versionId/dockerfile")
+            passthrough(targetUri, HttpMethods.GET)
+          } ~
+          path("tools" / Segment / "versions" / Segment) { (id, versionId) =>
+            val targetUri = Uri(s"$agoraGA4GH/tools/$id/versions/$versionId")
+            passthrough(targetUri, HttpMethods.GET)
+          } ~
+          path("tools" / Segment / "versions" / Segment / Segment / "descriptor") { (id, versionId, descriptorType) =>
+            val targetUri = Uri(s"$agoraGA4GH/tools/$id/versions/$versionId/$descriptorType/descriptor")
+            passthrough(targetUri, HttpMethods.GET)
+          } ~
+          path("tools" / Segment / "versions" / Segment / Segment / "descriptor" / Segment) { (id, versionId, descriptorType, relativePath) =>
+            val targetUri = Uri(s"$agoraGA4GH/tools/$id/versions/$versionId/$descriptorType/descriptor/$relativePath")
+            passthrough(targetUri, HttpMethods.GET)
+          } ~
+          path("tools" / Segment / "versions" / Segment / Segment / "tests") { (id, versionId, descriptorType) =>
+            val targetUri = Uri(s"$agoraGA4GH/tools/$id/versions/$versionId/$descriptorType/tests")
+            passthrough(targetUri, HttpMethods.GET)
           }
         }
       }
