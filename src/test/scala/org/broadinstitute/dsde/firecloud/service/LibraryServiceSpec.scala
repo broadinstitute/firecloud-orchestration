9b1d6c89755978b989b15e41836c299993f8fce3
Isaac Zarsky <zarsky@broadinstitute.org>
2017-11-09 18:00:55 -0500

    fix for tests again!

--- src/test/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/service/LibraryServiceSpec.scala
@@ -444,7 +444,7 @@ class LibraryServiceSpec extends BaseServiceSpec with FreeSpecLike with LibraryS
         val ex = intercept[ValidationException] {
           validateJsonSchema(sampleData, testSchema)
         }
-        assertResult(31){ex.getViolationCount}
+        assertResult(30){ex.getViolationCount}
       }
       "fails with one missing key" in {
         val testSchema = FileUtils.readAllTextFromResource("library/attribute-definitions.json")
