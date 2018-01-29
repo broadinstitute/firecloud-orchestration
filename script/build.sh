4662b4a571f2be56101dc2f377fb37121c8c89f4
Jackie <jacqueline.roberti93@gmail.com>
2018-01-29 11:26:21 -0500

    update build script (#590)

--- script/build.sh
+++ script/build.sh
@@ -15,7 +15,7 @@ PROJECT=firecloud-orchestration
 function make_jar()
 {
     # Set for versioning the jar
-    GIT_MODEL_HASH=$(git rev-parse --short ${GIT_BRANCH})
+    GIT_MODEL_HASH=$(git rev-parse --short ${BRANCH})
 
     docker run --rm -e GIT_MODEL_HASH=${GIT_MODEL_HASH} \
         -v $PWD:/working -w /working -v jar-cache:/root/.ivy -v jar-cache:/root/.ivy2 \
@@ -26,22 +26,28 @@ function docker_cmd()
 {
     if [ $DOCKER_CMD = "build" ] || [ $DOCKER_CMD = "push" ]; then
         echo "building docker image..."
-
-        GIT_SHA=$(git rev-parse ${GIT_BRANCH})
-        echo GIT_SHA=$GIT_SHA > env.properties  # for jenkins jobs
-        docker build -t $REPO:${GIT_SHA:0:12} .
+        if [ "$ENV" != "dev" ] && [ "$ENV" != "alpha" ] && [ "$ENV" != "staging" ] && [ "$ENV" != "perf" ]; then
+            DOCKER_TAG=${BRANCH}
+        else
+            GIT_SHA=$(git rev-parse origin/${BRANCH})
+            echo GIT_SHA=$GIT_SHA > env.properties
+            DOCKER_TAG=${GIT_SHA:0:12}
+        fi
+        docker build -t $REPO:${DOCKER_TAG} .
 
         if [ $DOCKER_CMD="push" ]; then
             echo "pushing docker image..."
-            docker push $REPO:${GIT_SHA:0:12}
+            docker push $REPO:${DOCKER_TAG}
         fi
     else
         echo "Not a valid docker option!  Choose either build or push (which includes build)"
     fi
 }
 
-GIT_BRANCH=${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}  # default to current branch
+BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}  # default to current branch
 REPO=${REPO:-broadinstitute/$PROJECT}
+ENV=${ENV:-""}  # if env is not set, push an image with branch name
+
 while [ "$1" != "" ]; do
     case $1 in
         jar) make_jar ;;
