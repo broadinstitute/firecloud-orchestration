018bc4fddfae99c39aa823b70ff8341c817aca69
David An <davidan@broadinstitute.org>
2018-04-20 12:15:08 -0400

    clarify MetricsSpec unit test

--- src/test/scala/org/broadinstitute/dsde/firecloud/metrics/MetricsSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/metrics/MetricsSpec.scala
@@ -32,9 +32,9 @@ class MetricsSpec extends BaseServiceSpec {
     }
     "should serialize NumSubjects metric correctly" in {
       List(Integer.MIN_VALUE, -1, 0, 1, 42, 123456789, Integer.MAX_VALUE) foreach { num =>
-        val numObjects = NumSubjects(num)
+        val numSubjects = NumSubjects(num)
         val expected = JsObject(Map("metricType" -> JsString("NumSubjects"), "numSubjects" -> JsNumber(num)))
-        assertResult(expected) {LogitMetricFormat.write(numObjects)}
+        assertResult(expected) {LogitMetricFormat.write(numSubjects)}
       }
     }
     "should serialize SamplesAndSubjects metric correctly" in {
@@ -93,13 +93,22 @@ class MetricsSpec extends BaseServiceSpec {
         searchDAO = new MetricsSpecMockSearchDAO(numFailures=0, numSubjects=seed-2),
         logitDAO = new MetricsSpecMockLogitDAO(numFailures=0))
       val metricsActor = system.actorOf(MetricsActor.props(testApp), "metrics-spec-actor")
+      // MetricsActor operates on a repeating schedule (e.g. every 6 hours). Mimic that here
+      // by asking it for metrics twice in a row to make sure it operates correctly on the second try.
       assertResult(SamplesAndSubjects(seed, seed-2)) { Await.result(metricsActor ? MetricsActor.RecordMetrics, duration) }
       assertResult(SamplesAndSubjects(seed, seed-2)) { Await.result(metricsActor ? MetricsActor.RecordMetrics, duration) }
     }
   }
 
-
 }
+
+/* =======================================================================================
+      Mock DAOs to support these tests. Each of these DAOs accepts a "numFailures" argument.
+      When numFailures is non-zero, each DAO will throw an Exception/Future.failed
+      the first $numFailures times it is called. This allows calling code to test resilience
+      to errors.
+   ======================================================================================= */
+
 
 class MetricsSpecMockLogitDAO(numFailures: Int) extends MockLogitDAO {
   val q = new java.util.concurrent.ConcurrentLinkedQueue[Int]
