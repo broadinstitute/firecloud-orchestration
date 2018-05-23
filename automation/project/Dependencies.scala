ecefa2d0d645ebe79f0c9e28db544b0a57fba3c4
Brian Reilly <breilly@broadinstitute.org>
2018-05-23 09:27:39 -0400

    Updated service-test to non-SNAP version

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
   val workbenchGoogle: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-google" % workbenchGoogleV excludeAll excludeWorkbenchModel
   val excludeWorkbenchGoogle = ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-google_2.11")
 
-  val workbenchServiceTestV = "0.9-e97a335-SNAP"
+  val workbenchServiceTestV = "0.9-cbc35de"
   val workbenchServiceTest: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-service-test" % workbenchServiceTestV % "test" classifier "tests" excludeAll (excludeWorkbenchGoogle, excludeWorkbenchModel)
 
   val rootDependencies = Seq(
