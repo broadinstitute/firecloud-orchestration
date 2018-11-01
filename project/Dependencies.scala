fedc3fc342fe8310716d8978c1e7f570df03fbfb
Douglas Voet <dvoet@users.noreply.github.com>
2018-11-01 11:51:00 -0400

    missed a file

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -46,6 +46,7 @@ object Dependencies {
     excludeGuavaJDK5("com.google.apis"                % "google-api-services-compute" % "v1-rev163-1.23.0"),
     excludeGuavaJDK5("com.google.apis"                % "google-api-services-sheets"  % "v4-rev494-1.23.0"),
     excludeGuavaJDK5("com.google.apis"                % "google-api-services-cloudbilling" % "v1-rev14-1.23.0"),
+    excludeGuavaJDK5("com.google.apis"                % "google-api-services-pubsub"       % "v1-rev357-1.22.0"),
 
     "org.webjars"                    % "swagger-ui"          % "2.2.5",
     "com.jason-goodwin"             %% "authentikat-jwt"     % "0.4.5"
