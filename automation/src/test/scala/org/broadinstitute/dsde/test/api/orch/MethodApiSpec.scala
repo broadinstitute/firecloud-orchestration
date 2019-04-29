a60ce097118852d9dcd7497500e0772bc87375a0
ansingh7115 <singh.an@husky.neu.edu>
2019-04-29 16:53:37 -0400

    add launch analysis on redacted method should fail test

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/orch/MethodApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/orch/MethodApiSpec.scala
@@ -32,7 +32,7 @@ class MethodApiSpec extends FreeSpec with Matchers
             //get method
             val getMethodResponse = intercept[RestException]{ Orchestration.methods.getMethod(method.methodNamespace, method.methodName, method.snapshotId)}
             println("getMethodResponse " + getMethodResponse.toString)
-            getMethodResponse.message should include("")
+            getMethodResponse.message.parseJson.asJsObject.fields("statusCode").convertTo[Int] should be (403)
 
             //            {
             //  "deleted": false,
@@ -68,7 +68,7 @@ class MethodApiSpec extends FreeSpec with Matchers
             )
 
             val editedMethodConfig = Map(
-              "deleted" -> "false",
+              "deleted" -> false,
               "inputs"  -> SimpleMethodConfig.inputs,
               "outputs" -> SimpleMethodConfig.outputs,
               "methodConfigVersion" -> 1,
@@ -89,46 +89,57 @@ class MethodApiSpec extends FreeSpec with Matchers
       }
     }
 
-//    "launch analysis and delete errors on redacted method" in {
-//      val user = UserPool.chooseProjectOwner
-//      implicit val authToken: AuthToken = user.makeAuthToken()
-//      withCleanBillingProject(user) { billingProject =>
-//        withWorkspace(billingProject, "MethodRedactedSpec_delete") { workspaceName =>
-//          withMethod("MethodRedactedSpec_delete", MethodData.SimpleMethod, cleanUp = false) { methodName =>
-//            val method = MethodData.SimpleMethod.copy(methodName = methodName)
-//            api.methodConfigurations.createMethodConfigInWorkspace(billingProject, workspaceName, method,
-//              SimpleMethodConfig.configNamespace, methodConfigName, 1, SimpleMethodConfig.inputs, SimpleMethodConfig.outputs, "participant")
-//            api.methods.redact(method)
+    "launch analysis and delete errors on redacted method" in {
+      val user = UserPool.chooseProjectOwner
+      implicit val authToken: AuthToken = user.makeAuthToken()
+      withCleanBillingProject(user) { billingProject =>
+        withWorkspace(billingProject, "MethodRedactedSpec_delete") { workspaceName =>
+          withMethod("MethodRedactedSpec_delete", MethodData.SimpleMethod, cleanUp = false) { methodName =>
+            val method = MethodData.SimpleMethod.copy(methodName = methodName)
+
+            Orchestration.methodConfigurations.createMethodConfigInWorkspace(
+              billingProject,
+              workspaceName,
+              method,
+              SimpleMethodConfig.configNamespace,
+              SimpleMethodConfig.configName,
+              1,
+              SimpleMethodConfig.inputs,
+              SimpleMethodConfig.outputs,
+              SimpleMethodConfig.rootEntityType)
+
+            Orchestration.methods.redact(method)
+
+            val participantId = "123"
+            Orchestration.importMetaData(billingProject, workspaceName, "participant", s"entity:participant_id\n$participantId")
+
+            val launchException = intercept[RestException]{ Orchestration.submissions.launchWorkflow(
+              billingProject,
+              workspaceName,
+              SimpleMethodConfig.configNamespace,
+              SimpleMethodConfig.configName,
+              SimpleMethodConfig.rootEntityType,
+              participantId,
+              "this",
+              false)}
+            println("launchException " + launchException)
+            launchException.message.parseJson.asJsObject.fields("statusCode").convertTo[Int] should be (403)
+
+
+            //            // should be able to be deleted when no unredacted snapshot exists
+//            methodConfigDetailsPage.openEditMode(expectSuccess = false)
+//            val modal = await ready new MessageModal()
+//            modal.isVisible shouldBe true
+//            modal.getMessageText should include("There are no available method snapshots")
+//            modal.clickOk()
 //
-//            withWebDriver { implicit driver =>
-//              withSignIn(user) { _ =>
-//                // launch analysis error:
-//                // should have warning icon in config list
-//                val workspaceMethodConfigPage = new WorkspaceMethodConfigListPage(billingProject, workspaceName).open
-//                workspaceMethodConfigPage.hasRedactedIcon(methodConfigName) shouldBe true
-//
-//                // should be disabled and show error if clicked
-//                val methodConfigDetailsPage = new WorkspaceMethodConfigDetailsPage(billingProject, workspaceName, SimpleMethodConfig.configNamespace, methodConfigName).open
-//                val errorModal = methodConfigDetailsPage.clickLaunchAnalysisButtonError()
-//                errorModal.isVisible shouldBe true
-//                errorModal.clickOk() // dismiss modal
-//
-//                // should be able to be deleted when no unredacted snapshot exists
-//                methodConfigDetailsPage.openEditMode(expectSuccess = false)
-//                val modal = await ready new MessageModal()
-//                modal.isVisible shouldBe true
-//                modal.getMessageText should include("There are no available method snapshots")
-//                modal.clickOk()
-//
-//                methodConfigDetailsPage.deleteMethodConfig()
-//                val list = await ready new WorkspaceMethodConfigListPage(billingProject, workspaceName)
-//                list.hasConfig(methodConfigName) shouldBe false
-//              }
-//            }
-//          }
-//        }
-//      }
-//    }
+//            methodConfigDetailsPage.deleteMethodConfig()
+//            val list = await ready new WorkspaceMethodConfigListPage(billingProject, workspaceName)
+//            list.hasConfig(methodConfigName) shouldBe false
+          }
+        }
+      }
+    }
   }
 
 
