4d2d7f8fee2cb9b0232d2ff366415d33559f2322
David An <davidan@broadinstitute.org>
2019-10-11 13:51:10 -0400

    fix Jackson assembly merge problems

--- project/Merging.scala
+++ project/Merging.scala
@@ -1,8 +1,10 @@
 import sbtassembly.{MergeStrategy, PathList}
 
-object Merging {
+object  Merging {
   def customMergeStrategy(oldStrategy: (String) => MergeStrategy):(String => MergeStrategy) = {
     case x if x.endsWith("io.netty.versions.properties") => MergeStrategy.discard
+    // we target Java 8, which does not use module-info.class. Some dependencies (Jackson) cause assembly problems on module-info
+    case x if x.endsWith("module-info.class") => MergeStrategy.discard
     case x => oldStrategy(x)
   }
 }
