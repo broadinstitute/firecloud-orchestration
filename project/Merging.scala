8b73d9308d2ba48231ad320b0a7760bf9d636dd4
David An <davidan@broadinstitute.org>
2019-10-13 11:02:40 -0400

    remove extra space character

--- project/Merging.scala
+++ project/Merging.scala
@@ -1,6 +1,6 @@
 import sbtassembly.{MergeStrategy, PathList}
 
-object  Merging {
+object Merging {
   def customMergeStrategy(oldStrategy: (String) => MergeStrategy):(String => MergeStrategy) = {
     case x if x.endsWith("io.netty.versions.properties") => MergeStrategy.discard
     // we target Java 8, which does not use module-info.class. Some dependencies (Jackson) cause assembly problems on module-info
