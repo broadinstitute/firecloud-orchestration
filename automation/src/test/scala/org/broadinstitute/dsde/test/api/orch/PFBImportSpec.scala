8e5716f8cfb59a234839104d97db539481cf052a
David An <davidan@broadinstitute.org>
2020-09-03 11:19:56 -0400

    positive test

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/orch/PFBImportSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/orch/PFBImportSpec.scala
@@ -2,7 +2,10 @@ package org.broadinstitute.dsde.test.api.orch
 
 import java.util.UUID
 
+import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.{ActorMaterializer, Materializer}
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.{Credentials, ServiceTestConfig, UserPool}
 import org.broadinstitute.dsde.workbench.fixture.{BillingFixtures, WorkspaceFixtures}
@@ -11,19 +14,32 @@ import org.broadinstitute.dsde.workbench.model.ErrorReportJsonSupport.ErrorRepor
 import org.broadinstitute.dsde.workbench.service.{AclEntry, Orchestration, RestException, WorkspaceAccessLevel}
 import org.scalatest.{FreeSpec, Matchers}
 import org.scalatest.OptionValues._
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time.{Seconds, Span}
+
+import scala.concurrent.duration._
 import spray.json.DefaultJsonProtocol._
 import spray.json._
 
+import scala.concurrent.ExecutionContext
+import scala.io.Source
 
-class PFBImportSpec extends FreeSpec with Matchers
+class PFBImportSpec extends FreeSpec with Matchers with Eventually
   with BillingFixtures with WorkspaceFixtures {
 
   val owner: Credentials = UserPool.chooseProjectOwner
   val ownerAuthToken: AuthToken = owner.makeAuthToken()
 
+  implicit val system: ActorSystem = ActorSystem("PFBImportSpec")
+  implicit val context: ExecutionContext = system.dispatcher
+  // final implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(context.system))
+  implicit val materializer: Materializer = ActorMaterializer()
+  implicit override val patienceConfig: PatienceConfig = PatienceConfig(timeout = scaled(Span(300, Seconds)), interval = scaled(Span(2, Seconds)))
+
   // maybe steal the test.avro from https://github.com/uc-cdis/pypfb/tree/master/tests/pfb-data ?
   // or do a small export from BDC staging env ?
-  val testPayload = Map("url" -> "gs://fixtures-for-tests/example.pfb")
+  val testPayload = Map("url" -> "https://storage.googleapis.com/fixtures-for-tests/fixtures/public/test.avro")
+  val expectedEntities: JsValue = Source.fromResource("PFBImportSpec-expected-entities.json").getLines().mkString.parseJson
 
   "Orchestration" - {
 
@@ -35,9 +51,29 @@ class PFBImportSpec extends FreeSpec with Matchers
           withWorkspace(projectName, prependUUID("owner-pfb-import")) { workspaceName =>
             // Orchestration.workspaces.waitForBucketReadAccess(projectName, workspaceName)
 
-            // TODO: call importPFB as owner
-            // TODO: poll for completion as owner
-            // TODO: inspect data entities and confirm correct import as owner
+            // call importPFB as owner
+            val postResponse: String = Orchestration.postRequest(importURL(projectName, workspaceName), testPayload)
+            // expect to get exactly one jobId back
+            val importJobIdValues: Seq[JsValue] = postResponse.parseJson.asJsObject.getFields("jobId")
+            importJobIdValues should have size 1
+            val importJobId: String = importJobIdValues.head.toString
+
+            // poll for completion as owner
+            eventually {
+              val resp = Orchestration.getRequest( s"${importURL(projectName, workspaceName)}/$importJobId")
+              Unmarshal(resp.entity).to[String] map { respString =>
+                respString.parseJson.asJsObject.fields.get("status").value shouldBe "Done"
+              }
+            }
+
+            // inspect data entities and confirm correct import as owner
+            eventually {
+              val resp = Orchestration.getRequest( s"${importURL(projectName, workspaceName)}/entities")
+              Unmarshal(resp.entity).to[String] map { respString =>
+                respString.parseJson shouldBe expectedEntities
+              }
+            }
+
           }
         }
       }
@@ -59,6 +95,7 @@ class PFBImportSpec extends FreeSpec with Matchers
     }
 
     "should return an error when attempting to import a PFB file via import service" - {
+
       "for readers of a workspace" in {
         val reader = UserPool.chooseStudent
 
@@ -79,6 +116,11 @@ class PFBImportSpec extends FreeSpec with Matchers
           } (ownerAuthToken)
         }
       }
+
+      "with an invalid POST payload" ignore {}
+      "if the PFB does not exist" ignore {}
+      "if the PFB is invalid" ignore {}
+
     }
   }
 
