4b425dc8546ba4a857979788e6853453f158d663
Matt Bemis <mbemis@broadinstitute.org>
2020-10-21 15:08:23 -0400

    rework rejection handlers

--- src/main/scala/org/broadinstitute/dsde/firecloud/model/package.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/model/package.scala
@@ -1,14 +1,10 @@
 package org.broadinstitute.dsde.firecloud
 
-import org.broadinstitute.dsde.rawls.model.{ErrorReport, ErrorReportSource}
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.marshalling.Marshal
-import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
-import akka.http.scaladsl.marshalling
-import akka.http.scaladsl.model.{ContentType, HttpEntity, HttpResponse, StatusCodes}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
+import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{MalformedRequestContentRejection, RejectionHandler}
+import org.broadinstitute.dsde.rawls.model.{ErrorReport, ErrorReportSource}
 
 import scala.language.implicitConversions
 
@@ -16,20 +12,20 @@ package object model {
   implicit val errorReportSource = ErrorReportSource("FireCloud")
 
   import org.broadinstitute.dsde.rawls.model.WorkspaceJsonSupport._
-  import akka.http.scaladsl.marshalling.{Marshaller, ToResponseMarshaller}
+  import spray.json._
 
-//  implicit val errorReportRejectionHandler = RejectionHandler.newBuilder().handle {
-//    case MalformedRequestContentRejection(errorMsg, _) =>
-//      complete { (StatusCodes.BadRequest, ErrorReport(StatusCodes.BadRequest, errorMsg)) }
-//    case _ => RejectionHandler.default.mapRejectionResponse {
-//      case resp@HttpResponse(statusCode, _, ent: HttpEntity.Strict, _) => {
-//        // since all Akka default rejection responses are Strict this will handle all rejections
-//        val message = ent.data.utf8String.replaceAll("\"", """\"""")
-//
-//        //TODO: wrap this message in an ErrorReport a la ErrorReport(statusCode, message))
-//        resp.withEntity(HttpEntity(ContentTypes.`application/json`, message))
-//      }
-//    }
-//  }
+  implicit val defaultErrorReportRejectionHandler = RejectionHandler.default.mapRejectionResponse {
+    case resp@HttpResponse(statusCode, _, ent: HttpEntity.Strict, _) => {
+      // since all Akka default rejection responses are Strict this will handle all rejections
+      val message = ent.data.utf8String.replaceAll("\"", """\"""")
+
+      resp.withEntity(HttpEntity(ContentTypes.`application/json`, ErrorReport(statusCode, message).toJson.toString))
+    }
+  }
+
+  implicit val malformedRequestContentRejectionHandler = RejectionHandler.newBuilder().handle {
+    case MalformedRequestContentRejection(errorMsg, _) =>
+      complete { (StatusCodes.BadRequest, ErrorReport(StatusCodes.BadRequest, errorMsg)) }
+  }
 
 }
