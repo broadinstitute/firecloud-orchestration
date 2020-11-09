ea424e608b037a4777d9216f5f78884cb3dde6e8
Matt Bemis <mbemis@broadinstitute.org>
2020-11-09 17:17:00 -0500

    update some other references to scala 2.11

--- src/docker/run.sh
+++ src/docker/run.sh
@@ -3,8 +3,8 @@ set -euox pipefail
 IFS=$'\n\t'
 
 
-if [ -e /app/target/scala-2.11/FireCloud-Orchestration-assembly-*.jar ]; then
-  exec java $JAVA_OPTS -jar /app/target/scala-2.11/FireCloud-Orchestration-assembly-*.jar
+if [ -e /app/target/scala-2.12/FireCloud-Orchestration-assembly-*.jar ]; then
+  exec java $JAVA_OPTS -jar /app/target/scala-2.12/FireCloud-Orchestration-assembly-*.jar
 else
   cd /app
   exec sbt '~ reStart'
