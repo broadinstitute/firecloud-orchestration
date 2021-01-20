3f7b8b4fe189e5b9247a5fcfa82b22ab4a2c8540
David An <davidan@broadinstitute.org>
2021-01-20 14:27:21 -0500

    it:test bash script for jenkins to call

---
+++ jenkins/ittests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eux
+
+# start elasticsearch and pre-populate test data
+./docker/run-es.sh start
+
+# execute tests, overriding elasticsearch.urls to point at the linked container
+SBT_IMAGE=hseeberger/scala-sbt:8u265_1.4.4_2.12.12
+docker run --rm \
+  --link elasticsearch-ittest:elasticsearch-ittest \
+  -v sbt-cache:/root/.sbt \
+  -v jar-cache:/root/.ivy2 \
+  -v coursier-cache:/root/.cache/coursier \
+  -v $PWD:/working \
+  -w /working \
+  $SBT_IMAGE sbt it:test -Delasticsearch.urls=elasticsearch-ittest:9300
+
+# stop elasticsearch
+./docker/run-es.sh stop
