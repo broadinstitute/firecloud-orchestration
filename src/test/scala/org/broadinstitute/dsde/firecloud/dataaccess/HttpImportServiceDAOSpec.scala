ef24850e095f214dcd9d0b90f0fd3847528878f2
David An <davidan@broadinstitute.org>
2021-04-01 11:46:45 -0400

    TDD: unit tests, one is known to fail

--- src/test/scala/org/broadinstitute/dsde/firecloud/dataaccess/HttpImportServiceDAOSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/dataaccess/HttpImportServiceDAOSpec.scala
@@ -4,9 +4,10 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse}
+import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import org.broadinstitute.dsde.firecloud.model.ModelJsonProtocol.{impImportServiceResponse, impPfbImportResponse}
-import org.broadinstitute.dsde.firecloud.model.{ImportServiceResponse, PfbImportRequest, PfbImportResponse}
+import org.broadinstitute.dsde.firecloud.model.{ImportServiceResponse, PfbImportRequest, PfbImportResponse, RequestCompleteWithErrorReport}
 import org.broadinstitute.dsde.firecloud.service.PerRequest.RequestComplete
 import org.broadinstitute.dsde.rawls.model.WorkspaceName
 import org.scalatest.flatspec.AsyncFlatSpec
@@ -27,24 +28,57 @@ class HttpImportServiceDAOSpec extends AsyncFlatSpec with Matchers with SprayJso
   val pfbUrl = "unittest-fixture-url"
   val pfbRequest = PfbImportRequest(Some(pfbUrl))
 
-  it should "return Accepted if import service returns Created" in {
-    val jobId = UUID.randomUUID()
-    val importServicePayload = ImportServiceResponse(jobId.toString, "status", None).toJson.compactPrint
+  val jobId = UUID.randomUUID()
+  val importServicePayload = ImportServiceResponse(jobId.toString, "status", None).toJson.compactPrint
 
+  val expectedSuccessPayload = PfbImportResponse(
+    pfbUrl,
+    jobId.toString,
+    WorkspaceName(workspaceNamespace, workspaceName))
+
+  val expectedErrorPayload = s"this is a random error string: ${UUID.randomUUID()}"
+
+  it should "return Accepted if import service returns Created with a strict entity" in {
     val importServiceResponse = HttpResponse(status = Created,
-      entity = HttpEntity.Strict(ContentTypes.`application/json`, ByteString(importServicePayload.getBytes)))
+      entity = HttpEntity.Strict(ContentTypes.`application/json`, ByteString(importServicePayload)))
 
     val futureActual = new HttpImportServiceDAO().generateResponse(importServiceResponse, workspaceNamespace, workspaceName, pfbRequest)
 
-    val expectedPayload = PfbImportResponse(
-      pfbUrl,
-      jobId.toString,
-      WorkspaceName(workspaceNamespace, workspaceName))
-
     futureActual map { actual =>
-      actual shouldBe RequestComplete(Accepted, expectedPayload)
+      actual shouldBe RequestComplete(Accepted, expectedSuccessPayload)
     }
-
   }
 
+  it should "return Accepted if import service returns Created with a non-strict entity" in {
+    val importServiceResponse = HttpResponse(status = Created,
+      entity = HttpEntity.Default(ContentTypes.`application/json`, importServicePayload.getBytes.length, Source.single(ByteString(importServicePayload))))
+
+    val futureActual = new HttpImportServiceDAO().generateResponse(importServiceResponse, workspaceNamespace, workspaceName, pfbRequest)
+
+    futureActual map { actual =>
+      actual shouldBe RequestComplete(Accepted, expectedSuccessPayload)
+    }
+  }
+
+  it should "bubble up status code and payload if import service returns non-Created with a strict entity" in {
+    val importServiceResponse = HttpResponse(status = ImATeapot,
+      entity = HttpEntity.Strict(ContentTypes.`text/plain(UTF-8)`, ByteString(expectedErrorPayload)))
+
+    val futureActual = new HttpImportServiceDAO().generateResponse(importServiceResponse, workspaceNamespace, workspaceName, pfbRequest)
+
+    futureActual map { actual =>
+      actual shouldBe RequestCompleteWithErrorReport(ImATeapot, expectedErrorPayload)
+    }
+  }
+
+  it should "bubble up status code and payload if import service returns non-Created with a non-strict entity" in {
+    val importServiceResponse = HttpResponse(status = ImATeapot,
+      entity = HttpEntity.Default(ContentTypes.`text/plain(UTF-8)`, expectedErrorPayload.getBytes.length, Source.single(ByteString(expectedErrorPayload))))
+
+    val futureActual = new HttpImportServiceDAO().generateResponse(importServiceResponse, workspaceNamespace, workspaceName, pfbRequest)
+
+    futureActual map { actual =>
+      actual shouldBe RequestCompleteWithErrorReport(ImATeapot, expectedErrorPayload)
+    }
+  }
 }
