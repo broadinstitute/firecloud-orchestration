d666c576f4881a8fe4e0f3d252f744edaaaa7349
ungwudik <ungwudik@broadinstitute.org>
2022-01-20 11:38:18 -0500

    Accept ToS via sam - Initial commit

--- src/main/scala/org/broadinstitute/dsde/firecloud/model/JWT.scala
+++
@@ -1,8 +0,0 @@
-package org.broadinstitute.dsde.firecloud.model
-
-
-case class JWTWrapper(
-  jwt: String
-)
-
-
--- src/main/scala/org/broadinstitute/dsde/firecloud/service/CommonMessages.scala
+++
@@ -1,5 +0,0 @@
-package org.broadinstitute.dsde.firecloud.service
-
-object CommonMessages {
-  case class ClientFailure(message: String)
-}
