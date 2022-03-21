aa2a4825f115942b9603ed36e4143ddaa7845fb5
Matt Bemis <mbemis@broadinstitute.org>
2022-03-21 15:19:39 -0400

    support for importEntities path

--- src/main/scala/org/broadinstitute/dsde/firecloud/webservice/WorkspaceApiService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/webservice/WorkspaceApiService.scala
@@ -130,8 +130,13 @@ trait WorkspaceApiService extends FireCloudRequestBuilding with FireCloudDirecti
                 path("importEntities") {
                   post {
                     requireUserInfo() { userInfo =>
-                      formFields('entities) { entitiesTSV =>
-                        complete { entityServiceConstructor(FirecloudModelSchema).importEntitiesFromTSV(workspaceNamespace, workspaceName, entitiesTSV, userInfo) }
+                      parameter("deleteEmptyValues" ? "false") { deleteEmptyValuesStr =>
+                        formFields('entities) { entitiesTSV =>
+                          complete {
+                            val deleteEmptyValues = java.lang.Boolean.valueOf(deleteEmptyValuesStr) // for lenient parsing
+                            entityServiceConstructor(FirecloudModelSchema).importEntitiesFromTSV(workspaceNamespace, workspaceName, entitiesTSV, userInfo, deleteEmptyValues = deleteEmptyValues)
+                          }
+                        }
                       }
                     }
                   }
