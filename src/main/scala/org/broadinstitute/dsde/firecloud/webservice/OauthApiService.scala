c4be906f08579876c1e05de7c181f585b200613d
Robert Title <rtitle@broadinstitute.org>
2022-04-11 08:39:00 -0400

    Google authorize endpoint

--- src/main/scala/org/broadinstitute/dsde/firecloud/webservice/OauthApiService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/webservice/OauthApiService.scala
@@ -1,7 +1,7 @@
 package org.broadinstitute.dsde.firecloud.webservice
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{StatusCodes, Uri}
 import akka.http.scaladsl.server.Route
 import org.broadinstitute.dsde.firecloud.service.FireCloudDirectives
 import org.broadinstitute.dsde.firecloud.service.PerRequest.RequestComplete
@@ -10,29 +10,21 @@ import spray.json.DefaultJsonProtocol._
 
 import scala.concurrent.ExecutionContext
 
-/**
-  * These two routes are no-ops. The backend behavior they previously supported is no longer necessary:
-  * - reporting on how old the user's refresh token is;
-  * - retrieving a new oauth token from Google and sending it to Rawls to store
-  *
-  * In the spirit of backwards compatibility, we are not (yet) removing these APIs. Instead, we hardcode
-  * them to respond as if they were successful, without performing any backend work.
-  */
 trait OauthApiService extends FireCloudDirectives with StandardUserInfoDirectives with SprayJsonSupport {
 
   implicit val executionContext: ExecutionContext
+  val googleOauth = "https://accounts.google.com/o/oauth2/v2/auth"
 
-  val oauthRoutes: Route =
-    path("handle-oauth-code") {
-      post {
-        complete(StatusCodes.NoContent)
+  val oauthRoutes: Route = {
+    path("authorize") {
+      get {
+        redirect(Uri(googleOauth), StatusCodes.Found)
       }
     } ~
-      path("api" / "refresh-token-status") {
-        get {
-          requireUserInfo() { _ =>
-            complete { RequestComplete(StatusCodes.OK, Map("requiresRefresh" -> false)) }
-          }
+      path("token") {
+        post {
+          complete(StatusCodes.NoContent)
         }
       }
+  }
 }
