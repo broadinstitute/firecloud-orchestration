35e04ab0790205fcdf3da6544c2e569782e31695
David An <davidan@broadinstitute.org>
2022-05-09 17:57:16 -0400

    revert google-api-client, standardize on workbench- hash, syntax tweaks

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -7,19 +7,20 @@ object Dependencies {
   val jacksonHotfixV = "2.13.2.2" // for when only some of the Jackson libs have hotfix releases
   val akkaV = "2.6.15"
   val akkaHttpV = "10.2.0"
+  val workbenchLibsHash = "89b188f"
 
-  val workbenchModelV  = "0.15-808590d"
+  val workbenchModelV  = s"0.15-$workbenchLibsHash"
   val workbenchModel: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-model" % workbenchModelV
   val excludeWorkbenchModel = ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-model_" + scalaV)
 
-  val workbenchGoogleV = "0.21-808590d"
-  val workbenchGoogle: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-google" % workbenchGoogleV excludeAll (excludeWorkbenchModel)
+  val workbenchGoogleV = s"0.21-$workbenchLibsHash"
+  val workbenchGoogle: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-google" % workbenchGoogleV excludeAll excludeWorkbenchModel
   val excludeWorkbenchGoogle = ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-google_" + scalaV)
 
-  val workbenchServiceTestV = "2.0-89b188f"
+  val workbenchServiceTestV = s"2.0-$workbenchLibsHash"
   val workbenchServiceTest: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-service-test" % workbenchServiceTestV % "test" classifier "tests" excludeAll (excludeWorkbenchGoogle, excludeWorkbenchModel)
 
-  val rootDependencies = Seq(
+  val rootDependencies: Seq[ModuleID] = Seq(
     // proactively pull in latest versions of Jackson libs, instead of relying on the versions
     // specified as transitive dependencies, due to OWASP DependencyCheck warnings for earlier versions.
     "com.fasterxml.jackson.core" % "jackson-annotations" % jacksonV,
@@ -32,7 +33,7 @@ object Dependencies {
       ExclusionRule("com.google.guava", "guava-jdk5"),
       ExclusionRule("org.apache.httpcomponents", "httpclient")
     ),
-    "com.google.api-client" % "google-api-client" % "1.34.0" excludeAll (
+    "com.google.api-client" % "google-api-client" % "1.22.0" excludeAll (
       ExclusionRule("com.google.guava", "guava-jdk5"),
       ExclusionRule("org.apache.httpcomponents", "httpclient")),
     "com.typesafe.akka"   %%  "akka-http-core"     % akkaHttpV,
