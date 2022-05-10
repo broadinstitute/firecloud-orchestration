80c0bb16be88b501563ae517fbfd9dbbb7b4826e
David An <davidan@broadinstitute.org>
2022-05-10 13:33:23 -0400

    Update automation dependencies [AJ-414] (#948)

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -3,48 +3,45 @@ import sbt._
 object Dependencies {
   val scalaV = "2.13"
 
-  val jacksonV = "2.12.1"
-  val akkaV = "2.6.15"
-  val akkaHttpV = "10.2.0"
+  val jacksonV = "2.13.2"
+  val jacksonHotfixV = "2.13.2.2" // for when only some of the Jackson libs have hotfix releases
+  val akkaV = "2.6.19"
+  val akkaHttpV = "10.2.9"
+  val workbenchLibsHash = "89b188f"
 
-  val workbenchModelV  = "0.15-808590d"
+  val workbenchModelV  = s"0.15-$workbenchLibsHash"
   val workbenchModel: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-model" % workbenchModelV
   val excludeWorkbenchModel = ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-model_" + scalaV)
 
-  val workbenchGoogleV = "0.21-808590d"
-  val workbenchGoogle: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-google" % workbenchGoogleV excludeAll (excludeWorkbenchModel)
+  val workbenchGoogleV = s"0.21-$workbenchLibsHash"
+  val workbenchGoogle: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-google" % workbenchGoogleV excludeAll excludeWorkbenchModel
   val excludeWorkbenchGoogle = ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-google_" + scalaV)
 
-  val workbenchServiceTestV = "2.0-89b188f"
+  val workbenchServiceTestV = s"2.0-$workbenchLibsHash"
   val workbenchServiceTest: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-service-test" % workbenchServiceTestV % "test" classifier "tests" excludeAll (excludeWorkbenchGoogle, excludeWorkbenchModel)
 
-  val rootDependencies = Seq(
+  val rootDependencies: Seq[ModuleID] = Seq(
     // proactively pull in latest versions of Jackson libs, instead of relying on the versions
     // specified as transitive dependencies, due to OWASP DependencyCheck warnings for earlier versions.
     "com.fasterxml.jackson.core" % "jackson-annotations" % jacksonV,
-    "com.fasterxml.jackson.core" % "jackson-databind" % jacksonV,
+    "com.fasterxml.jackson.core" % "jackson-databind" % jacksonHotfixV,
     "com.fasterxml.jackson.core" % "jackson-core" % jacksonV,
-    "com.fasterxml.jackson.module" % ("jackson-module-scala_" + scalaV) % jacksonV,
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonV,
     "net.virtual-void" %% "json-lenses" % "0.6.2" % "test",
-    "ch.qos.logback" % "logback-classic" % "1.2.3",
-    "com.google.apis" % "google-api-services-oauth2" % "v1-rev127-1.22.0" excludeAll (
-      ExclusionRule("com.google.guava", "guava-jdk5"),
-      ExclusionRule("org.apache.httpcomponents", "httpclient")
-    ),
-    "com.google.api-client" % "google-api-client" % "1.22.0" excludeAll (
-      ExclusionRule("com.google.guava", "guava-jdk5"),
-      ExclusionRule("org.apache.httpcomponents", "httpclient")),
+    "ch.qos.logback" % "logback-classic" % "1.2.11",
     "com.typesafe.akka"   %%  "akka-http-core"     % akkaHttpV,
     "com.typesafe.akka"   %%  "akka-stream-testkit" % akkaV,
     "com.typesafe.akka"   %%  "akka-http"           % akkaHttpV,
+    "com.typesafe.akka"   %%  "akka-http-spray-json" % akkaHttpV,
     "com.typesafe.akka"   %%  "akka-testkit"        % akkaV     % "test",
     "com.typesafe.akka"   %%  "akka-slf4j"          % akkaV,
     "org.specs2"          %%  "specs2-core"   % "4.15.0"  % "test",
-    "org.scalatest"       %%  "scalatest"     % "3.2.2"   % Test,
-    "org.seleniumhq.selenium" % "selenium-java" % "3.11.0" % "test",
+    "org.scalatest"       %%  "scalatest"     % "3.2.12"   % Test,
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4",
 
-    "net.logstash.logback" % "logstash-logback-encoder" % "6.6", // needed by workbench-google
+    // required but not provided by workbench-google.
+    // workbench-google specifies 7.0.1
+    "net.logstash.logback" % "logstash-logback-encoder" % "7.1.1",
 
     workbenchServiceTest,
     workbenchModel,
