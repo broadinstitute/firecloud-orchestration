49ab2bfa90fb4c41b61c8c58ca6c7ffb82b11c28
David An <davidan@broadinstitute.org>
2022-07-12 16:40:20 -0400

    try to make FireCloudExceptionWithErrorReport messages clearer

--- src/main/scala/org/broadinstitute/dsde/firecloud/FireCloudException.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/FireCloudException.scala
@@ -4,4 +4,4 @@ import org.broadinstitute.dsde.rawls.model.ErrorReport
 
 class FireCloudException(message: String = null, cause: Throwable = null) extends Exception(message, cause)
 
-class FireCloudExceptionWithErrorReport(val errorReport: ErrorReport) extends FireCloudException(errorReport.toString)
+class FireCloudExceptionWithErrorReport(val errorReport: ErrorReport) extends FireCloudException(s"${errorReport.message}: ${errorReport.toString}")
