1b0ae0f005c11e7aa1443d43af8bcd7794529a8e
broadbot <broadbot@broadinstitute.org>
2022-08-08 12:01:21 +0000

    Update jackson-annotations, jackson-core from 2.13.2 to 2.13.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val akkaV = "2.5.32"
   val akkaHttpV = "10.2.9"
-  val jacksonV = "2.13.2"
+  val jacksonV = "2.13.3"
   val jacksonHotfixV = "2.13.2.2" // for when only some of the Jackson libs have hotfix releases
   val nettyV = "4.1.77.Final"
   val workbenchLibsHash = "20f9225"
