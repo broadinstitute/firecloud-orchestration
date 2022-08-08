a0c84ce91bc66daca3288c93e24d2579c39e0c6b
David An <davidan@broadinstitute.org>
2022-08-08 09:57:47 -0400

    cleanup

--- src/main/scala/org/broadinstitute/dsde/firecloud/utils/EnabledUserDirectives.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/utils/EnabledUserDirectives.scala
@@ -2,12 +2,11 @@ package org.broadinstitute.dsde.firecloud.utils
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.{StatusCode, StatusCodes}
-import akka.http.scaladsl.server.Directives.{complete, extractUri}
+import akka.http.scaladsl.server.Directives.extractUri
 import akka.http.scaladsl.server.{RequestContext, Route, RouteResult}
 import com.typesafe.scalalogging.LazyLogging
-import org.broadinstitute.dsde.firecloud.{FireCloudConfig, FireCloudExceptionWithErrorReport}
 import org.broadinstitute.dsde.firecloud.model.UserInfo
-import org.broadinstitute.dsde.rawls.model.WorkspaceJsonSupport.ErrorReportFormat
+import org.broadinstitute.dsde.firecloud.{FireCloudConfig, FireCloudExceptionWithErrorReport}
 import org.broadinstitute.dsde.rawls.model.{ErrorReport, ErrorReportSource}
 import org.broadinstitute.dsde.workbench.client.sam.api.UsersApi
 import org.broadinstitute.dsde.workbench.client.sam.{ApiClient, ApiException}
@@ -16,8 +15,6 @@ import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
 
 trait EnabledUserDirectives extends LazyLogging  with SprayJsonSupport {
-
-  implicit val errorReportSource: ErrorReportSource = ErrorReportSource("enabled-check")
 
   /**
     * Queries Sam to see if the current user is enabled. If the user is disabled,
@@ -31,22 +28,20 @@ trait EnabledUserDirectives extends LazyLogging  with SprayJsonSupport {
     extractUri { uri =>
       Try(getUserEnabled(userInfo.accessToken.token, samBaseUrl)) match {
         case Success(true) =>
-          logger.debug(s"User ${userInfo.userEmail} is enabled: ${uri}")
+          logger.debug(s"User ${userInfo.userEmail} is enabled: $uri")
           innerRoute
         case Success(false) =>
-          logger.warn(s"User ${userInfo.userEmail} is disabled: ${uri}")
-          throw new FireCloudExceptionWithErrorReport(ErrorReport(StatusCodes.Forbidden, StatusCodes.Forbidden.defaultMessage))
-          // complete(StatusCodes.Forbidden) // TODO: do we want a more helpful message like "user is disabled"?
+          logger.warn(s"User ${userInfo.userEmail} is disabled: $uri")
+          // TODO: do we want a more helpful message like "user is disabled"?
+          throwErrorReport(StatusCodes.Forbidden, StatusCodes.Forbidden.defaultMessage)
         case Failure(ex) => ex match {
           case apiex:ApiException =>
-            logger.error(s"Exception checking enabled status for user ${userInfo.userEmail}: (${apiex.getMessage}) while calling ${uri}", apiex)
+            logger.error(s"Exception checking enabled status for user ${userInfo.userEmail}: (${apiex.getMessage}) while calling $uri", apiex)
             val code = StatusCode.int2StatusCode(apiex.getCode)
-            throw new FireCloudExceptionWithErrorReport(ErrorReport(code, apiex.getMessage))
-//            complete(code, ErrorReport(code, apiex.getMessage))
+            throwErrorReport(code, apiex.getMessage)
           case ex =>
-            logger.error(s"Exception checking enabled status for user ${userInfo.userEmail}: (${ex.getMessage}) while calling ${uri}", ex)
-            throw new FireCloudExceptionWithErrorReport(ErrorReport(StatusCodes.InternalServerError, ex.getMessage))
-//            complete(StatusCodes.InternalServerError, ErrorReport(StatusCodes.InternalServerError, ex.getMessage))
+            logger.error(s"Exception checking enabled status for user ${userInfo.userEmail}: (${ex.getMessage}) while calling $uri", ex)
+            throwErrorReport(StatusCodes.InternalServerError, ex.getMessage)
         }
       }
     }
@@ -58,7 +53,23 @@ trait EnabledUserDirectives extends LazyLogging  with SprayJsonSupport {
     apiClient.setBasePath(samBaseUrl)
     val sam = new UsersApi(apiClient)
     val userStatus = sam.getUserStatusInfo
+    // TODO: is it enough to check this one `getEnabled` boolean?
+    // TODO: retries?
     userStatus.getEnabled
+  }
+
+  /**
+    * Constructs and throws a FireCloudExceptionWithErrorReport in response to the
+    * enabled-user check. Hardcodes an ErrorReportSource to allow differentiating
+    * between enabled-user errors and other errors.
+    *
+    * @param statusCode the http status code to throw
+    * @param message message to use in the thrown exception
+    * @return nothing
+    */
+  private def throwErrorReport(statusCode: StatusCode, message: String): Nothing = {
+    val errRpt = ErrorReport(statusCode, message)(ErrorReportSource("enabled-check"))
+    throw new FireCloudExceptionWithErrorReport(errRpt)
   }
 
 }
