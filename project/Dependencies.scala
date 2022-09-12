c7260e0c2499ccf00dbf8a6b08aa1064d0393446
broadbot <broadbot@broadinstitute.org>
2022-09-12 12:01:36 +0000

    Update akka-actor, akka-slf4j, akka-stream, ... from 2.5.32 to 2.6.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val akkaV = "2.5.32"
+  val akkaV = "2.6.20"
   val akkaHttpV = "10.2.9"
   val jacksonV = "2.13.3"
   val jacksonHotfixV = "2.13.3" // for when only some of the Jackson libs have hotfix releases
