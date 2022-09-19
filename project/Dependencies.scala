2a1b0f9368f9c73c4da8729eafc7ad39b219d695
broadbot <broadbot@broadinstitute.org>
2022-09-19 12:01:20 +0000

    Update log4j-to-slf4j from 2.17.2 to 2.19.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     // END transitive dependency overrides
 
     // elasticsearch requires log4j, but we redirect log4j to logback
-    "org.apache.logging.log4j"       % "log4j-to-slf4j"      % "2.17.2",
+    "org.apache.logging.log4j"       % "log4j-to-slf4j"      % "2.19.0",
     "ch.qos.logback"                 % "logback-classic"     % "1.2.11",
     "com.getsentry.raven"            % "raven-logback"       % "8.0.3", // TODO: this should be io.sentry / sentry-logback instead
     "com.typesafe.scala-logging"    %% "scala-logging"       % "3.9.4",
