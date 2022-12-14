84119e9d04c5bb94d0652e5bb3770aa053d4f78c
David An <davidan@broadinstitute.org>
2022-12-14 13:58:55 -0500

    Update sbt-assembly from 1.2.0 to 2.1.0

--- src/docker/install.sh
+++ src/docker/install.sh
@@ -7,7 +7,7 @@ cd $ORCH_DIR
 
 sbt -batch -d clean reload update compile
 sbt -batch test
-sbt -batch assembly
+sbt --mem 2048 -batch assembly
 
 ORCH_JAR=$(find target | grep 'FireCloud-Orchestration.*\.jar')
 mv $ORCH_JAR .
