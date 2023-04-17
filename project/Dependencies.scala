cd27984f5c418ce782dc21895b5653d3d263bf3a
broadbot <broadbot@broadinstitute.org>
2023-04-17 12:01:06 +0000

    Update google-cloud-nio from 0.126.11 to 0.126.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -92,7 +92,7 @@ object Dependencies {
     // jaxb-api needed by WorkspaceApiServiceSpec.bagitService() method
     "javax.xml.bind"                 % "jaxb-api"            % "2.3.1"   % "test",
     // provides testing mocks
-    "com.google.cloud"               % "google-cloud-nio"    % "0.126.11" % "test",
+    "com.google.cloud"               % "google-cloud-nio"    % "0.126.13" % "test",
     "org.scalatestplus"             %% "mockito-4-5"         % "3.2.12.0" % "test"
   )
 }
