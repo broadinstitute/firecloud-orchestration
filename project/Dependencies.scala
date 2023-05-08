157b258b0bfd5ba954fbbbf7bedac21afaf37f07
broadbot <broadbot@broadinstitute.org>
2023-05-08 12:01:13 +0000

    Update google-api-services-pubsub from v1-rev20230218-2.0.0 to v1-rev20230425-2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -76,7 +76,7 @@ object Dependencies {
       exclude("org.apache.logging.log4j", "log4j-core"),
 
 
-    excludeGuava("com.google.apis"     % "google-api-services-pubsub"       % "v1-rev20230218-2.0.0"),
+    excludeGuava("com.google.apis"     % "google-api-services-pubsub"       % "v1-rev20230425-2.0.0"),
     excludeGuava("com.google.apis"     % "google-api-services-admin-directory"  % "directory_v1-rev20230425-2.0.0"),
 
 
