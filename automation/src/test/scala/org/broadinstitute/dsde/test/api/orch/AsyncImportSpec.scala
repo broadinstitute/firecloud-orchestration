e721268f87c22c2de054cb61cb6bacbdd8c02af9
aaronkanzer <kanzer.a@husky.neu.edu>
2023-05-11 16:57:47 -0400

    whoops

--- automation/src/test/scala/org/broadinstitute/dsde/test/api/orch/AsyncImportSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/test/api/orch/AsyncImportSpec.scala
@@ -47,187 +47,184 @@ class AsyncImportSpec
   lazy private val expectedEntities: Map[String, EntityTypeMetadata] = Source.fromResource("AsyncImportSpec-pfb-expected-entities.json").getLines().mkString
     .parseJson.convertTo[Map[String, EntityTypeMetadata]]
 
-// See https://broadworkbench.atlassian.net/browse/AJ-1036
-// These tests were consistently failing in Jenkins, and we need to reevaluate their utility in general, thus they are commented out for now.
-//  "Orchestration" - {
-//
-//    "should import a PFB file via import service" - {
-//      "for the owner of a workspace" in {
-//        implicit val token: AuthToken = ownerAuthToken
-//
-//        withTemporaryBillingProject(billingAccountId) { projectName =>
-//          withWorkspace(projectName, prependUUID("owner-pfb-import")) { workspaceName =>
-//            // call importPFB as owner
-//            val postResponse: String = Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB", testPayload)
-//            // expect to get exactly one jobId back
-//            val importJobIdValues: Seq[JsValue] = postResponse.parseJson.asJsObject.getFields("jobId")
-//            importJobIdValues should have size 1
-//
-//            val importJobId: String = importJobIdValues.head match {
-//              case js: JsString => js.value
-//              case x => fail("got an invalid jobId: " + x.toString())
-//            }
-//
-//            // poll for completion as owner
-//            eventuallyWithEscape {
-//              val resp: HttpResponse = Orchestration.getRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB/$importJobId")
-//              resp.status shouldBe StatusCodes.OK
-//              val importStatus = blockForStringBody(resp).parseJson.asJsObject.fields.get("status").value
-//              assertWithEscape(
-//                importStatus shouldBe JsString("Done"),
-//                importStatus should not equal JsString("Error"),
-//                "Error encountered during import, failing test early"
-//              )
-//            }
-//
-//            // inspect data entities and confirm correct import as owner
-//            eventually {
-//              val resp = Orchestration.getRequest(s"${ServiceTestConfig.FireCloud.orchApiUrl}api/workspaces/$projectName/$workspaceName/entities")
-//              compareMetadata(blockForStringBody(resp).parseJson.convertTo[Map[String, EntityTypeMetadata]], expectedEntities)
-//            }
-//
-//          }
-//        }(owner.makeAuthToken(billingScopes))
-//      }
+  "Orchestration" - {
 
-//      "for writers of a workspace" in {
-//        val writer = UserPool.chooseStudent
-//        val writerToken = writer.makeAuthToken()
-//
-//        withTemporaryBillingProject(billingAccountId) { projectName =>
-//          withWorkspace(projectName, prependUUID("writer-pfb-import"), aclEntries = List(AclEntry(writer.email, WorkspaceAccessLevel.Writer))) { workspaceName =>
-//            // call importPFB as writer
-//            val postResponse: String = Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB", testPayload)(writerToken)
-//            // expect to get exactly one jobId back
-//            val importJobIdValues: Seq[JsValue] = postResponse.parseJson.asJsObject.getFields("jobId")
-//            importJobIdValues should have size 1
-//            val importJobId: String = importJobIdValues.head match {
-//              case js: JsString => js.value
-//              case x => fail("got an invalid jobId: " + x.toString())
-//            }
-//
-//            // poll for completion as writer
-//            eventuallyWithEscape {
-//              val resp = Orchestration.getRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB/$importJobId")(writerToken)
-//              val importStatus = blockForStringBody(resp).parseJson.asJsObject.fields.get("status").value
-//              assertWithEscape(
-//                importStatus shouldBe JsString("Done"),
-//                importStatus should not equal JsString("Error"),
-//                "Error encountered during import, failing test early"
-//              )
-//            }
-//
-//            // inspect data entities and confirm correct import as writer
-//            eventually {
-//              val resp = Orchestration.getRequest(s"${workspaceUrl(projectName, workspaceName)}/entities")(writerToken)
-//              compareMetadata(blockForStringBody(resp).parseJson.convertTo[Map[String, EntityTypeMetadata]], expectedEntities)
-//            }
-//
-//          }(ownerAuthToken)
-//        }(owner.makeAuthToken(billingScopes))
-//      }
-//    }
+    "should import a PFB file via import service" - {
+      "for the owner of a workspace" in {
+        implicit val token: AuthToken = ownerAuthToken
 
-//    "should asynchronously result in an error when attempting to import a PFB file via import service" - {
-//      "if the file to be imported is invalid" in {
-//        implicit val token: AuthToken = ownerAuthToken
-//
-//        withTemporaryBillingProject(billingAccountId) { projectName =>
-//          withWorkspace(projectName, prependUUID("owner-pfb-import")) { workspaceName =>
-//            // call importPFB as owner
-//            val postResponse: String = Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB",
-//              Map("url" -> "https://storage.googleapis.com/fixtures-for-tests/fixtures/this-intentionally-does-not-exist"))
-//            // expect to get exactly one jobId back
-//            val importJobIdValues: Seq[JsValue] = postResponse.parseJson.asJsObject.getFields("jobId")
-//            importJobIdValues should have size 1
-//
-//            val importJobId: String = importJobIdValues.head match {
-//              case js: JsString => js.value
-//              case x => fail("got an invalid jobId: " + x.toString())
-//            }
-//
-//            // poll for completion as owner
-//            eventually {
-//              val resp: HttpResponse = Orchestration.getRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB/$importJobId")
-//              resp.status shouldBe StatusCodes.OK
-//              blockForStringBody(resp).parseJson.asJsObject.fields.get("status").value shouldBe JsString("Error")
-//            }
-//          }
-//        }(owner.makeAuthToken(billingScopes))
-//      }
-//    }
-//
-//    "should synchronously return an error when attempting to import a PFB file via import service" - {
-//
-//      "for readers of a workspace" in {
-//        val reader = UserPool.chooseStudent
-//
-//        withTemporaryBillingProject(billingAccountId) { projectName =>
-//          withWorkspace(projectName, prependUUID("reader-pfb-import"), aclEntries = List(AclEntry(reader.email, WorkspaceAccessLevel.Reader))) { workspaceName =>
-//
-//            // call importPFB as reader
-//            val exception = intercept[RestException] {
-//              Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB", testPayload)(reader.makeAuthToken())
-//            }
-//
-//            val errorReport = exception.message.parseJson.convertTo[ErrorReport]
-//
-//            errorReport.statusCode.value shouldBe StatusCodes.Forbidden
-//            errorReport.message should include(s"Cannot perform the action write on $projectName/$workspaceName")
-//
-//          }(ownerAuthToken)
-//        }(owner.makeAuthToken(billingScopes))
-//      }
-//
-//      "with an invalid POST payload" in {
-//        implicit val token: AuthToken = ownerAuthToken
-//        withTemporaryBillingProject(billingAccountId) { projectName =>
-//          withWorkspace(projectName, prependUUID("reader-pfb-import")) { workspaceName =>
-//
-//            // call importPFB with a payload of the wrong shape
-//            val exception = intercept[RestException] {
-//              Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB", "this is a string, not json")
-//            }
-//
-//            val errorReport = exception.message.parseJson.convertTo[ErrorReport]
-//
-//            errorReport.statusCode.value shouldBe StatusCodes.BadRequest
-//            errorReport.message should include(s"Object expected in field 'url'")
-//
-//          }(ownerAuthToken)
-//        }(owner.makeAuthToken(billingScopes))
-//      }
-//
-//    }
-//
-//    "should import a TSV asynchronously for entities and entity membership" in {
-//      implicit val token: AuthToken = ownerAuthToken
-//      withTemporaryBillingProject(billingAccountId) { projectName =>
-//        withWorkspace(projectName, prependUUID("tsv-entities")) { workspaceName =>
-//          val participantEntityMetadata = Map("participant" -> EntityTypeMetadata(8, "participant_id", Seq()))
-//          val participantAndSetEntityMetadata = participantEntityMetadata + ("participant_set" -> EntityTypeMetadata(2, "participant_set_id", Seq("participants")))
-//
-//          importAsync(projectName, workspaceName, "ADD_PARTICIPANTS.tsv", participantEntityMetadata)
-//          importAsync(projectName, workspaceName, "MEMBERSHIP_PARTICIPANT_SET.tsv", participantAndSetEntityMetadata)
-//        }
-//      }(owner.makeAuthToken(billingScopes))
-//    }
-//
-//    "should import a TSV asynchronously for updates" in {
-//      implicit val token: AuthToken = ownerAuthToken
-//      withTemporaryBillingProject(billingAccountId) { projectName =>
-//        withWorkspace(projectName, prependUUID("tsv-updates")) { workspaceName =>
-//          val participantEntityMetadata = Map("participant" -> EntityTypeMetadata(8, "participant_id", Seq()))
-//          val updatedParticipantEntityMetadata = Map("participant" -> EntityTypeMetadata(8, "participant_id", Seq("age")))
-//
-//          importAsync(projectName, workspaceName, "ADD_PARTICIPANTS.tsv", participantEntityMetadata)
-//          importAsync(projectName, workspaceName, "UPDATE_PARTICIPANTS.tsv", updatedParticipantEntityMetadata)
-//        }
-//      }(owner.makeAuthToken(billingScopes))
-//
-//    }
-//  }
-  
+        withTemporaryBillingProject(billingAccountId) { projectName =>
+          withWorkspace(projectName, prependUUID("owner-pfb-import")) { workspaceName =>
+            // call importPFB as owner
+            val postResponse: String = Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB", testPayload)
+            // expect to get exactly one jobId back
+            val importJobIdValues: Seq[JsValue] = postResponse.parseJson.asJsObject.getFields("jobId")
+            importJobIdValues should have size 1
+
+            val importJobId: String = importJobIdValues.head match {
+              case js: JsString => js.value
+              case x => fail("got an invalid jobId: " + x.toString())
+            }
+
+            // poll for completion as owner
+            eventuallyWithEscape {
+              val resp: HttpResponse = Orchestration.getRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB/$importJobId")
+              resp.status shouldBe StatusCodes.OK
+              val importStatus = blockForStringBody(resp).parseJson.asJsObject.fields.get("status").value
+              assertWithEscape(
+                importStatus shouldBe JsString("Done"),
+                importStatus should not equal JsString("Error"),
+                "Error encountered during import, failing test early"
+              )
+            }
+
+            // inspect data entities and confirm correct import as owner
+            eventually {
+              val resp = Orchestration.getRequest(s"${ServiceTestConfig.FireCloud.orchApiUrl}api/workspaces/$projectName/$workspaceName/entities")
+              compareMetadata(blockForStringBody(resp).parseJson.convertTo[Map[String, EntityTypeMetadata]], expectedEntities)
+            }
+
+          }
+        }(owner.makeAuthToken(billingScopes))
+      }
+
+      "for writers of a workspace" in {
+        val writer = UserPool.chooseStudent
+        val writerToken = writer.makeAuthToken()
+
+        withTemporaryBillingProject(billingAccountId) { projectName =>
+          withWorkspace(projectName, prependUUID("writer-pfb-import"), aclEntries = List(AclEntry(writer.email, WorkspaceAccessLevel.Writer))) { workspaceName =>
+            // call importPFB as writer
+            val postResponse: String = Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB", testPayload)(writerToken)
+            // expect to get exactly one jobId back
+            val importJobIdValues: Seq[JsValue] = postResponse.parseJson.asJsObject.getFields("jobId")
+            importJobIdValues should have size 1
+            val importJobId: String = importJobIdValues.head match {
+              case js: JsString => js.value
+              case x => fail("got an invalid jobId: " + x.toString())
+            }
+
+            // poll for completion as writer
+            eventuallyWithEscape {
+              val resp = Orchestration.getRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB/$importJobId")(writerToken)
+              val importStatus = blockForStringBody(resp).parseJson.asJsObject.fields.get("status").value
+              assertWithEscape(
+                importStatus shouldBe JsString("Done"),
+                importStatus should not equal JsString("Error"),
+                "Error encountered during import, failing test early"
+              )
+            }
+
+            // inspect data entities and confirm correct import as writer
+            eventually {
+              val resp = Orchestration.getRequest(s"${workspaceUrl(projectName, workspaceName)}/entities")(writerToken)
+              compareMetadata(blockForStringBody(resp).parseJson.convertTo[Map[String, EntityTypeMetadata]], expectedEntities)
+            }
+
+          }(ownerAuthToken)
+        }(owner.makeAuthToken(billingScopes))
+      }
+    }
+
+    "should asynchronously result in an error when attempting to import a PFB file via import service" - {
+      "if the file to be imported is invalid" in {
+        implicit val token: AuthToken = ownerAuthToken
+
+        withTemporaryBillingProject(billingAccountId) { projectName =>
+          withWorkspace(projectName, prependUUID("owner-pfb-import")) { workspaceName =>
+            // call importPFB as owner
+            val postResponse: String = Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB",
+              Map("url" -> "https://storage.googleapis.com/fixtures-for-tests/fixtures/this-intentionally-does-not-exist"))
+            // expect to get exactly one jobId back
+            val importJobIdValues: Seq[JsValue] = postResponse.parseJson.asJsObject.getFields("jobId")
+            importJobIdValues should have size 1
+
+            val importJobId: String = importJobIdValues.head match {
+              case js: JsString => js.value
+              case x => fail("got an invalid jobId: " + x.toString())
+            }
+
+            // poll for completion as owner
+            eventually {
+              val resp: HttpResponse = Orchestration.getRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB/$importJobId")
+              resp.status shouldBe StatusCodes.OK
+              blockForStringBody(resp).parseJson.asJsObject.fields.get("status").value shouldBe JsString("Error")
+            }
+          }
+        }(owner.makeAuthToken(billingScopes))
+      }
+    }
+
+    "should synchronously return an error when attempting to import a PFB file via import service" - {
+
+      "for readers of a workspace" in {
+        val reader = UserPool.chooseStudent
+
+        withTemporaryBillingProject(billingAccountId) { projectName =>
+          withWorkspace(projectName, prependUUID("reader-pfb-import"), aclEntries = List(AclEntry(reader.email, WorkspaceAccessLevel.Reader))) { workspaceName =>
+
+            // call importPFB as reader
+            val exception = intercept[RestException] {
+              Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB", testPayload)(reader.makeAuthToken())
+            }
+
+            val errorReport = exception.message.parseJson.convertTo[ErrorReport]
+
+            errorReport.statusCode.value shouldBe StatusCodes.Forbidden
+            errorReport.message should include(s"Cannot perform the action write on $projectName/$workspaceName")
+
+          }(ownerAuthToken)
+        }(owner.makeAuthToken(billingScopes))
+      }
+
+      "with an invalid POST payload" in {
+        implicit val token: AuthToken = ownerAuthToken
+        withTemporaryBillingProject(billingAccountId) { projectName =>
+          withWorkspace(projectName, prependUUID("reader-pfb-import")) { workspaceName =>
+
+            // call importPFB with a payload of the wrong shape
+            val exception = intercept[RestException] {
+              Orchestration.postRequest(s"${workspaceUrl(projectName, workspaceName)}/importPFB", "this is a string, not json")
+            }
+
+            val errorReport = exception.message.parseJson.convertTo[ErrorReport]
+
+            errorReport.statusCode.value shouldBe StatusCodes.BadRequest
+            errorReport.message should include(s"Object expected in field 'url'")
+
+          }(ownerAuthToken)
+        }(owner.makeAuthToken(billingScopes))
+      }
+
+    }
+
+    "should import a TSV asynchronously for entities and entity membership" in {
+      implicit val token: AuthToken = ownerAuthToken
+      withTemporaryBillingProject(billingAccountId) { projectName =>
+        withWorkspace(projectName, prependUUID("tsv-entities")) { workspaceName =>
+          val participantEntityMetadata = Map("participant" -> EntityTypeMetadata(8, "participant_id", Seq()))
+          val participantAndSetEntityMetadata = participantEntityMetadata + ("participant_set" -> EntityTypeMetadata(2, "participant_set_id", Seq("participants")))
+
+          importAsync(projectName, workspaceName, "ADD_PARTICIPANTS.tsv", participantEntityMetadata)
+          importAsync(projectName, workspaceName, "MEMBERSHIP_PARTICIPANT_SET.tsv", participantAndSetEntityMetadata)
+        }
+      }(owner.makeAuthToken(billingScopes))
+    }
+
+    "should import a TSV asynchronously for updates" in {
+      implicit val token: AuthToken = ownerAuthToken
+      withTemporaryBillingProject(billingAccountId) { projectName =>
+        withWorkspace(projectName, prependUUID("tsv-updates")) { workspaceName =>
+          val participantEntityMetadata = Map("participant" -> EntityTypeMetadata(8, "participant_id", Seq()))
+          val updatedParticipantEntityMetadata = Map("participant" -> EntityTypeMetadata(8, "participant_id", Seq("age")))
+
+          importAsync(projectName, workspaceName, "ADD_PARTICIPANTS.tsv", participantEntityMetadata)
+          importAsync(projectName, workspaceName, "UPDATE_PARTICIPANTS.tsv", updatedParticipantEntityMetadata)
+        }
+      }(owner.makeAuthToken(billingScopes))
+
+    }
+  }
 
   private def importAsync(projectName: String, workspaceName: String, importFilePath: String, expectedResult: Map[String, EntityTypeMetadata])(implicit authToken: AuthToken): Unit = {
     val startTime = System.currentTimeMillis()
