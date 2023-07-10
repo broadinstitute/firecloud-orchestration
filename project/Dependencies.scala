b6a280fbe0794a92d5aa406ca4fc4477b03cea83
Blake Geno <bgeno@broadinstitute.org>
2023-07-10 09:27:26 -0400

    put back rawls model update overwritten from rebasing conflict

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,7 +43,7 @@ object Dependencies {
     "com.typesafe.scala-logging"    %% "scala-logging"       % "3.9.5",
 
     "org.parboiled" % "parboiled-core" % "1.4.1",
-    excludeGuava("org.broadinstitute.dsde"       %% "rawls-model"         % "0.1-1bd0a855")
+    excludeGuava("org.broadinstitute.dsde"       %% "rawls-model"         % "0.1-3be1dba4")
       exclude("com.typesafe.scala-logging", "scala-logging_2.13")
       exclude("com.typesafe.akka", "akka-stream_2.13")
       exclude("com.google.code.findbugs", "jsr305")
