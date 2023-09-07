b6e8eb599a7afcfa1fd180ee712d42840cd7c110
cahrens <roberts@broadinstitute.org>
2023-09-07 14:23:37 -0400

    Update for rawls-model change (WOR-1243).

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,7 @@ object Dependencies {
     "com.typesafe.scala-logging"    %% "scala-logging"       % "3.9.5",
 
     "org.parboiled" % "parboiled-core" % "1.4.1",
-    excludeGuava("org.broadinstitute.dsde"       %% "rawls-model"         % "0.1-97814d79a")
+    excludeGuava("org.broadinstitute.dsde"       %% "rawls-model"         % "0.1-fb0c9691b")
       exclude("com.typesafe.scala-logging", "scala-logging_2.13")
       exclude("com.typesafe.akka", "akka-stream_2.13")
       exclude("com.google.code.findbugs", "jsr305")
