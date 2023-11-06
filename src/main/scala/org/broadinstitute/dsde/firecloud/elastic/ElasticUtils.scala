2bb6225df2321a8dd0869e5f3015f8e88da04d60
David An <davidan@broadinstitute.org>
2023-11-06 11:25:11 -0500

    omit netty3 plugin from TransportClient

--- src/main/scala/org/broadinstitute/dsde/firecloud/elastic/ElasticUtils.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/elastic/ElasticUtils.scala
@@ -1,21 +1,40 @@
 package org.broadinstitute.dsde.firecloud.elastic
 
 import java.net.InetAddress
-
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.transport.InetSocketTransportAddress
 import org.elasticsearch.transport.client.PreBuiltTransportClient
 import akka.http.scaladsl.model.Uri.Authority
+import org.elasticsearch.index.reindex.ReindexPlugin
+import org.elasticsearch.join.ParentJoinPlugin
+import org.elasticsearch.percolator.PercolatorPlugin
+import org.elasticsearch.plugins.Plugin
+import org.elasticsearch.script.mustache.MustachePlugin
+import org.elasticsearch.transport.Netty4Plugin
+
+import java.util
+import java.util.Collections
 
 object ElasticUtils {
-  def buildClient(servers:Seq[Authority], clusterName: String): TransportClient = {
+
+  // copied from PreBuiltTransportClient, but removed Netty3Plugin
+  val pluginList: util.Collection[Class[_ <: Plugin]] = Collections.unmodifiableList(util.Arrays.asList(
+    // classOf[Netty3Plugin],
+    classOf[Netty4Plugin],
+    classOf[ReindexPlugin],
+    classOf[PercolatorPlugin],
+    classOf[MustachePlugin],
+    classOf[ParentJoinPlugin]))
+
+  def buildClient(servers: Seq[Authority], clusterName: String): TransportClient = {
     val settings = Settings.builder
       .put("cluster.name", clusterName)
       .build
     val addresses = servers map { server =>
       new InetSocketTransportAddress(InetAddress.getByName(server.host.address), server.port)
     }
-    new PreBuiltTransportClient(settings).addTransportAddresses(addresses: _*)
+
+    new PreBuiltTransportClient(settings, pluginList).addTransportAddresses(addresses: _*)
   }
 }
