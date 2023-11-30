b985a68dc7250f4b0cd33c40933516983789edd0
David An <davidan@broadinstitute.org>
2023-11-30 15:46:17 -0500

    formatting

--- src/main/scala/org/broadinstitute/dsde/firecloud/Application.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/Application.scala
@@ -6,4 +6,14 @@ import org.broadinstitute.dsde.firecloud.dataaccess._
   * Created by davidan on 9/23/16.
   */
 
-case class Application(agoraDAO: AgoraDAO, googleServicesDAO: GoogleServicesDAO, ontologyDAO: OntologyDAO, rawlsDAO: RawlsDAO, samDAO: SamDAO, searchDAO: SearchDAO, researchPurposeSupport: ResearchPurposeSupport, thurloeDAO: ThurloeDAO, shareLogDAO: ShareLogDAO, importServiceDAO: ImportServiceDAO, shibbolethDAO: ShibbolethDAO)
+case class Application(agoraDAO: AgoraDAO,
+                       googleServicesDAO: GoogleServicesDAO,
+                       ontologyDAO: OntologyDAO,
+                       rawlsDAO: RawlsDAO,
+                       samDAO: SamDAO,
+                       searchDAO: SearchDAO,
+                       researchPurposeSupport: ResearchPurposeSupport,
+                       thurloeDAO: ThurloeDAO,
+                       shareLogDAO: ShareLogDAO,
+                       importServiceDAO: ImportServiceDAO,
+                       shibbolethDAO: ShibbolethDAO)
