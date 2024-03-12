73c1d3562bfb3b212e5c97d8c013301b7cef8ba7
broadbot <broadbot@broadinstitute.org>
2024-03-12 12:01:41 +0000

    Update logback-classic from 1.5.2 to 1.5.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
 
     // elasticsearch requires log4j, but we redirect log4j to logback
     "org.apache.logging.log4j"       % "log4j-to-slf4j"      % "2.23.1",
-    "ch.qos.logback"                 % "logback-classic"     % "1.5.2",
+    "ch.qos.logback"                 % "logback-classic"     % "1.5.3",
     "io.sentry"                      % "sentry-logback"      % "7.5.0",
     "com.typesafe.scala-logging"    %% "scala-logging"       % "3.9.5",
 
