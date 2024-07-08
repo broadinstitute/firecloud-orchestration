2ee1500376a6e648e08b83c6b08af2a52e80e9f3
David An <davidan@broadinstitute.org>
2024-07-08 11:39:49 -0400

    upgrade sbt 1.10.0 -> 1.10.1

--- local-dev/templates/docker-rsync-local-orch.sh
+++ local-dev/templates/docker-rsync-local-orch.sh
@@ -111,7 +111,7 @@ start_server () {
     -p 5051:5051 \
     --network=fc-orch \
     -e JAVA_OPTS="$DOCKER_JAVA_OPTS" \
-    sbtscala/scala-sbt:eclipse-temurin-jammy-17.0.10_7_1.10.0_2.13.14 \
+    sbtscala/scala-sbt:eclipse-temurin-jammy-17.0.10_7_1.10.1_2.13.14 \
     sbt \~reStart
 
     docker cp config/firecloud-account.pem orch-sbt:/etc/firecloud-account.pem
