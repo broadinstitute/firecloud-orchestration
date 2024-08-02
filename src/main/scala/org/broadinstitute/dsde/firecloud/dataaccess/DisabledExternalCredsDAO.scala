12b4aee8b579b5dcc533977c5c15a7cf25eed13d
tlangs <tlangs@broadinstitute.org>
2024-08-02 14:30:35 -0400

    warn is more appropriate

--- src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/DisabledExternalCredsDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/DisabledExternalCredsDAO.scala
@@ -8,25 +8,25 @@ import scala.concurrent.Future
 class DisabledExternalCredsDAO extends ExternalCredsDAO with LazyLogging {
 
   override def getLinkedAccount(implicit userInfo: UserInfo): Future[Option[LinkedEraAccount]] = Future.successful {
-    logger.info("Getting Linked eRA Account from ECM, but ECM is disabled.")
+    logger.warn("Getting Linked eRA Account from ECM, but ECM is disabled.")
     None
   }
 
   override def putLinkedEraAccount(linkedEraAccount: LinkedEraAccount)(implicit orchInfo: WithAccessToken): Future[Unit] = Future.successful {
-    logger.info("Putting Linked eRA Account to ECM, but ECM is disabled.")
+    logger.warn("Putting Linked eRA Account to ECM, but ECM is disabled.")
   }
 
   override def deleteLinkedEraAccount(userInfo: UserInfo)(implicit orchInfo: WithAccessToken): Future[Unit] = Future.successful {
-    logger.info("Deleting Linked eRA Account from ECM, but ECM is disabled.")
+    logger.warn("Deleting Linked eRA Account from ECM, but ECM is disabled.")
   }
 
   override def getLinkedEraAccountForUsername(username: String)(implicit orchInfo: WithAccessToken): Future[Option[LinkedEraAccount]] = Future.successful {
-    logger.info("Getting Linked eRA Account for username from ECM, but ECM is disabled.")
+    logger.warn("Getting Linked eRA Account for username from ECM, but ECM is disabled.")
     None
   }
 
   override def getActiveLinkedEraAccounts(implicit orchInfo: WithAccessToken): Future[Seq[LinkedEraAccount]] = Future.successful {
-    logger.info("Getting Active Linked eRA Accounts from ECM, but ECM is disabled.")
+    logger.warn("Getting Active Linked eRA Accounts from ECM, but ECM is disabled.")
     Seq.empty
   }
 }
