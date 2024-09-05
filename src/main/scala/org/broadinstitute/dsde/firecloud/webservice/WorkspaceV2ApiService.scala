274b7a8beb18f94b346515f639e425c49168a8aa
trholdridge <trholdridge@gmail.com>
2024-09-05 13:09:46 -0400

    simplify passthrough

--- src/main/scala/org/broadinstitute/dsde/firecloud/webservice/WorkspaceV2ApiService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/webservice/WorkspaceV2ApiService.scala
@@ -3,32 +3,15 @@ package org.broadinstitute.dsde.firecloud.webservice
 import akka.http.scaladsl.server.Route
 
 import org.broadinstitute.dsde.firecloud.FireCloudConfig
-import org.broadinstitute.dsde.firecloud.service.{FireCloudDirectives, FireCloudRequestBuilding}
-import org.broadinstitute.dsde.firecloud.utils.{StandardUserInfoDirectives, StreamingPassthrough}
+import org.broadinstitute.dsde.firecloud.service.FireCloudDirectives
+import org.broadinstitute.dsde.firecloud.utils.StreamingPassthrough
 
-import scala.concurrent.ExecutionContext
+trait WorkspaceV2ApiService extends FireCloudDirectives with StreamingPassthrough {
 
-trait WorkspaceV2ApiService
-    extends FireCloudRequestBuilding
-    with FireCloudDirectives
-    with StandardUserInfoDirectives
-    with StreamingPassthrough {
-
-  implicit val executionContext: ExecutionContext
-
-  lazy val rawlsWorkspacesV2Root: String = FireCloudConfig.Rawls.workspacesV2Url
+  private val rawlsWorkspacesV2Root: String = FireCloudConfig.Rawls.workspacesV2Url
 
   val workspaceV2Routes: Route =
     pathPrefix("api" / "workspaces" / "v2") {
-      pathPrefix(Segment / Segment) {
-        (workspaceNamespace, workspaceName) => {
-          val workspaceV2Path = encodeUri(rawlsWorkspacesV2Root + "/%s/%s".format(workspaceNamespace, workspaceName))
-          path("settings") {
-            requireUserInfo() { _ =>
-              streamingPassthrough(workspaceV2Path + "/settings")
-            }
-          }
-        }
-      }
+      streamingPassthrough(rawlsWorkspacesV2Root)
     }
 }
--- src/test/scala/org/broadinstitute/dsde/firecloud/webservice/WorkspaceV2ApiServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/firecloud/webservice/WorkspaceV2ApiServiceSpec.scala
@@ -1,88 +1,22 @@
 package org.broadinstitute.dsde.firecloud.webservice
 
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.StatusCodes.OK
-import akka.http.scaladsl.model.{HttpMethod, HttpMethods, StatusCode}
-import akka.http.scaladsl.server.Route.{seal => sealRoute}
-
-import org.broadinstitute.dsde.firecloud.FireCloudConfig
-import org.broadinstitute.dsde.firecloud.mock.MockUtils
+import akka.http.scaladsl.model.HttpMethods
 import org.broadinstitute.dsde.firecloud.service.BaseServiceSpec
-import org.mockserver.integration.ClientAndServer
-import org.mockserver.integration.ClientAndServer.startClientAndServer
-import org.scalatest.BeforeAndAfterEach
+import org.broadinstitute.dsde.firecloud.FireCloudConfig
 
 import scala.concurrent.ExecutionContext
 
-class WorkspaceV2ApiServiceSpec
-    extends BaseServiceSpec
-    with WorkspaceV2ApiService
-    with BeforeAndAfterEach
-    with SprayJsonSupport {
+class WorkspaceV2ApiServiceSpec extends BaseServiceSpec with WorkspaceV2ApiService {
 
   override val executionContext: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 
-  // Mock remote endpoints
-  private final val workspacesV2Root = FireCloudConfig.Rawls.authPrefix + FireCloudConfig.Rawls.workspacesV2Path
-  private final val workspacesV2Path = workspacesV2Root + "/%s/%s".format("namespace", "name")
+  private val testableRoutes = workspaceV2Routes
 
-  private final val settingsPath = workspacesV2Path + "/settings"
+  "WorkspaceV2 passthrough" - {
 
-  val dummyUserId = "1234"
-
-  var rawlsServer: ClientAndServer = _
-
-  /** Stubs the mock Rawls service to respond to a request. Used for testing passthroughs.
-    *
-    * @param method HTTP method to respond to
-    * @param path   request path
-    * @param status status for the response
-    */
-  def stubRawlsService(method: HttpMethod,
-                       path: String,
-                       status: StatusCode,
-                       body: Option[String] = None,
-                       query: Option[(String, String)] = None,
-                       requestBody: Option[String] = None): Unit = {
-    rawlsServer.reset()
-    val request = org.mockserver.model.HttpRequest.request()
-      .withMethod(method.name)
-      .withPath(path)
-    if (query.isDefined)
-      request.withQueryStringParameter(query.get._1, query.get._2)
-    requestBody.foreach(request.withBody)
-    val response = org.mockserver.model.HttpResponse.response()
-      .withHeaders(MockUtils.header).withStatusCode(status.intValue)
-    if (body.isDefined) response.withBody(body.get)
-    rawlsServer
-      .when(request)
-      .respond(response)
-  }
-
-  override def beforeAll(): Unit = {
-    rawlsServer = startClientAndServer(MockUtils.workspaceServerPort)
-  }
-
-  override def afterAll(): Unit = {
-    rawlsServer.stop
-  }
-
-  "WorkspaceService Passthrough Tests" - {
-
-    "Passthrough tests on the /workspaces/v2/segment/segment/settings path" - {
-      s"OK status is returned for HTTP GET if Rawls returns it" in {
-        stubRawlsService(HttpMethods.GET, settingsPath, OK)
-        Get(settingsPath) ~> dummyUserIdHeaders(dummyUserId) ~> sealRoute(workspaceV2Routes) ~> check {
-          status should equal(OK)
-        }
-      }
-
-      s"OK status is returned for HTTP PUT if Rawls returns it" in {
-        stubRawlsService(HttpMethods.PUT, settingsPath, OK)
-        Put(settingsPath) ~> dummyUserIdHeaders(dummyUserId) ~> sealRoute(workspaceV2Routes) ~> check {
-          status should equal(OK)
-        }
-      }
+    "should pass through HTTP GET for settings" in {
+      val settingsEndpoint = FireCloudConfig.Rawls.workspacesV2Url + "/namespace/name/settings"
+      checkIfPassedThrough(testableRoutes, HttpMethods.GET, settingsEndpoint, toBeHandled = true)
     }
   }
 }
