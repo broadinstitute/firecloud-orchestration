eb8f92b6deb85234a56e1ef9b04b3852602e610b
trholdridge <trholdridge@gmail.com>
2024-10-17 17:22:40 -0400

    Add user project parameter to cost estimate

--- src/main/scala/org/broadinstitute/dsde/firecloud/webservice/WorkspaceApiService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/webservice/WorkspaceApiService.scala
@@ -405,9 +405,14 @@ trait WorkspaceApiService extends FireCloudRequestBuilding with FireCloudDirecti
                 } ~
                 path("storageCostEstimate") {
                   get {
-                    requireUserInfo() { userInfo =>
-                      complete {
-                        workspaceServiceConstructor(userInfo).getStorageCostEstimate(workspaceNamespace, workspaceName)
+                    parameters("userProject".as[GoogleProjectId].optional) { userProject =>
+                      requireUserInfo() { userInfo =>
+                        complete {
+                          workspaceServiceConstructor(userInfo).getStorageCostEstimate(workspaceNamespace,
+                                                                                       workspaceName,
+                                                                                       userProject
+                          )
+                        }
                       }
                     }
                   }
