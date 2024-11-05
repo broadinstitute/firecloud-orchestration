f33dd01dc98c53693392097ec853341a677da943
David An <davidan@broadinstitute.org>
2024-11-05 16:05:58 -0500

    consolidate and simplify passthroughs

---
+++ src/main/scala/org/broadinstitute/dsde/firecloud/webservice/PassthroughApiService.scala
@@ -0,0 +1,18 @@
+package org.broadinstitute.dsde.firecloud.webservice
+
+import akka.http.scaladsl.server.{Directives, Route}
+import org.broadinstitute.dsde.firecloud.FireCloudConfig
+import org.broadinstitute.dsde.firecloud.utils.StreamingPassthrough
+
+trait PassthroughApiService extends Directives with StreamingPassthrough {
+
+  private lazy val agora = FireCloudConfig.Agora.baseUrl
+  private lazy val rawls = FireCloudConfig.Rawls.baseUrl
+
+  val passthroughRoutes: Route = concat(
+    pathPrefix("ga4gh")(streamingPassthrough(s"$agora/ga4gh")),
+    pathPrefix("api" / "billing")(streamingPassthrough(s"$rawls/api/billing")),
+    pathPrefix("api" / "notifications")(streamingPassthrough(s"$rawls/api/notifications"))
+  )
+
+}
