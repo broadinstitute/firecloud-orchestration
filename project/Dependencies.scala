5cc1d856562f5acfb78921d0f081836123cc2e6c
broadbot <broadbot@broadinstitute.org>
2024-12-02 12:01:22 +0000

    CORE-69: Update sentry-logback from 7.18.0 to 7.18.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,7 @@ object Dependencies {
     // elasticsearch requires log4j, but we redirect log4j to logback
     "org.apache.logging.log4j"       % "log4j-to-slf4j"      % "2.24.2",
     "ch.qos.logback"                 % "logback-classic"     % "1.5.12",
-    "io.sentry"                      % "sentry-logback"      % "7.18.0",
+    "io.sentry"                      % "sentry-logback"      % "7.18.1",
     "com.typesafe.scala-logging"    %% "scala-logging"       % "3.9.5",
 
     "org.parboiled" % "parboiled-core" % "1.4.1",
