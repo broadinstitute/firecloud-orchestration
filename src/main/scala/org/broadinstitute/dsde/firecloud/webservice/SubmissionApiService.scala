ac963be39626a06b717f244db65d7e41d8857bf6
David An <davidan@broadinstitute.org>
2024-12-16 08:42:01 -0500

    CORE-165: /api/workspaces passthroughs (#1507)

--- src/main/scala/org/broadinstitute/dsde/firecloud/webservice/SubmissionApiService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/webservice/SubmissionApiService.scala
@@ -1,7 +1,7 @@
 package org.broadinstitute.dsde.firecloud.webservice
 
 import akka.http.scaladsl.server.Route
-import org.broadinstitute.dsde.firecloud.FireCloudConfig.Rawls.{submissionQueueStatusUrl, workspacesUrl}
+import org.broadinstitute.dsde.firecloud.FireCloudConfig.Rawls.submissionQueueStatusUrl
 import org.broadinstitute.dsde.firecloud.service.FireCloudDirectives
 import org.broadinstitute.dsde.firecloud.utils.StreamingPassthrough
 
@@ -9,9 +9,5 @@ trait SubmissionApiService extends FireCloudDirectives with StreamingPassthrough
   val submissionServiceRoutes: Route =
     pathPrefix("submissions" / "queueStatus") {
       streamingPassthrough(submissionQueueStatusUrl)
-    } ~
-      pathPrefix("workspaces" / Segment / Segment / "submissions") { (namespace, name) =>
-        // N.B. streamingPassthrough to ".../submissions" also handles ".../submissionsCount"
-        streamingPassthrough(s"$workspacesUrl/${escapePathSegment(namespace)}/${escapePathSegment(name)}/submissions")
-      }
+    }
 }
