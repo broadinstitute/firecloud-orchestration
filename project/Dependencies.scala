6097f2b5136ad252830804bfcf7b802ee37dbe56
broadbot <broadbot@broadinstitute.org>
2024-12-23 12:10:05 +0000

    CORE-69: Update netty-handler from 4.1.115.Final to 4.1.116.Final

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akkaHttpV = "10.6.3"
   val jacksonV = "2.18.2"
   val jacksonHotfixV = "2.18.2" // for when only some of the Jackson libs have hotfix releases
-  val nettyV = "4.1.115.Final"
+  val nettyV = "4.1.116.Final"
   val workbenchLibsHash = "3e0cf25" // see https://github.com/broadinstitute/workbench-libs readme for hash values
 
   def excludeGuava(m: ModuleID): ModuleID = m.exclude("com.google.guava", "guava")
