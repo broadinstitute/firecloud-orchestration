9e4f4bd50a36798a2572f893ee499fea772253bc
Kevin Marete <kevinmmarete@gmail.com>
2025-03-12 10:41:28 -0400

    Updated matcher to use regex

--- src/main/scala/org/broadinstitute/dsde/firecloud/filematch/strategy/OntSingleReadStrategy.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/filematch/strategy/OntSingleReadStrategy.scala
@@ -1,26 +1,29 @@
 package org.broadinstitute.dsde.firecloud.filematch.strategy
 
 import org.broadinstitute.dsde.firecloud.filematch.result.{FailedMatchResult, FileMatchResult, SuccessfulMatchResult}
-import OntSingleReadStrategy.{fastqGz, FILE_STARTS_ENDINGS}
+import OntSingleReadStrategy.FILE_CONTAIN_ENDINGS
 
 import java.nio.file.{Path, Paths}
+import scala.util.matching.Regex
 
 class OntSingleReadStrategy extends FileRecognitionStrategy {
 
   override def matchFirstFile(path: Path): FileMatchResult = {
-    // search known patterns for a "read1" file
-    val foundMatch = FILE_STARTS_ENDINGS.find { case (key, values) =>
-      path.getFileName.toString.startsWith(key) && values.exists(value => path.toString.endsWith(value))
+    val fileName = path.getFileName.toString
+
+    // Used regex to match the file name containing the key and ending with any of the specified values
+    val foundMatch = FILE_CONTAIN_ENDINGS.find { case (key, values) =>
+      val regex = new Regex(s".*${Regex.quote(key)}.*(${values.map(Regex.quote).mkString("|")})$$")
+      regex.findFirstIn(fileName).isDefined
     }
 
     foundMatch match {
       // we found a "read1"
       case Some((_, values)) =>
         // find the matching value
-        val value = values.find(value => path.toString.endsWith(value)).get
-        // generate the id: strip the suffix from the filename.
-        val id = path.getFileName.toString.replace(value, "")
-
+        val value = values.find(value => fileName.endsWith(value)).get
+        // generate the id: strip the value from the filename.
+        val id = fileName.replace(value, "")
         SuccessfulMatchResult(path, Paths.get(""), id)
 
       // the file is not recognized
@@ -30,12 +33,9 @@ class OntSingleReadStrategy extends FileRecognitionStrategy {
 }
 
 object OntSingleReadStrategy {
-  private val fastqGz = ".fastq.gz"
-  // if the first file starts with ${key} and ends with any of the values in the list, then this is a single read file
-  val FILE_STARTS_ENDINGS: Map[String, List[String]] = Map(
-    "Complete_barcode" -> List(fastqGz, ".clean.fastq"),
-    "Incomplete_barcode" -> List(fastqGz),
-    "Repeat_barcode" -> List(fastqGz),
-    "barcode" -> List(fastqGz)
+  // if the first file contains ${key} and ends with any of the values in the list, then this is a single read file
+  val FILE_CONTAIN_ENDINGS: Map[String, List[String]] = Map(
+    "Complete_barcode" -> List(".clean.fastq"),
+    "barcode" -> List(".fastq", ".fastq.gz")
   )
 }
