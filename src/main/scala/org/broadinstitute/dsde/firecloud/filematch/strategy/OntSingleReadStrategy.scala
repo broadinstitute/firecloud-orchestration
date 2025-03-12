e97d1d1ef61c78ebb10ad5b46e46a1c200c31c1a
Kevin Marete <kevinmmarete@gmail.com>
2025-03-12 16:40:41 -0400

    Simplify regex

--- src/main/scala/org/broadinstitute/dsde/firecloud/filematch/strategy/OntSingleReadStrategy.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/filematch/strategy/OntSingleReadStrategy.scala
@@ -1,44 +1,21 @@
 package org.broadinstitute.dsde.firecloud.filematch.strategy
 
 import org.broadinstitute.dsde.firecloud.filematch.result.{FailedMatchResult, FileMatchResult, SuccessfulMatchResult}
-import OntSingleReadStrategy.FILE_CONTAIN_ENDINGS
+import OntSingleReadStrategy.PATTERN
 
 import java.nio.file.{Path, Paths}
 import scala.util.matching.Regex
 
 class OntSingleReadStrategy extends FileRecognitionStrategy {
 
-  // Precompile regex patterns: (?i).*barcode\d+.*(\.fastq|\.fastq\.gz)$
-  private val compiledPatterns: Map[String, Regex] = FILE_CONTAIN_ENDINGS.map { case (key, values) =>
-    key -> new Regex(s"(?i).*${Regex.quote(key)}\\d+.*(${values.map(Regex.quote).mkString("|")})$$")
-  }
-
-  override def matchFirstFile(path: Path): FileMatchResult = {
-    val fileName = path.getFileName.toString
-
-    // Use precompiled regex patterns to match the file name
-    val foundMatch = compiledPatterns.find { case (_, regex) =>
-      regex.findFirstIn(fileName).isDefined
+  override def matchFirstFile(path: Path): FileMatchResult =
+    PATTERN.findFirstMatchIn(path.toString) match {
+      case Some(regexMatch) => SuccessfulMatchResult(path, Paths.get(""), regexMatch.group(1))
+      case None             => FailedMatchResult(path)
     }
-
-    foundMatch match {
-      // we found a "read1"
-      case Some((key, _)) =>
-        // Extract the matching value directly from the regex match
-        val value = FILE_CONTAIN_ENDINGS(key).find(value => fileName.endsWith(value)).get
-        // generate the id: strip the value and any characters before the extension from the filename.
-        val id = fileName.stripSuffix(value).replaceAll("\\..*$", "")
-        SuccessfulMatchResult(path, Paths.get(""), id)
-
-      // the file is not recognized
-      case None => FailedMatchResult(path)
-    }
-  }
 }
 
 object OntSingleReadStrategy {
-  // if the file contains ${key} and ends with any value in the list, it's an ONT single read file
-  val FILE_CONTAIN_ENDINGS: Map[String, List[String]] = Map(
-    "barcode" -> List(".fastq", ".fastq.gz")
-  )
+  // if the file contains barcode### and ends with .fastq or .fastq.gz, it's an ONT single read file
+  val PATTERN: Regex = "(?i)(.*barcode\\d+).*\\.fastq(?:.gz)*$".r
 }
