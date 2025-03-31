12ec28563e89cbddd77385bdb916fb1e6c4cd914
broadbot <broadbot@broadinstitute.org>
2025-03-31 12:01:12 +0000

    CORE-69: Update google-api-services-admin-directory from directory_v1-rev20250217-2.0.0 to directory_v1-rev20250325-2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
     "org.yaml"                   % "snakeyaml"                  % "2.4",
     "org.apache.commons"         % "commons-compress"           % "1.27.1", // workbench-libs libraries pull this in
     "com.google.apis"            % "google-api-services-pubsub" % "v1-rev20250311-2.0.0", // from workbench-google2
-    "com.google.apis"  % "google-api-services-admin-directory"  % "directory_v1-rev20250217-2.0.0" // from workbench-google2
+    "com.google.apis"  % "google-api-services-admin-directory"  % "directory_v1-rev20250325-2.0.0" // from workbench-google2
   )
 
   val rootDependencies: Seq[ModuleID] = Seq(
