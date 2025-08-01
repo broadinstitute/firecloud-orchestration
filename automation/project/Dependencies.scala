d7ac1d56da34a618d537dc952cb306eb129eccb3
broadbot <broadbot@broadinstitute.org>
2025-08-01 12:11:56 +0000

    CORE-69: Update jackson-core from 2.19.1 to 2.19.2

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val scalaV = "2.13.16"
 
   val jacksonV = "2.19.2"
-  val jacksonHotfixV = "2.19.1" // for when only some of the Jackson libs have hotfix releases
+  val jacksonHotfixV = "2.19.2" // for when only some of the Jackson libs have hotfix releases
   val akkaV = "2.6.19"
   val akkaHttpV = "10.2.10"
   val workbenchLibsHash = "9df42f5"
