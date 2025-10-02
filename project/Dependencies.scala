d3f6e785c18d34114deb4f381fe4182b145a8139
David An <davidan@broadinstitute.org>
2025-10-02 12:57:13 -0400

    use last non-BSL akka versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Dependencies {
-  val akkaV = "2.8.8"
-  val akkaHttpV = "10.5.3"
+  val akkaV = "2.6.21"
+  val akkaHttpV = "10.2.10"
   val jacksonV = "2.20.0"
   val jacksonAnnotationsV = "2.20"
   val workbenchLibsHash = "56f2c74" // see https://github.com/broadinstitute/workbench-libs readme for hash values
