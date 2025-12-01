cf63e81c5834221a495e3318b1efb081b36e8b7c
broadbot <broadbot@broadinstitute.org>
2025-12-01 12:19:49 +0000

    CORE-69: Update spring-web from 6.2.12 to 6.2.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -57,7 +57,7 @@ object Dependencies {
     "org.broadinstitute.dsde.workbench" %% "workbench-notifications" % s"2.0-$workbenchLibsHash",
     "org.databiosphere" % "workspacedataservice-client-okhttp-jakarta" % "0.2.167-SNAPSHOT",
     "bio.terra" % "externalcreds-client-resttemplate" % "1.83.0-SNAPSHOT" excludeAll (excludeSpring, excludeSpringBoot),
-    "org.springframework" % "spring-web" % "6.2.12" excludeAll (excludeSpringBoot, excludeSpringJcl),
+    "org.springframework" % "spring-web" % "6.2.14" excludeAll (excludeSpringBoot, excludeSpringJcl),
     "com.typesafe.akka" %% "akka-actor" % akkaV,
     "com.typesafe.akka" %% "akka-slf4j" % akkaV,
     "com.typesafe.akka" %% "akka-http" % akkaHttpV excludeAll (excludeAkkaActor, excludeAkkaStream),
