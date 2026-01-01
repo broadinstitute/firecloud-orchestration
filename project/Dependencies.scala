49b5f9fe43dd3506e00d15b4b13db2fc380032ce
broadbot <broadbot@broadinstitute.org>
2026-01-01 12:10:21 +0000

    CORE-69: Update google-api-services-pubsub from v1-rev20251104-2.0.0 to v1-rev20251212-2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     "com.fasterxml.jackson.core" % "jackson-core" % jacksonV,
     "org.yaml" % "snakeyaml" % "2.5",
     "org.apache.commons" % "commons-compress" % "1.28.0", // workbench-libs libraries pull this in
-    "com.google.apis" % "google-api-services-pubsub" % "v1-rev20251104-2.0.0", // from workbench-google2
+    "com.google.apis" % "google-api-services-pubsub" % "v1-rev20251212-2.0.0", // from workbench-google2
     "com.google.apis" % "google-api-services-admin-directory" % "directory_v1-rev20251107-2.0.0" // from workbench-google2
   )
 
