1eda20a72fe2ed723a9a01094bdd2e0d45ac4eb3
broadbot <broadbot@broadinstitute.org>
2026-01-01 12:19:32 +0000

    CORE-69: Update sentry-logback from 8.27.1 to 8.29.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
 
   val rootDependencies: Seq[ModuleID] = Seq(
     "ch.qos.logback" % "logback-classic" % "1.5.23",
-    "io.sentry" % "sentry-logback" % "8.27.1",
+    "io.sentry" % "sentry-logback" % "8.29.0",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.6",
     "org.parboiled" % "parboiled-core" % "1.4.1",
     "org.broadinstitute.dsde" %% "rawls-model" % "v0.0.537-SNAP"
