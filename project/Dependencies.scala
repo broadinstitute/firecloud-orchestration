b9a3aae66cd12aa7eb5d5f1706934f64f275dc8f
broadbot <broadbot@broadinstitute.org>
2026-01-01 12:19:27 +0000

    CORE-69: Update logback-classic from 1.5.21 to 1.5.23

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies {
   )
 
   val rootDependencies: Seq[ModuleID] = Seq(
-    "ch.qos.logback" % "logback-classic" % "1.5.21",
+    "ch.qos.logback" % "logback-classic" % "1.5.23",
     "io.sentry" % "sentry-logback" % "8.27.1",
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.6",
     "org.parboiled" % "parboiled-core" % "1.4.1",
