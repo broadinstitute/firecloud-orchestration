9148b22735c5d5e7f6339ad528165d04a7671a8d
Bria Morgan <bmorgan@manifold.ai>
2026-05-22 10:36:18 -0400

    remove api

--- src/main/scala/org/broadinstitute/dsde/firecloud/webservice/NihApiService.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/webservice/NihApiService.scala
@@ -34,12 +34,7 @@ trait NihApiService extends Directives with RequestBuilding with EnabledUserDire
     requireUserInfo() { userInfo =>
       requireEnabledUser(userInfo) {
         pathPrefix("nih") {
-          path("callback") {
-            post {
-              complete(StatusCodes.Gone)
-            }
-          } ~
-            path("status") {
+          path("status") {
               complete(nihServiceConstructor().getNihStatus(userInfo))
             } ~
             path("account") {
