9fb9e71f8d6c56748a5154b68e1112ce66ba7e80
Bria Morgan <calypsomatic@gmail.com>
2026-06-03 10:06:50 -0400

    [CTM-42] Remove ERA Commons/AnVIL provider sync from Orchestration (#1709)

--- src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/DisabledExternalCredsDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/DisabledExternalCredsDAO.scala
@@ -1,39 +1,11 @@
 package org.broadinstitute.dsde.firecloud.dataaccess
 
 import com.typesafe.scalalogging.LazyLogging
-import org.broadinstitute.dsde.firecloud.model.{LinkedEraAccount, UserInfo, WithAccessToken}
+import org.broadinstitute.dsde.firecloud.model.WithAccessToken
 
 import scala.concurrent.Future
 
 class DisabledExternalCredsDAO extends ExternalCredsDAO with LazyLogging {
-
-  override def getLinkedAccount(userInfo: UserInfo): Future[Option[LinkedEraAccount]] = Future.successful {
-    logger.warn("Getting Linked eRA Account from ECM, but ECM is disabled.")
-    None
-  }
-
-  override def putLinkedEraAccount(linkedEraAccount: LinkedEraAccount, orchInfo: WithAccessToken): Future[Unit] =
-    Future.successful {
-      logger.warn("Putting Linked eRA Account to ECM, but ECM is disabled.")
-    }
-
-  override def deleteLinkedEraAccount(userInfo: UserInfo, orchInfo: WithAccessToken): Future[Unit] =
-    Future.successful {
-      logger.warn("Deleting Linked eRA Account from ECM, but ECM is disabled.")
-    }
-
-  override def getLinkedEraAccountForUsername(username: String,
-                                              orchInfo: WithAccessToken
-  ): Future[Option[LinkedEraAccount]] = Future.successful {
-    logger.warn("Getting Linked eRA Account for username from ECM, but ECM is disabled.")
-    None
-  }
-
-  override def getActiveLinkedEraAccounts(orchInfo: WithAccessToken): Future[Seq[LinkedEraAccount]] =
-    Future.successful {
-      logger.warn("Getting Active Linked eRA Accounts from ECM, but ECM is disabled.")
-      Seq.empty
-    }
 
   override def getVisas(provider: String,
                         userId: String,
--- src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ExternalCredsDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/firecloud/dataaccess/ExternalCredsDAO.scala
@@ -1,26 +1,11 @@
 package org.broadinstitute.dsde.firecloud.dataaccess
 
-import org.broadinstitute.dsde.firecloud.model.{LinkedEraAccount, UserInfo, WithAccessToken}
+import org.broadinstitute.dsde.firecloud.model.WithAccessToken
 import org.databiosphere.workspacedata.client.ApiException
 
 import scala.concurrent.Future
 
 trait ExternalCredsDAO {
-
-  @throws(classOf[ApiException])
-  def getLinkedAccount(userInfo: UserInfo): Future[Option[LinkedEraAccount]]
-
-  @throws(classOf[ApiException])
-  def putLinkedEraAccount(linkedEraAccount: LinkedEraAccount, orchInfo: WithAccessToken): Future[Unit]
-
-  @throws(classOf[ApiException])
-  def deleteLinkedEraAccount(userInfo: UserInfo, orchInfo: WithAccessToken): Future[Unit]
-
-  @throws(classOf[ApiException])
-  def getLinkedEraAccountForUsername(username: String, orchInfo: WithAccessToken): Future[Option[LinkedEraAccount]]
-
-  @throws(classOf[ApiException])
-  def getActiveLinkedEraAccounts(orchInfo: WithAccessToken): Future[Seq[LinkedEraAccount]]
 
   @throws(classOf[ApiException])
   def getVisas(provider: String,
